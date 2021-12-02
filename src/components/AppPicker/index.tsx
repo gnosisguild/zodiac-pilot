c176d49150616bada6412645c93a45383676383a
samepant <sampanter@gmail.com>
2021-12-02 14:10:56 -0500

    adds logo files and component

---
+++ src/components/AppPicker/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+import classes from './style.module.css'
+
+const apps = [{ name: 'uniswap', url: 'https://app.uniswap.org' }]
+const AppPicker = () => {
+  return <ul>apps</ul>
+}
