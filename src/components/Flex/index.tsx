6b99aa7fbe701fb78edd750f868dbc492013f2be
samepant <sampanter@gmail.com>
2021-12-02 19:30:16 -0500

    stlyes for the browser page

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -4,12 +4,25 @@ import React from 'react'
 import classes from './style.module.css'
 
 interface Props {
+  justifyContent?: 'spaceAround' | 'spaceBetween' | 'center' | 'end' | 'start'
   direction?: 'row' | 'column'
   gap: 0 | 1 | 2 | 3
 }
 
-const Flex: React.FC<Props> = ({ gap, direction = 'row', children }) => (
-  <div className={cn(classes.flex, classes[`gap${gap}`], classes[direction])}>
+const Flex: React.FC<Props> = ({
+  gap,
+  direction = 'row',
+  justifyContent = 'start',
+  children,
+}) => (
+  <div
+    className={cn(
+      classes.flex,
+      classes[`gap${gap}`],
+      classes[direction],
+      classes[justifyContent]
+    )}
+  >
     {children}
   </div>
 )
