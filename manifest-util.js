26fb6328c6c4cf04c0b7b07e5f8b5419f8908258
samepant <sampanter@gmail.com>
2022-05-25 14:18:15 -0400

    add github action for zipping and uploading to IPFS

---
+++ manifest-util.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+// this script is used to update the release value
+// in the manifest.json when released through github
+// actions. Meant to be used as a cli script:
+//
+// node manifest-util.js ./public/manifest.json
+
+const main = () => {
+  const manifestPath = process.argv[2];
+  const releaseTag = process.env.RELEASE_TAG;
+  if (!manifestPath) {
+    return console.log('Please provide a path to the Manifest file.')
+  }
+  if (!releaseTag) {
+    return console.log('Please provide a RELEASE_TAG env variable.')
+  }
+
+  updateManifest(manifestPath, releaseTag);
+}
+
+const updateManifest = (manifestFilename, releaseTag) => {
+  try {
+    const data = fs.readFileSync(manifestFilename);
+    const manifest = JSON.parse(data);
+    manifest["version"] = releaseTag;
+    fs.writeFileSync(manifestFilename, JSON.stringify(manifest))
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
