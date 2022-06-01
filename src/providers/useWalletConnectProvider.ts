7d772f3f560c2a549ebcb7108b8ec6274400bb29
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-06-01 16:50:34 +0200

    fix event emitter max listeners warning

--- src/providers/useWalletConnectProvider.ts
+++ src/providers/useWalletConnectProvider.ts
@@ -6,7 +6,25 @@ interface Result {
   connected: boolean
 }
 
+class ProviderEventTarget extends EventTarget {
+  constructor(provider: WalletConnectEthereumProvider) {
+    super()
+
+    // @ts-expect-error signer is a private property, but we didn't find another way
+    provider.signer.on('connect', () => {
+      this.dispatchEvent(new Event('connect'))
+    })
+
+    provider.on('disconnect', () => {
+      this.dispatchEvent(new Event('disconnect'))
+    })
+  }
+}
+
+// Global states for providers and event targets
 const providers: Record<string, WalletConnectEthereumProvider> = {}
+// We need the indirection via an extra EventTarget to workaround MaxListenersExceededWarning emitted from WalletConnectEthereumProvider
+const providerEventTargets: Record<string, ProviderEventTarget> = {}
 
 const useWalletConnectProvider = (connectionId: string): Result => {
   if (!providers[connectionId]) {
@@ -17,30 +35,33 @@ const useWalletConnectProvider = (connectionId: string): Result => {
         100: 'https://rpc.gnosischain.com/',
       },
     })
+    providerEventTargets[connectionId] = new ProviderEventTarget(
+      providers[connectionId]
+    )
   }
+
   const provider = providers[connectionId]
 
   const [connected, setConnected] = useState(provider.connected)
+  const providerEventTarget = providerEventTargets[connectionId]
   useEffect(() => {
     const handleConnection = () => {
       console.log(`WalletConnect connected: ${connectionId}`)
       setConnected(true)
     }
-    // @ts-expect-error signer is a private property, but we didn't find another way
-    provider.signer.on('connect', handleConnection)
+    providerEventTarget.addEventListener('connect', handleConnection)
 
     const handleDisconnection = () => {
       console.log(`WalletConnect disconnected: ${connectionId}`)
       setConnected(false)
     }
-    provider.on('disconnect', handleDisconnection)
+    providerEventTarget.addEventListener('disconnect', handleDisconnection)
 
     return () => {
-      // @ts-expect-error signer is a private property, but we didn't find another way
-      provider.signer.off('connect', handleConnection)
-      provider.off('disconnect', handleDisconnection)
+      providerEventTarget.removeEventListener('connect', handleConnection)
+      providerEventTarget.removeEventListener('disconnect', handleDisconnection)
     }
-  }, [provider, connectionId])
+  }, [providerEventTarget, connectionId])
 
   return { provider, connected }
 }
