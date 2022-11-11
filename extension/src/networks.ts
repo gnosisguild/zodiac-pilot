8f077a23d9edc051a8756959586bf69aea5fdd36
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-11-11 10:35:23 +0100

    complete networks info, add zodiac dep

--- extension/src/networks.ts
+++ extension/src/networks.ts
@@ -1,36 +1,76 @@
 export const RPC = {
   1: 'https://mainnet.infura.io/v3/b81b456501e34bed8a85a3c2ff8f4577',
   4: 'https://rinkeby.infura.io/v3/b81b456501e34bed8a85a3c2ff8f4577',
-  100: 'https://rpc.gnosischain.com/',
+  5: 'https://goerli.infura.io/v3/b81b456501e34bed8a85a3c2ff8f4577',
+  10: 'https://mainnet.optimism.io',
+  56: 'https://bsc-dataseed.binance.org',
+  100: 'https://rpc.gnosischain.com',
+  137: 'https://polygon-rpc.com',
+  246: 'https://rpc.energyweb.org',
+  42161: 'https://arb1.arbitrum.io/rpc',
+  42220: 'https://forno.celo.org',
+  73799: 'https://volta-rpc.energyweb.org',
+  80001: 'https://rpc-mumbai.maticvigil.com',
 }
 
-export const EXPLORER_URL = {
+export type ChainId = keyof typeof RPC
+
+export const EXPLORER_URL: Record<ChainId, string> = {
   1: 'https://etherscan.io',
   4: 'https://rinkeby.etherscan.io',
+  5: 'https://goerli.etherscan.io',
+  10: 'https://optimistic.etherscan.io',
+  56: 'https://bscscan.com',
   100: 'https://gnosisscan.io',
+  137: 'https://polygonscan.com',
+  246: 'https://explorer.energyweb.org',
+  42161: 'https://arbiscan.io',
+  42220: 'https://explorer.celo.org',
+  73799: 'https://volta-explorer.energyweb.org',
+  80001: 'https:/testnet.polygonscan.com',
 }
 
-export const EXPLORER_API_URL = {
+export const EXPLORER_API_URL: Record<ChainId, string> = {
   1: 'https://api.etherscan.io/api',
   4: 'https://api-rinkeby.etherscan.io/api',
+  5: 'https://api-goerli.etherscan.io/api',
+  10: 'https://api-optimistic.etherscan.io/api',
+  56: 'https://api.bscscan.com/api',
   100: 'https://blockscout.com/xdai/mainnet/api',
-  // 73799: 'https://volta-explorer.energyweb.org/api',
-  // 246: 'https://explorer.energyweb.org/api',
-  // 137: 'https://api.polygonscan.com/api',
-  // 56: 'https://api.bscscan.com/api',
-  // 42161: 'https://api.arbiscan.io/api',
+  137: 'https://api.polygonscan.com/api',
+  246: 'https://explorer.energyweb.org/api',
+  42161: 'https://api.arbiscan.io/api',
+  42220: 'https://explorer.celo.org/api',
+  73799: 'https://volta-explorer.energyweb.org/api',
+  80001: 'https://api-testnet.polygonscan.com/api',
 }
 
-export const NETWORK_PREFIX = {
+export const NETWORK_PREFIX: Record<ChainId, string> = {
   1: 'eth',
   4: 'rin',
+  5: 'gor',
+  10: 'oeth',
+  56: 'bsc',
   100: 'gno',
+  137: 'matic',
+  246: 'ewt',
+  42161: 'arb1',
+  42220: 'celo',
+  73799: 'vt',
+  80001: 'maticmum',
 }
 
-export const NETWORK_CURRENCY = {
+export const NETWORK_CURRENCY: Record<ChainId, string> = {
   1: 'ETH',
   4: 'ETH',
+  5: 'ETH',
+  56: 'BNB',
+  10: 'ETH',
   100: 'xDAI',
+  137: 'MATIC',
+  246: 'EWT',
+  42161: 'ETH',
+  42220: 'CELO',
+  73799: 'VT',
+  80001: 'MATIC',
 }
-
-export type ChainId = keyof typeof EXPLORER_URL
