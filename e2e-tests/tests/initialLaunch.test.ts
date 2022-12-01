bf3cb10448f72a6f2757463fc46841f3f04d1838
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-12-01 17:43:35 +0100

    two tests green, one to make work

--- e2e-tests/tests/initialLaunch.test.ts
+++ e2e-tests/tests/initialLaunch.test.ts
@@ -1,17 +1,33 @@
-import { Dappeteer } from '@chainsafe/dappeteer'
-import { Browser, Page } from 'puppeteer'
+import { getDocument, queries, waitFor } from 'pptr-testing-library'
+import { launch, launchFresh } from '../helpers/launch'
 
 describe('initial launch', () => {
   it('should show the settings of the default connection on first ever launch', async () => {
-    const page = await browser.newPage()
-    await page.goto('https://pilot.gnosisguild.org')
-    const $addConn = await page.waitForSelector('._button_137m7_1')
-    console.log('ok', await page.content())
-    expect($addConn).toBeFalsy()
+    const page = await launchFresh()
+    const $doc = await getDocument(page)
+
+    await queries.findByText($doc, 'New connection')
+
+    await page.close()
   })
 
-  it.todo(
-    'should show the connections list if multiple connections are stored but the selected one is not ready'
-  )
-  it.todo('should show the browser if the selected connection is ready')
+  it('should show the connections list if multiple connections are stored but the selected one is not ready', async () => {
+    const page = await launch('gnosis')
+    const $doc = await getDocument(page)
+
+    await queries.findByText($doc, 'Connections')
+    await queries.findByText($doc, 'Gnosis DAO on GC')
+
+    await page.close()
+  })
+
+  it('should show the browser if the selected connection is ready', async () => {
+    const page = await launch('jan')
+    const $doc = await getDocument(page)
+
+    await queries.findByPlaceholderText($doc, 'Type a url')
+    await queries.findByText($doc, 'Jan Test DAO')
+
+    await page.close()
+  })
 })
