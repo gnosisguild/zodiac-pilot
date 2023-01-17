e1c327eac059ae9dcabddddf0e9898c1444102eb
Laurent De Marez <laurent@demarez.org>
2023-01-17 12:02:03 +0100

    fix: support all uniswap multicall translations

--- extension/src/transactionTranslations/uniswapMulticall.ts
+++ extension/src/transactionTranslations/uniswapMulticall.ts
@@ -1,10 +1,12 @@
 import { KnownContracts } from '@gnosis.pm/zodiac'
-import { Interface } from 'ethers/lib/utils'
+import { FunctionFragment, Interface } from 'ethers/lib/utils'
 
 import { TransactionTranslation } from './types'
 
 const uniswapMulticallInterface = new Interface([
   'function multicall(bytes[] calldata data) external returns (bytes[] memory results)',
+  'function multicall(uint256 deadline, bytes[] calldata data) external returns (bytes[] memory results)',
+  'function multicall(bytes32 previousBlockhash, bytes[] calldata data) external returns (bytes[] memory results)',
 ])
 
 export default {
@@ -15,17 +17,21 @@ export default {
   translate: (transaction) => {
     if (!transaction.data) return [transaction]
 
-    let functionCalls: string[]
-    try {
-      functionCalls = uniswapMulticallInterface.decodeFunctionData(
-        'multicall',
-        transaction.data
-      ).data as string[]
-    } catch (e) {
-      return undefined
+    let functionCalls: string[] = []
+    for (const fragment of uniswapMulticallInterface.fragments) {
+      if (!(fragment instanceof FunctionFragment)) continue
+      try {
+        functionCalls = uniswapMulticallInterface.decodeFunctionData(
+          fragment,
+          transaction.data
+        ).data as string[]
+        break
+      } catch (e) {
+        continue
+      }
     }
 
-    if (functionCalls && functionCalls.length > 0) {
+    if (functionCalls.length > 0) {
       return functionCalls.map((data) => ({ ...transaction, data }))
     }
 
