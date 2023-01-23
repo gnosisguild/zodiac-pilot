824f5c7999ca3a60edcabd89000dbc2937f88070
samepant <sampanter@gmail.com>
2023-01-23 14:32:07 -0500

    tweak drawer styles and connection list styles

--- extension/src/browser/ConnectionsDrawer/ConnectionsList.tsx
+++ extension/src/browser/ConnectionsDrawer/ConnectionsList.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { VscDebugDisconnect } from 'react-icons/vsc'
 
 import { Box, BoxButton, ConnectionStack, Flex } from '../../components'
+import {
+  ConnectedIcon,
+  DisconnectedIcon,
+} from '../../settings/Connection/ConnectIcon'
 import { useConnection, useConnections } from '../../settings/connectionHooks'
 import { Connection } from '../../types'
 
@@ -34,48 +38,68 @@ const ConnectionItem: React.FC<ConnectionItem> = ({
       className={classes.connectionItemContainer}
       onClick={handleLaunch}
     >
-      <Flex gap={1} justifyContent="space-between" alignItems="center">
-        <Flex gap={2} alignItems="center">
-          <Box rounded className={classes.connectionIcon}>
-            {connected && (
-              <ConnectIcon
-                role="status"
-                size={24}
-                color="green"
-                title="Pilot wallet is connected"
-              />
-            )}
-            {!connected && !connect && (
-              <VscDebugDisconnect
-                role="status"
-                size={24}
-                color="crimson"
-                title="Pilot wallet is not connected"
-              />
-            )}
-            {!connected && connect && (
-              <ConnectIcon
-                role="status"
-                size={24}
-                color="orange"
-                title="Pilot wallet is connected to a different chain"
-              />
-            )}
-          </Box>
-          <div>
-            <h2>{connection.label || 'Untitled'}</h2>
-          </div>
+      <Flex direction="column" gap={4}>
+        <Flex
+          direction="row"
+          gap={2}
+          justifyContent="space-between"
+          className={classes.labelContainer}
+        >
+          <Flex direction="row" alignItems="center" gap={3}>
+            <Box className={classes.connectionIcon}>
+              {connected && (
+                <ConnectedIcon
+                  role="status"
+                  size={24}
+                  color="green"
+                  title="Pilot wallet is connected"
+                />
+              )}
+              {!connected && !connect && (
+                <DisconnectedIcon
+                  role="status"
+                  size={24}
+                  color="crimson"
+                  title="Pilot wallet is not connected"
+                />
+              )}
+              {!connected && connect && (
+                <ConnectedIcon
+                  role="status"
+                  size={24}
+                  color="orange"
+                  title="Pilot wallet is connected to a different chain"
+                />
+              )}
+            </Box>
+            <h2>{connection.label}</h2>
+          </Flex>
+
+          <BoxButton onClick={handleModify} className={classes.modifyButton}>
+            Modify
+          </BoxButton>
         </Flex>
-        <BoxButton className={classes.modifyButton} onClick={handleModify}>
-          Modify
-        </BoxButton>
+        <Flex
+          direction="row"
+          gap={5}
+          alignItems="baseline"
+          className={classes.infoContainer}
+        >
+          <ConnectionStack
+            connection={connection}
+            addressBoxClass={classes.addressBox}
+          />
+          <Flex
+            direction="column"
+            alignItems="start"
+            gap={2}
+            className={classes.info}
+          >
+            <div className={classes.infoDatum}>STUB DAYS AGO</div>
+            <div className={classes.infoLabel}>Last Used</div>
+          </Flex>
+        </Flex>
       </Flex>
-      <div className={classes.connectionInfoContainer}>
-        <ConnectionStack
-          connection={connection}
-          helperClass={classes.addressHelper}
-        />
-      </div>
     </BoxButton>
   )
 }
@@ -87,7 +111,7 @@ const ConnectionsList: React.FC<ConnectionsListProps> = ({
   const [connections] = useConnections()
 
   return (
-    <Flex direction="column" gap={3}>
+    <>
       {connections.map((connection) => (
         <ConnectionItem
           key={connection.id}
@@ -96,7 +120,7 @@ const ConnectionsList: React.FC<ConnectionsListProps> = ({
           onModify={onModify}
         />
       ))}
-    </Flex>
+    </>
   )
 }
 
