56142d4fa67a0235a5f48a09922199c20a11c290
Laurent De Marez <laurent@demarez.org>
2023-03-09 11:55:46 +0100

    feat: add transactionHooks

---
+++ extension/src/browser/state/transactionHooks.ts
@@ -0,0 +1,30 @@
+import { useCallback } from "react"
+import { useAllTransactions, useDispatch } from "."
+import { ForkProvider } from "../../providers"
+import { useProvider } from "../ProvideProvider"
+
+export const useClearTransactions = () => {
+  const transactions = useAllTransactions()
+  const provider = useProvider()
+  const dispatch = useDispatch()
+
+  const hasTransactions = transactions.length > 0
+  const clearTransactions = useCallback(async () => {
+    if (transactions.length === 0) {
+      return
+    }
+
+    const firstTransaction = transactions[0]
+    const checkpoint = firstTransaction.input.id
+
+    dispatch({ type: 'REMOVE_TRANSACTION', payload: { id: checkpoint } })
+
+    if (!(provider instanceof ForkProvider)) {
+      throw new Error('This is only supported when using ForkProvider')
+    }
+
+    await provider.request({ method: 'evm_revert', params: [checkpoint] })
+  }, [provider, transactions, dispatch])
+
+  return { hasTransactions, clearTransactions }
+}
