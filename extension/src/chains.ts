64bd425bc926d5d158cdcf3cdef0b71db0235a99
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-06-06 09:31:25 +0200

    drop rinkeby

--- extension/src/chains.ts
+++ extension/src/chains.ts
@@ -2,7 +2,6 @@
 
 export const RPC = {
   1: 'https://mainnet.infura.io/v3/b81b456501e34bed8a85a3c2ff8f4577',
-  4: 'https://rinkeby.infura.io/v3/b81b456501e34bed8a85a3c2ff8f4577',
   5: 'https://goerli.infura.io/v3/b81b456501e34bed8a85a3c2ff8f4577',
   10: 'https://mainnet.optimism.io',
   56: 'https://bsc-dataseed.binance.org',
@@ -21,7 +20,6 @@ export type ChainId = keyof typeof RPC
 
 export const EXPLORER_URL: Record<ChainId, string> = {
   1: 'https://etherscan.io',
-  4: 'https://rinkeby.etherscan.io',
   5: 'https://goerli.etherscan.io',
   10: 'https://optimistic.etherscan.io',
   56: 'https://bscscan.com',
@@ -38,7 +36,6 @@ export const EXPLORER_URL: Record<ChainId, string> = {
 
 export const EXPLORER_API_URL: Record<ChainId, string> = {
   1: 'https://api.etherscan.io/api',
-  4: 'https://api-rinkeby.etherscan.io/api',
   5: 'https://api-goerli.etherscan.io/api',
   10: 'https://api-optimistic.etherscan.io/api',
   56: 'https://api.bscscan.com/api',
@@ -55,7 +52,6 @@ export const EXPLORER_API_URL: Record<ChainId, string> = {
 
 export const EXPLORER_API_KEY: Record<ChainId, string> = {
   1: 'N53BKW6ABNX7CNUK8QIXGRAQS2NME92YAN',
-  4: 'N53BKW6ABNX7CNUK8QIXGRAQS2NME92YAN',
   5: 'N53BKW6ABNX7CNUK8QIXGRAQS2NME92YAN',
   10: '',
   56: '',
@@ -72,7 +68,6 @@ export const EXPLORER_API_KEY: Record<ChainId, string> = {
 
 export const CHAIN_PREFIX: Record<ChainId, string> = {
   1: 'eth',
-  4: 'rin',
   5: 'gor',
   10: 'oeth',
   56: 'bsc',
@@ -89,7 +84,6 @@ export const CHAIN_PREFIX: Record<ChainId, string> = {
 
 export const CHAIN_CURRENCY: Record<ChainId, string> = {
   1: 'ETH',
-  4: 'ETH',
   5: 'ETH',
   56: 'BNB',
   10: 'ETH',
@@ -106,7 +100,6 @@ export const CHAIN_CURRENCY: Record<ChainId, string> = {
 
 export const CHAIN_NAME: Record<ChainId, string> = {
   1: 'Ethereum',
-  4: 'Rinkeby',
   5: 'Görli',
   56: 'Binance Smart Chain',
   10: 'Optimism',
