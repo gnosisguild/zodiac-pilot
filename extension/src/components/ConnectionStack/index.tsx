e27cd4a8fd850ff98b7321d3e88529257371cf1a
Colin Spence <udappia@gmail.com>
2024-08-13 15:07:38 -0600

    WIP: Adds overflowing connection mods

--- extension/src/components/ConnectionStack/index.tsx
+++ extension/src/components/ConnectionStack/index.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { LegacyConnection } from '../../types'
 import Address from '../Address'
 import Box from '../Box'
+import Flex from '../Flex'
 
 import classes from './style.module.css'
 
@@ -36,23 +37,59 @@ const ConnectionStack: React.FC<Props> = ({
           <img src={PUBLIC_PATH + circleIcon} alt="Dot Icon" />
         </div>
       </Box>
-      {moduleAddress && (
-        <div
-          className={cn([
-            classes.routeModContainer,
-            classes.grow,
-            addressBoxClass,
-          ])}
-        >
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}>
-            <p>1 connections</p>
-            <p className={cn([classes.routesLink])}>View connections</p>
+      {/* To Do: Hook up with real data */}
+      {/* {moduleAddress && ( */}
+      {true && (
+        <Flex gap={0} style={{flexGrow: 1, overflow: "hidden", position: 'relative'}}>
+          <p className={classes.connectionsLabel}>4 connections</p>
+          <div
+            className={cn([
+              classes.routeModContainer,
+              classes.grow,
+              addressBoxClass,
+              ])}
+          >
+
+              <Flex className={classes.connectionsContainer} gap={1} alignItems='end' style={{flexGrow: 1}} >
+                <Box className={cn([classes.routeStackContainer, addressBoxClass])}>
+                  <Flex gap={1} direction="column">
+                    <p style={{ textAlign: 'start', lineHeight: 1 }}>{"Connection #1"}</p>
+                    <Address address={avatarAddress} />
+                    <div className={classes.routeModArrowContainer}>
+                      <img src={PUBLIC_PATH + circleIcon} alt="Dot Icon" />
+                    </div>
+                  </Flex>
+                </Box>
+                <Box className={cn([classes.routeStackContainer, addressBoxClass])}>
+                  <Flex gap={1} direction="column">
+                    <p style={{ textAlign: 'start', lineHeight: 1 }}>{"Connection #2"}</p>
+                    <Address address={avatarAddress} />
+                    <div className={classes.routeModArrowContainer}>
+                      <img src={PUBLIC_PATH + circleIcon} alt="Dot Icon" />
+                    </div>
+                  </Flex>
+                </Box>
+                <Box className={cn([classes.routeStackContainer, addressBoxClass])}>
+                  <Flex gap={1} direction="column">
+                    <p style={{ textAlign: 'start', lineHeight: 1 }}>{"Connection #3"}</p>
+                    <Address address={avatarAddress} />
+                    <div className={classes.routeModArrowContainer}>
+                      <img src={PUBLIC_PATH + circleIcon} alt="Dot Icon" />
+                    </div>
+                  </Flex>
+                </Box>
+                <Box className={cn([classes.routeStackContainer, addressBoxClass])}>
+                  <Flex gap={1} direction="column">
+                    <p style={{ textAlign: 'start', lineHeight: 1 }}>{"Connection #4"}</p>
+                    <Address address={avatarAddress} />
+                    <div className={classes.routeModArrowContainer}>
+                      <img src={PUBLIC_PATH + circleIcon} alt="Dot Icon" />
+                    </div>
+                  </Flex>
+                </Box>
+              </Flex>
           </div>
-          <BlockieStack addresses={[moduleAddress]} />
-          <div className={classes.routeModArrowContainer}>
-            <img src={PUBLIC_PATH + circleIcon} alt="Dot Icon" />
-          </div>
-        </div>
+      </Flex>
       )}
       <Box className={cn([classes.routeStackContainer, addressBoxClass])}>
         <RouteBadgeIcon badgeType="target" label="Target Safe" />
