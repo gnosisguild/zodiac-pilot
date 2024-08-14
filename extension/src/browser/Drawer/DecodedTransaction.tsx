77a5f399582cbc1ce7bc89c164d2053acd14dcd1
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-08-14 12:00:39 +0200

    cleanup

--- extension/src/browser/Drawer/DecodedTransaction.tsx
+++ extension/src/browser/Drawer/DecodedTransaction.tsx
@@ -8,26 +8,23 @@ interface Props {
   functionFragment: FunctionFragment
   data: Result
 }
-const DecodedTransaction: React.FC<Props> = ({ functionFragment, data }) => {
-  console.log(data, functionFragment.inputs)
-  return (
-    <div className={classes.transaction}>
-      {functionFragment.inputs.length > 0 && (
-        <fieldset>
-          {functionFragment.inputs.map((input, i) => (
-            <label key={input.name}>
-              <span>
-                {input.name} <i className={classes.inputType}>{input.type}</i>
-              </span>
-              <Box p={1} bg>
-                <input type="text" value={data[i].toString()} readOnly />
-              </Box>
-            </label>
-          ))}
-        </fieldset>
-      )}
-    </div>
-  )
-}
+const DecodedTransaction: React.FC<Props> = ({ functionFragment, data }) => (
+  <div className={classes.transaction}>
+    {functionFragment.inputs.length > 0 && (
+      <fieldset>
+        {functionFragment.inputs.map((input, i) => (
+          <label key={input.name}>
+            <span>
+              {input.name} <i className={classes.inputType}>{input.type}</i>
+            </span>
+            <Box p={1} bg>
+              <input type="text" value={data[i].toString()} readOnly />
+            </Box>
+          </label>
+        ))}
+      </fieldset>
+    )}
+  </div>
+)
 
 export default DecodedTransaction
