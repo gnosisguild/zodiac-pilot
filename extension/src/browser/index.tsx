8de9bc4ae9497b60473d6da1a5d390413c30ddc6
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-08-20 11:29:09 +0200

    per tab panel toggling

--- extension/src/browser/index.tsx
+++ extension/src/browser/index.tsx
@@ -23,47 +23,9 @@ const useNoPageScroll = () => {
 }
 
 const Browser: React.FC = () => {
-  const location = useUrl()
-  const pushConnectionsRoute = usePushConnectionsRoute()
-
-  useNoPageScroll()
-
-  const handleOpenConnectionsDrawer = () => {
-    pushConnectionsRoute('')
-  }
-
-  // When the user browses in the iframe the location will update constantly.
-  // This must not trigger an update of the iframe's src prop, though, since that would rerender the iframe.
-  const [initialLocation, setInitialLocation] = useState(location)
 
   return (
-    <Layout
-      navBox={<UrlInput onSubmit={setInitialLocation} />}
-      navFullWidth
-      headerRight={
-        <ConnectionBubble onConnectionsClick={handleOpenConnectionsDrawer} />
-      }
-    >
-      <Box className={classNames.frame} double p={2}>
-        {initialLocation ? (
-          <BrowserFrame src={initialLocation} />
-        ) : (
-          <div className={classes.launchPage}>
-            <Box p={3} double>
-              <h2>Choose an app to get started</h2>
-              <AppPicker
-                large
-                onPick={(url) => {
-                  pushLocation(url)
-                  setInitialLocation(url)
-                }}
-              />
-            </Box>
-          </div>
-        )}
-      </Box>
-      <Drawer />
-    </Layout>
+     <Drawer />
   )
 }
 
