9f91efd1b1db3534589cea336520e6bf12a0971d
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-09-09 17:31:15 +0200

    make rpc intercept work

--- extension/src/background/sessionTracking.ts
+++ extension/src/background/sessionTracking.ts
@@ -1,10 +1,4 @@
-import {
-  Message,
-  PILOT_PANEL_CLOSED,
-  PILOT_PANEL_OPENED,
-  SIMULATE_START,
-  SIMULATE_STOP,
-} from '../messages'
+import { Message, SIMULATE_START, SIMULATE_STOP } from '../messages'
 import { updateRpcRedirectRules } from './rpcRedirect'
 import { startTrackingTab, stopTrackingTab } from './tabsTracking'
 import { PilotSession } from './types'
@@ -32,6 +26,7 @@ export const stopPilotSession = (windowId: number) => {
   activePilotSessions.delete(windowId)
   // make sure all rpc redirects are cleared
   updateRpcRedirectRules(activePilotSessions)
+  updateSimulatingBadge(windowId)
 }
 
 // track when a Pilot session is started for a window and when the simulation is started/stopped
@@ -39,38 +34,44 @@ chrome.runtime.onMessage.addListener((message: Message, sender) => {
   // ignore messages that don't come from the extension itself
   if (sender.id !== chrome.runtime.id) return
 
-  // TODO
-  // if (message.type === SIMULATE_START) {
-  //   const { networkId, rpcUrl } = message
-  //   const session = activePilotSessions.get(message.windowId)
-  //   if (!session) {
-  //     throw new Error(`Pilot session not found for window #${message.windowId}`)
-  //   }
-  //   session.fork = { networkId, rpcUrl }
-  //   updateRpcRedirectRules(activePilotSessions)
-  //   console.debug(
-  //     `start intercepting JSON RPC requests in window #${message.windowId}`,
-  //     session.fork
-  //   )
+  if (message.type === SIMULATE_START) {
+    const { networkId, rpcUrl } = message
+    const session = activePilotSessions.get(message.windowId)
+    if (!session) {
+      throw new Error(`Pilot session not found for window #${message.windowId}`)
+    }
+    session.fork = { networkId, rpcUrl }
+    updateRpcRedirectRules(activePilotSessions)
+    console.debug(
+      `start intercepting JSON RPC requests in window #${message.windowId}`,
+      session.fork
+    )
+    updateSimulatingBadge(message.windowId)
+  }
 
-  //   // TODO set status badge if a simulation is active
-  //   // chrome.action.setBadgeBackgroundColor(
-  //   //   { color: [0, 255, 0, 0] }, // Green
-  //   //   () => {
-  //   //     /* ... */
-  //   //   }
-  //   // )
-  // }
-
-  // if (message.type === SIMULATE_STOP) {
-  //   const session = activePilotSessions.get(message.windowId)
-  //   if (!session) {
-  //     throw new Error(`Pilot session not found for window #${message.windowId}`)
-  //   }
-  //   session.fork = null
-  //   updateRpcRedirectRules(activePilotSessions)
-  //   console.debug(
-  //     `stop intercepting JSON RPC requests in window #${message.windowId}`
-  //   )
-  // }
+  if (message.type === SIMULATE_STOP) {
+    const session = activePilotSessions.get(message.windowId)
+    if (!session) {
+      throw new Error(`Pilot session not found for window #${message.windowId}`)
+    }
+    session.fork = null
+    updateRpcRedirectRules(activePilotSessions)
+    console.debug(
+      `stop intercepting JSON RPC requests in window #${message.windowId}`
+    )
+    updateSimulatingBadge(message.windowId)
+  }
 })
+
+export const updateSimulatingBadge = (windowId: number) => {
+  const isSimulating = !!activePilotSessions.get(windowId)?.fork
+  chrome.tabs.query({ windowId }, (tabs) => {
+    for (const tab of tabs) {
+      // TODO use a different icon while simulating to make this more beautiful
+      chrome.action.setBadgeText({
+        text: isSimulating ? '🟢' : '',
+        tabId: tab.id,
+      })
+    }
+  })
+}
