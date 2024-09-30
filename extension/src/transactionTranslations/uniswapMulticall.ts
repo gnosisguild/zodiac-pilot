a7fdddf494e4f4203634260ea2397e0353662a06
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-09-30 09:35:18 +0200

    uniswap multicall: don't unfold batches with value > 0

--- extension/src/transactionTranslations/uniswapMulticall.ts
+++ extension/src/transactionTranslations/uniswapMulticall.ts
@@ -15,7 +15,14 @@ export default {
   recommendedFor: [KnownContracts.ROLES_V1, KnownContracts.ROLES_V2],
 
   translate: async (transaction) => {
-    if (!transaction.data) {
+    const { to, data, value } = transaction
+
+    if (!data) {
+      return undefined
+    }
+
+    if (parseInt(value) > 0) {
+      // We don't support unfolding of transactions with value since it's hard to tell which individual calls are supposed to receive the value
       return undefined
     }
 
@@ -25,7 +32,7 @@ export default {
       try {
         functionCalls = uniswapMulticallInterface.decodeFunctionData(
           fragment as FunctionFragment,
-          transaction.data
+          data
         ).data as string[]
         break
       } catch (e) {
@@ -37,6 +44,6 @@ export default {
       return undefined
     }
 
-    return functionCalls.map((data) => ({ ...transaction, data }))
+    return functionCalls.map((data) => ({ to, data, value: '0' }))
   },
 } satisfies TransactionTranslation
