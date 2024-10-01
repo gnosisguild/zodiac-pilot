0deb1d7792a1ca93091fff14f114c37a376f79c9
Philipp Giese <giese.philipp+git@gmail.com>
2024-10-01 17:43:03 +0200

    make it possible to disconnect the account

--- example-app/app/transfer/Transfer.tsx
+++ example-app/app/transfer/Transfer.tsx
@@ -1,11 +1,12 @@
 import { ConnectKitButton } from 'connectkit'
-import { useAccount } from 'wagmi'
+import { useAccount, useDisconnect } from 'wagmi'
 import { Balance } from './Balance'
 
 export const Transfer = () => {
   const account = useAccount()
+  const { disconnect } = useDisconnect()
 
-  if (account.isDisconnected) {
+  if (account.isDisconnected || account.isConnecting) {
     return <ConnectKitButton />
   }
 
@@ -13,11 +14,18 @@ export const Transfer = () => {
     <dl>
       <dt className="font-semibold">Account</dt>
       <dd className="flex items-center gap-2">
-        {account.isConnected ? account.address : 'Connecting...'}
+        {account.address}
 
         <span className="rounded-sm bg-gray-200 px-1 text-xs font-bold uppercase tabular-nums text-gray-500">
           {account.chain.name}
         </span>
+
+        <button
+          className="rounded bg-red-50 px-2 text-red-500 outline-none transition-colors hover:bg-red-100"
+          onClick={() => disconnect()}
+        >
+          Disconnect
+        </button>
       </dd>
 
       <dt className="font-semibold">Balance</dt>
