e33bac9bc7b882198f08157082ea4f863d5e666d
Philipp Giese <giese.philipp+git@gmail.com>
2024-10-01 17:52:02 +0200

    list all addresses in an account

--- example-app/app/transfer/Transfer.tsx
+++ example-app/app/transfer/Transfer.tsx
@@ -1,4 +1,5 @@
 import { ConnectKitButton } from 'connectkit'
+import { Fragment } from 'react/jsx-runtime'
 import { useAccount, useDisconnect } from 'wagmi'
 import { Balance } from './Balance'
 
@@ -12,26 +13,26 @@ export const Transfer = () => {
 
   return (
     <dl>
-      <dt className="font-semibold">Account</dt>
-      <dd className="flex items-center gap-2">
-        {account.address}
+      {account.addresses.map((address) => (
+        <Fragment key={address}>
+          <dt className="font-semibold">Account</dt>
+          <dd className="flex items-center gap-2">
+            {account.address}
 
-        <span className="rounded-sm bg-gray-200 px-1 text-xs font-bold uppercase tabular-nums text-gray-500">
-          {account.chain.name}
-        </span>
+            <button
+              className="rounded bg-red-50 px-2 text-red-500 outline-none transition-colors hover:bg-red-100"
+              onClick={() => disconnect()}
+            >
+              Disconnect
+            </button>
+          </dd>
 
-        <button
-          className="rounded bg-red-50 px-2 text-red-500 outline-none transition-colors hover:bg-red-100"
-          onClick={() => disconnect()}
-        >
-          Disconnect
-        </button>
-      </dd>
-
-      <dt className="font-semibold">Balance</dt>
-      <dd>
-        <Balance contract={account.address} />
-      </dd>
+          <dt className="font-semibold">Balance</dt>
+          <dd>
+            <Balance contract={account.address} />
+          </dd>
+        </Fragment>
+      ))}
     </dl>
   )
 }
