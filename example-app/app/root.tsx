cddb9d0f29aba8f70f23323b7396c9f360123916
Philipp Giese <giese.philipp+git@gmail.com>
2024-10-02 09:46:41 +0200

    add invariant

--- example-app/app/root.tsx
+++ example-app/app/root.tsx
@@ -1,4 +1,6 @@
-import { json, Links, Meta, Scripts, useLoaderData } from '@remix-run/react'
+import { invariantResponse } from '@epic-web/invariant'
+import { json } from '@remix-run/node'
+import { Links, Meta, Scripts, useLoaderData } from '@remix-run/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ConnectKitProvider } from 'connectkit'
 import { WagmiProvider } from 'wagmi'
@@ -7,12 +9,25 @@ import { Connect, Connected } from './Connect'
 import './tailwind.css'
 import { Transfer } from './transfer'
 
-export const loader = () => json({ projectId: process.env.PROJECT_ID })
+const getProjectId = () => {
+  const { PROJECT_ID } = process.env
+
+  invariantResponse(PROJECT_ID, '"PROJECT_ID" environment variable not found')
+
+  return PROJECT_ID
+}
+
+export const loader = () => {
+  const projectId = getProjectId()
+
+  return json({ projectId })
+}
 
 export const queryClient = new QueryClient()
 
 export default function App() {
   const { projectId } = useLoaderData<typeof loader>()
+  const wagmiConfig = config(projectId)
 
   return (
     <html className="h-full w-full">
@@ -22,7 +37,7 @@ export default function App() {
         <Links />
       </head>
       <body className="flex h-full w-full flex-col items-center justify-center gap-8 text-sm">
-        <WagmiProvider config={config(projectId)}>
+        <WagmiProvider config={wagmiConfig}>
           <QueryClientProvider client={queryClient}>
             <ConnectKitProvider>
               <h1 className="text-2xl font-bold">
