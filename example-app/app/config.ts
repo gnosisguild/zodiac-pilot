b255003fb81b8c3f863cd3da2bb4b04cfb26a73b
Philipp Giese <giese.philipp+git@gmail.com>
2024-10-07 13:31:46 +0200

    add anvil config

--- example-app/app/config.ts
+++ example-app/app/config.ts
@@ -1,15 +1,16 @@
-import { getDefaultConfig } from "connectkit";
-import { createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { getDefaultConfig } from 'connectkit'
+import { createConfig, http } from 'wagmi'
+import { anvil, mainnet } from 'wagmi/chains'
 
 export const config = (projectId: string) =>
   createConfig(
     getDefaultConfig({
-      appName: "Zodiac Pilot Example App",
+      appName: 'Zodiac Pilot Example App',
       walletConnectProjectId: projectId,
-      chains: [mainnet],
+      chains: [mainnet, anvil],
       transports: {
         [mainnet.id]: http(),
+        [anvil.id]: http('http://127.0.0.1:8545'),
       },
-    })
-  );
+    }),
+  )
