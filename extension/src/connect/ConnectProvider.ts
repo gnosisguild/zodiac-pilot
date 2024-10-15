46fc6f9be461fe23c2330bc2b9e61cfda0bbcecd
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2024-10-15 16:13:49 +0200

    fix: sidepanel wallet connect  (#163)
    * update setup
    
    * start rewriting connect logic to always use current tab
    
    * rewrite connect provider to use currently active tab
    
    * only render pilot when a connection to the current tab is established
    
    * split up components
    
    * do not require full react import in every file
    
    * improve tab update handling
    
    * updating to an unsupported url tears down the port
    
    * consistent style
    
    * clear urls from cache when disconnecting
    
    * do not generate request ids from stale data

--- extension/src/connect/ConnectProvider.ts
+++ extension/src/connect/ConnectProvider.ts
@@ -9,6 +9,7 @@ import {
   CONNECTED_WALLET_REQUEST,
   CONNECTED_WALLET_RESPONSE,
 } from './messages'
+import { invariant } from '@epic-web/invariant'
 
 interface JsonRpcRequest {
   method: string
@@ -25,165 +26,335 @@ class InjectedWalletError extends Error {
 
 let instanceCounter = 0
 
+const tabInfo = new Map<number, string | undefined>()
+
 export default class ConnectProvider
   extends EventEmitter
   implements Eip1193Provider
 {
-  private portPromise: Promise<chrome.runtime.Port>
+  private port: chrome.runtime.Port | null = null
 
-  private initialized = false
   private instanceId = instanceCounter++
   private messageCounter = 0
 
-  // private emittedEventIds = new Set<string>()
-
   constructor() {
     super()
-    this.portPromise = this.#connect()
-  }
 
-  #connect = () => {
-    return new Promise<chrome.runtime.Port>((resolve) => {
-      chrome.tabs.query({ currentWindow: true }, async (tabs) => {
-        const trackedTabs = tabs.filter(
-          (tab) =>
-            tab.id &&
-            tab.url &&
-            !tab.url.startsWith('chrome:') &&
-            !tab.url.startsWith('about:')
-        )
-        // const activeTab = trackedTabs.find((tab) => tab.active)
-        // const otherTabs = trackedTabs.filter((tab) => tab !== activeTab)
-        // console.log({ trackedTabs, activeTab, otherTabs })
+    chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+      const tab = await chrome.tabs.get(tabId)
 
-        // Sequentially try to connect to each tab that potentially has a connect iframe
-        for (const tab of trackedTabs) {
-          console.log(tab, { ...tab })
-          try {
-            const port = await this.#connectToTab(tab.id!)
-            console.log('Connected to tab', tab.id)
-            port.onMessage.addListener(this.#handleEventMessage)
-            port.onDisconnect.addListener(() => {
-              this.initialized = false
-              console.log(
-                'Disconnected from connect iframe, reconnecting to a different tab...'
-              )
-              this.#connect()
-            })
-            resolve(port)
-            return
-          } catch (error) {
-            console.debug(
-              `could not connect to tab #${tab.id}, trying next tab...`,
-              error
-            )
-          }
-        }
+      console.debug(`Tab (id: "${tabId}", url: "${tab.url}") became active.`)
 
-        if (trackedTabs.length === 0) {
-          // There's no open tab with a connect iframe
-          // TODO handle this by opening a new tab with a dummy page (could show some help text saying "at least one tab must be open")
-          throw new Error('No tab with a connect iframe available')
+      const port = await handleActiveTab(tab)
+
+      if (port != null) {
+        this.setupPort(tabId, port)
+      } else {
+        this.tearDownPort(tabId)
+      }
+    })
+
+    chrome.tabs.onUpdated.addListener(async (tabId) => {
+      const currentTab = await getCurrentTab()
+
+      if (currentTab == null) {
+        return
+      }
+
+      if (currentTab.id !== tabId) {
+        return
+      }
+
+      console.debug(
+        `Tab (id: "${tabId}", url: "${currentTab.url}") has been updated.`
+      )
+
+      const url = tabInfo.get(tabId)
+
+      if (url === currentTab.url) {
+        if (isValidTab(url)) {
+          console.debug(
+            `Tab (id: "${tabId}", url: "${currentTab.url}") has no changes that require a new port.`
+          )
         } else {
-          // retry after timeout to wait on connect content script to be injected into currently reloading tabs
-          window.setTimeout(() => {
-            resolve(this.#connect())
-          }, 100)
+          console.debug(
+            `Tab (id: "${tabId}", url: "${currentTab.url}") cannot be used.`
+          )
+
+          this.tearDownPort(tabId)
         }
-      })
+
+        return
+      }
+
+      console.debug(
+        `Tab (id: "${tabId}") updated URL from "${url}" to "${currentTab.url}".`
+      )
+
+      tabInfo.set(tabId, currentTab.url)
+
+      const port = await handleActiveTab(currentTab)
+
+      if (port != null) {
+        this.setupPort(tabId, port)
+      }
+    })
+
+    getCurrentTab().then(async (tab) => {
+      if (tab == null || tab.id == null) {
+        return
+      }
+
+      const port = await handleActiveTab(tab)
+
+      if (port != null) {
+        this.setupPort(tab.id, port)
+      }
     })
   }
 
-  #connectToTab = (tabId: number) => {
-    return new Promise<chrome.runtime.Port>((resolve, reject) => {
-      const potentialPort = chrome.tabs.connect(tabId, {
-        name: 'connect',
-      })
+  async setupPort(tabId: number, port: chrome.runtime.Port) {
+    this.tearDownPort(tabId)
 
-      // if the port connection cannot be established, this will trigger a disconnect event
-      const handleDisconnect = () => {
-        reject(new Error('No response from connect iframe'))
-      }
-      potentialPort.onDisconnect.addListener(handleDisconnect)
+    console.debug('Connecting new port.')
 
-      // if we receive the CONNECTED_WALLET_INITIALIZED message, we resolve the promise to this port
-      const handleInitMessage = (message: Message) => {
-        if (message.type === CONNECTED_WALLET_INITIALIZED) {
-          potentialPort.onDisconnect.removeListener(handleDisconnect)
-          potentialPort.onMessage.removeListener(handleInitMessage)
-          resolve(potentialPort)
-        }
+    port.onMessage.addListener(this.#handleEventMessage)
+
+    this.port = port
+
+    this.emit('readyChanged', true)
+  }
+
+  tearDownPort(tabId: number) {
+    tabInfo.delete(tabId)
+
+    if (this.port == null) {
+      return
+    }
+
+    console.debug('Disconnecting current port')
+
+    this.port.disconnect()
+    this.port = null
+
+    this.emit('readyChanged', false)
+  }
+
+  waitForPort(maxWait: number = 1000, waited: number = 0) {
+    const waitTime = 10
+
+    if (waited > maxWait) {
+      return Promise.reject(`Port did not open in time. Waited ${maxWait}ms.`)
+    }
+
+    return new Promise<void>((resolve) => {
+      if (this.port != null) {
+        resolve()
+      } else {
+        sleep(waitTime)
+          .then(() => this.waitForPort(maxWait, waited + waitTime))
+          .then(resolve)
       }
-      potentialPort.onMessage.addListener(handleInitMessage)
     })
   }
 
-  isInitialized = (): boolean => {
-    return this.initialized
+  getPort() {
+    invariant(this.port != null, 'Port has not been initialized')
+
+    return this.port
   }
 
-  request = async (request: JsonRpcRequest) => {
+  async request(request: JsonRpcRequest): Promise<any> {
     const requestId = `${this.instanceId}:${this.messageCounter}`
     this.messageCounter++
 
-    const responseMessage: Message = await this.#sendRequestToConnectIframe(
-      requestId,
-      request
-    )
+    await this.waitForPort()
 
-    if (responseMessage.type === CONNECTED_WALLET_RESPONSE) {
-      return responseMessage.response
-    } else if (responseMessage.type === CONNECTED_WALLET_ERROR) {
-      const error = new InjectedWalletError(
-        responseMessage.error.message,
-        responseMessage.error.code
-      )
-      throw error
-    } else {
+    try {
+      const responseMessage = await sendRequestToConnectIframe({
+        port: this.getPort(),
+        requestId,
+        request,
+      })
+
+      if (responseMessage.type === CONNECTED_WALLET_RESPONSE) {
+        return responseMessage.response
+      }
+
+      if (responseMessage.type === CONNECTED_WALLET_ERROR) {
+        const error = new InjectedWalletError(
+          responseMessage.error.message,
+          responseMessage.error.code
+        )
+
+        throw error
+      }
+
       console.error('Unexpected response', responseMessage)
-      throw new Error('Unexpected response')
+
+      throw new Error('Unexpected response', { cause: responseMessage })
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        error.message === 'Attempting to use a disconnected port object'
+      ) {
+        // the port was closed in the meantime, we reconnect and resend...
+
+        return this.request(request)
+      }
+
+      throw error
     }
   }
 
-  #sendRequestToConnectIframe = async (
-    requestId: string,
-    request: JsonRpcRequest
-  ): Promise<Message> => {
-    const port = await this.portPromise
-    return new Promise((resolve) => {
-      const handleResponse = (message: Message) => {
-        if (
-          message.type !== CONNECTED_WALLET_RESPONSE &&
-          message.type !== CONNECTED_WALLET_ERROR
-        ) {
-          return
-        }
-        if (message.requestId !== requestId) return
-        port.onMessage.removeListener(handleResponse)
-        resolve(message)
-      }
-      port.onMessage.addListener(handleResponse)
-      try {
-        port.postMessage({
-          type: CONNECTED_WALLET_REQUEST,
-          requestId,
-          request,
-        })
-      } catch (error: any) {
-        if (error.message === 'Attempting to use a disconnected port object') {
-          // the port was closed in the meantime, we reconnect and resend...
-          this.portPromise = this.#connect()
-          this.#sendRequestToConnectIframe(requestId, request)
-        } else {
-          console.error('Error sending message to connect iframe', error)
-        }
-      }
-    })
-  }
-
-  #handleEventMessage = (message: Message) => {
+  #handleEventMessage(message: Message) {
     if (message.type === CONNECTED_WALLET_EVENT) {
       this.emit(message.eventName, message.eventData)
     }
   }
+}
+
+const isValidTab = (url: string | undefined) =>
+  url != null && url !== '' && isValidProtocol(url)
+
+const isValidProtocol = (url: string) =>
+  ['chrome:', 'about:'].every((protocol) => !url.startsWith(protocol))
+
+const createPort = (tabId: number, url: string | undefined) => {
+  if (!isValidTab(url)) {
+    console.debug(
+      `Tab (id: "${tabId}", url: "${url}") does not meet connect criteria.`
+    )
+
+    return Promise.resolve(null)
+  }
+
+  return new Promise<chrome.runtime.Port>((resolve, reject) => {
+    const port = chrome.tabs.connect(tabId, {
+      name: 'connect',
+    })
+
+    // if the port connection cannot be established, this will trigger a disconnect event
+    const handleDisconnect = () => {
+      reject(new Error('No response from connect iframe'))
+    }
+    port.onDisconnect.addListener(handleDisconnect)
+
+    // if we receive the CONNECTED_WALLET_INITIALIZED message, we resolve the promise to this port
+    const handleInitMessage = (message: Message) => {
+      if (message.type !== CONNECTED_WALLET_INITIALIZED) {
+        return
+      }
+
+      port.onDisconnect.removeListener(handleDisconnect)
+      port.onMessage.removeListener(handleInitMessage)
+
+      console.debug(`Tab (id: "${tabId}") port created.`)
+
+      resolve(port)
+    }
+
+    port.onMessage.addListener(handleInitMessage)
+  })
+}
+
+type SendRequestToConnectIFrameOptions = {
+  port: chrome.runtime.Port
+  requestId: string
+  request: JsonRpcRequest
+}
+
+const sendRequestToConnectIframe = async ({
+  port,
+  request,
+  requestId,
+}: SendRequestToConnectIFrameOptions): Promise<Message> =>
+  new Promise((resolve) => {
+    const handleResponse = (message: Message) => {
+      if (
+        message.type !== CONNECTED_WALLET_RESPONSE &&
+        message.type !== CONNECTED_WALLET_ERROR
+      ) {
+        return
+      }
+
+      if (message.requestId !== requestId) {
+        return
+      }
+
+      port.onMessage.removeListener(handleResponse)
+
+      resolve(message)
+    }
+
+    port.onMessage.addListener(handleResponse)
+
+    port.postMessage({
+      type: CONNECTED_WALLET_REQUEST,
+      requestId,
+      request,
+    })
+  })
+
+const handleActiveTab = async (tab: chrome.tabs.Tab) => {
+  return new Promise<chrome.runtime.Port | null>((resolve) => {
+    const handleTabWhenReady = async (
+      tabId: number,
+      info: chrome.tabs.TabChangeInfo
+    ) => {
+      const url = info.url || tabInfo.get(tabId)
+
+      tabInfo.set(tabId, url)
+
+      if (tab.id === tabId && info.status === 'complete') {
+        console.debug(
+          `Tab (id: "${tab.id}", url: "${url}") became ready. Trying to open port.`
+        )
+
+        const port = await createPort(tabId, url)
+
+        if (port != null) {
+          chrome.tabs.onUpdated.removeListener(handleTabWhenReady)
+
+          console.debug(`Port to Tab (id: "${tab.id}", url: "${url}") created.`)
+
+          resolve(port)
+        }
+      }
+    }
+
+    if (tab.id != null && tab.status === 'complete') {
+      console.debug(
+        `Tab (id: "${tab.id}", url: "${tab.url}") was already loaded. Trying to create port.`
+      )
+
+      createPort(tab.id, tab.url).then((port) => {
+        if (port != null) {
+          console.debug(
+            `Port to Tab (id: "${tab.id}", url: "${tab.url}") created.`
+          )
+
+          resolve(port)
+        } else {
+          resolve(null)
+        }
+      })
+    } else {
+      console.debug(`Tab (id: "${tab.id}", url: "${tab.url}") NOT ready.`)
+
+      chrome.tabs.onUpdated.addListener(handleTabWhenReady)
+    }
+  })
+}
+
+const sleep = (time: number) =>
+  new Promise<void>((resolve) => setTimeout(resolve, time))
+
+const getCurrentTab = async () => {
+  const [currentTab] = await chrome.tabs.query({
+    currentWindow: true,
+    active: true,
+  })
+
+  return currentTab
 }
--- extension/src/panel/providers/useInjectedWallet.tsx
+++ extension/src/panel/providers/useInjectedWallet.tsx
@@ -1,21 +1,10 @@
-import React, {
-  ReactNode,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react'
-import { toast } from 'react-toastify'
+import React, { ReactNode, useContext, useMemo } from 'react'
 import { ChainId } from 'ser-kit'
-
-import { EXPLORER_URL, CHAIN_CURRENCY, CHAIN_NAME, RPC } from '../../chains'
 import { Eip1193Provider } from '../../types'
-import { BrowserProvider } from 'ethers'
-import ConnectProvider from '../../connect/ConnectProvider'
+import { useConnectProvider } from './useConnectProvider'
 
 export interface InjectedWalletContextT {
-  provider: Eip1193Provider | undefined
+  provider: Eip1193Provider
   connect: () => Promise<{ chainId: number; accounts: string[] }>
   switchChain: (chainId: ChainId) => Promise<void>
   accounts: string[]
@@ -24,82 +13,32 @@ export interface InjectedWalletContextT {
 const InjectedWalletContext =
   React.createContext<InjectedWalletContextT | null>(null)
 
-// Wallet extensions won't inject connectProvider to the extension panel, so we've built ConnectProvider.
-// connectProvider can be used just like window.ethereum
-const connectProvider = new ConnectProvider()
-
 export const ProvideInjectedWallet: React.FC<{
   children: ReactNode
 }> = ({ children }) => {
-  const [accounts, setAccounts] = useState<string[]>([])
-  const [chainId, setChainId] = useState<number | null>(null)
-
-  useEffect(() => {
-    let canceled = false
-    const ifNotCanceled =
-      <Args extends any[]>(callback: (...operationParameters: Args) => void) =>
-      (...args: Args) => {
-        if (!canceled) {
-          callback(...args)
-        }
-      }
-
-    const handleAccountsChanged = ifNotCanceled((accounts: string[]) => {
-      console.log(`InjectedWallet accounts changed to ${accounts}`)
-      setAccounts(accounts)
-    })
-
-    const handleChainChanged = ifNotCanceled((chainIdHex: string) => {
-      const chainId = parseInt(chainIdHex.slice(2), 16)
-      console.log(`InjectedWallet network changed to ${chainId}`)
-      setChainId(chainId)
-    })
-
-    const handleDisconnect = ifNotCanceled((error: Error) => {
-      console.warn(error)
-      setChainId(null)
-      setAccounts([])
-    })
-
-    connectProvider.on('accountsChanged', handleAccountsChanged)
-    connectProvider.on('chainChanged', handleChainChanged)
-    connectProvider.on('disconnect', handleDisconnect)
-
-    return () => {
-      if (!connectProvider) return
-      canceled = true
-      connectProvider.removeListener('accountsChanged', handleAccountsChanged)
-      connectProvider.removeListener('chainChanged', handleChainChanged)
-      connectProvider.removeListener('disconnect', handleDisconnect)
-    }
-  }, [])
-
-  const connect = useCallback(async () => {
-    const { accounts, chainId: chainIdBigInt } = await connectInjectedWallet()
-    const chainId = Number(chainIdBigInt)
-    setAccounts(accounts)
-    setChainId(chainId)
-    return { accounts, chainId }
-  }, [])
+  const { provider, connect, accounts, chainId, ready, switchChain } =
+    useConnectProvider()
 
   const packed = useMemo(
     () => ({
-      provider: connectProvider,
+      provider,
       connect,
       switchChain,
       accounts,
       chainId,
     }),
-    [accounts, connect, chainId]
+    [provider, connect, switchChain, accounts, chainId]
   )
 
-  console.log('InjectedWalletProvider PACKED', packed)
+  if (ready) {
+    return (
+      <InjectedWalletContext.Provider value={packed}>
+        {children}
+      </InjectedWalletContext.Provider>
+    )
+  }
 
-  return (
-    <InjectedWalletContext.Provider value={packed}>
-      {children}
-    </InjectedWalletContext.Provider>
-  )
+  return null
 }
 
 const useInjectedWallet = () => {
@@ -113,118 +52,3 @@ const useInjectedWallet = () => {
 }
 
 export default useInjectedWallet
-
-interface InjectedWalletError extends Error {
-  code: number
-}
-
-let pendingPromise: Promise<any> | null = null
-const memoWhilePending = <T extends (...args: any) => Promise<any>>(
-  callback: T
-): T =>
-  ((...args) => {
-    if (pendingPromise) return pendingPromise
-    pendingPromise = callback(...args)
-    pendingPromise.finally(() => {
-      pendingPromise = null
-    })
-    return pendingPromise
-  }) as T
-
-const connectInjectedWallet = memoWhilePending(async () => {
-  console.log('connecting...............')
-  const browserProvider = new BrowserProvider(connectProvider)
-
-  const accountsPromise = browserProvider
-    .send('eth_requestAccounts', [])
-    .catch((err: any) => {
-      console.log('error eth_requestAccounts', err)
-      if ((err as InjectedWalletError).code === -32002) {
-        return new Promise((resolve: (value: string[]) => void) => {
-          const toastId = toast.warn(
-            <>Check your wallet to confirm connection</>,
-            { autoClose: false }
-          )
-          const handleAccountsChanged = (accounts: string[]) => {
-            resolve(accounts)
-            toast.dismiss(toastId)
-            connectProvider?.removeListener(
-              'accountsChanged',
-              handleAccountsChanged
-            )
-          }
-          connectProvider?.on('accountsChanged', handleAccountsChanged)
-        })
-      }
-
-      throw err
-    })
-
-  const [accounts, chainId] = await Promise.all([
-    accountsPromise,
-    browserProvider.getNetwork().then((network) => network.chainId),
-  ])
-
-  console.log('connected!!!!!!!!!!!!!', { accounts, chainId })
-
-  return { accounts, chainId }
-})
-
-const switchChain = async (chainId: ChainId) => {
-  if (!connectProvider) throw new Error('InjectedWallet not found')
-
-  try {
-    await connectProvider.request({
-      method: 'wallet_switchEthereumChain',
-      params: [{ chainId: `0x${chainId.toString(16)}` }],
-    })
-  } catch (err) {
-    if ((err as InjectedWalletError).code === -32002) {
-      // another wallet_switchEthereumChain request is already pending
-      await new Promise((resolve: (value: void) => void) => {
-        const toastId = toast.warn(
-          <>Check your wallet to confirm switching the network</>,
-          { autoClose: false }
-        )
-        const handleChainChanged = () => {
-          resolve()
-          toast.dismiss(toastId)
-          connectProvider?.removeListener('chainChanged', handleChainChanged)
-        }
-        connectProvider?.on('chainChanged', handleChainChanged)
-      })
-      return
-    }
-
-    if ((err as InjectedWalletError).code === 4902) {
-      // the requested chain has not been added by InjectedWallet
-      await connectProvider.request({
-        method: 'wallet_addEthereumChain',
-        params: [
-          {
-            chainId: `0x${chainId.toString(16)}`,
-            chainName: CHAIN_NAME[chainId],
-            nativeCurrency: {
-              name: CHAIN_CURRENCY[chainId],
-              symbol: CHAIN_CURRENCY[chainId],
-              decimals: 18,
-            },
-            rpcUrls: [RPC[chainId]],
-            blockExplorerUrls: [EXPLORER_URL[chainId]],
-          },
-        ],
-      })
-    } else {
-      throw err
-    }
-  }
-
-  // wait for chain change event (InjectedWallet will emit this event only after some delay, so our state that reacts to this event would be out of sync if we don't wait here)
-  await new Promise((resolve: (value: void) => void) => {
-    const handleChainChanged = () => {
-      resolve()
-      connectProvider?.removeListener('chainChanged', handleChainChanged)
-    }
-    connectProvider?.on('chainChanged', handleChainChanged)
-  })
-}
