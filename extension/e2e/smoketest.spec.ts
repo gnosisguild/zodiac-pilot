295b995acb3aafaab4f866eff51cbe82c2cf43de
Philipp Giese <giese.philipp+git@gmail.com>
2024-10-22 17:23:14 +0200

    fix type error

--- extension/e2e/smoketest.spec.ts
+++ extension/e2e/smoketest.spec.ts
@@ -7,8 +7,8 @@ test('connection to example app', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'zodiac' })).toBeInViewport()
 })
 
-test('possibility to open the panel', async ({ page, extensionId }) => {
-  const extension = await loadExtension(page, extensionId)
+test('possibility to open the panel', async ({ page }) => {
+  const extension = await loadExtension(page)
 
   await expect(
     extension.getByRole('heading', {
