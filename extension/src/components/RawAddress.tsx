c23a1e297fd010be4ab505a34f01002cdd7eec6d
Philipp Giese <giese.philipp+git@gmail.com>
2024-10-22 17:23:08 +0200

    fix formatting

--- extension/src/components/RawAddress.tsx
+++ extension/src/components/RawAddress.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react'
 
 export const RawAddress = ({ children }: PropsWithChildren) => (
-  <code className="bg-zodiac-dark-mustard max-w-full overflow-hidden text-ellipsis text-nowrap rounded-md border border-zodiac-light-mustard border-opacity-30 px-2 py-1 font-mono text-sm">
+  <code className="max-w-full overflow-hidden text-ellipsis text-nowrap rounded-md border border-zodiac-light-mustard border-opacity-30 bg-zodiac-dark-mustard px-2 py-1 font-mono text-sm">
     {children}
   </code>
 )
--- extension/src/panel/routes/ConnectButton/Account.tsx
+++ extension/src/panel/routes/ConnectButton/Account.tsx
@@ -11,7 +11,7 @@ type AccountProps = {
 export const Account = ({ providerType, children }: AccountProps) => {
   return (
     <div className="flex items-center gap-4 overflow-hidden">
-      <div className="border-zodiac-dark-green relative flex size-12 flex-shrink-0 items-center justify-center rounded-full border">
+      <div className="relative flex size-12 flex-shrink-0 items-center justify-center rounded-full border border-zodiac-dark-green">
         <ProviderLogo providerType={providerType} />
       </div>
 
