d6fedd211513648036002f7115ebfc8f1ee04945
Philipp Giese <giese.philipp+git@gmail.com>
2024-10-22 16:41:14 +0200

    untangle logic

--- extension/src/panel/routes/ConnectButton/index.tsx
+++ extension/src/panel/routes/ConnectButton/index.tsx
@@ -27,21 +27,60 @@ interface Props {
 const ConnectButton: React.FC<Props> = ({ route, onConnect, onDisconnect }) => {
   let pilotAddress =
     route.initiator && parsePrefixedAddress(route.initiator)[1].toLowerCase()
+
   if (pilotAddress === ZeroAddress) pilotAddress = ''
 
   const injectedWallet = useInjectedWallet()
   const walletConnect = useWalletConnect(route.id)
 
-  const disconnect = () => {
-    if (route.providerType === ProviderType.WalletConnect) {
-      if (!walletConnect) {
-        throw new Error('walletConnect provider is not available')
-      }
-      walletConnect.disconnect()
-    }
+  // not connected
+  if (pilotAddress === '' || pilotAddress == null) {
+    return (
+      <div className="flex flex-col gap-2">
+        <Button
+          disabled={walletConnect == null}
+          onClick={async () => {
+            invariant(
+              walletConnect != null,
+              'walletConnect provider is not available'
+            )
 
-    onDisconnect()
+            const { chainId, accounts } = await walletConnect.connect()
+            onConnect({
+              providerType: ProviderType.WalletConnect,
+              chainId: chainId as ChainId,
+              account: accounts[0],
+            })
+          }}
+        >
+          <ProviderLogo providerType={ProviderType.WalletConnect} />
+          Connect with WalletConnect
+        </Button>
+        {injectedWallet.provider && (
+          <Button
+            disabled={!injectedWallet.connected}
+            onClick={async () => {
+              const { chainId, accounts } = await injectedWallet.connect()
+              onConnect({
+                providerType: ProviderType.InjectedWallet,
+                chainId: chainId as ChainId,
+                account: accounts[0],
+              })
+            }}
+          >
+            <ProviderLogo providerType={ProviderType.InjectedWallet} />
+            Connect with MetaMask
+          </Button>
+        )}
+      </div>
+    )
   }
+
+  const isWalletConnectWallet =
+    route.providerType === ProviderType.WalletConnect
+  const walletConnectProviderAvailable = walletConnect != null
+
+  const isInjectedWallet = route.providerType === ProviderType.InjectedWallet
 
   // atm, we don't yet support cross-chain routes, so can derive a general chainId from the avatar
   const [chainId] = parsePrefixedAddress(route.avatar)
@@ -51,29 +90,41 @@ const ConnectButton: React.FC<Props> = ({ route, onConnect, onDisconnect }) => {
   const connected =
     route.initiator &&
     isConnectedTo(
-      route.providerType === ProviderType.InjectedWallet
-        ? injectedWallet
-        : walletConnect,
+      isInjectedWallet ? injectedWallet : walletConnect,
       route.initiator,
       chainId
     )
 
   // good to go
-  if (connected && pilotAddress) {
+  if (connected) {
     return (
       <div className="flex flex-col gap-4">
         <Account providerType={route.providerType}>{pilotAddress}</Account>
 
-        <Button onClick={disconnect}>Disconnect</Button>
+        <Button
+          onClick={() => {
+            if (isWalletConnectWallet) {
+              invariant(
+                walletConnectProviderAvailable,
+                'walletConnect provider is not available'
+              )
+
+              walletConnect.disconnect()
+            }
+
+            onDisconnect()
+          }}
+        >
+          Disconnect
+        </Button>
       </div>
     )
   }
 
   // WalletConnect: wrong chain
   if (
-    route.providerType === ProviderType.WalletConnect &&
+    isWalletConnectWallet &&
     walletConnect &&
-    pilotAddress &&
     walletConnect.accounts.some((acc) => acc.toLowerCase() === pilotAddress) &&
     walletConnect.chainId !== chainId
   ) {
@@ -88,51 +139,22 @@ const ConnectButton: React.FC<Props> = ({ route, onConnect, onDisconnect }) => {
         <Tag head={<RiAlertLine />} color="warning">
           Chain mismatch
         </Tag>
-        <Button onClick={disconnect} className={classes.disconnectButton}>
+        <Button
+          onClick={() => walletConnect.disconnect()}
+          className={classes.disconnectButton}
+        >
           Disconnect
         </Button>
       </div>
     )
   }
 
-  // Injected wallet: right account, wrong chain
-  if (
-    route.providerType === ProviderType.InjectedWallet &&
-    injectedWallet.provider &&
-    pilotAddress &&
-    injectedWallet.accounts.some((acc) => acc.toLowerCase() === pilotAddress) &&
-    injectedWallet.chainId !== chainId
-  ) {
-    return (
-      <div
-        className={classNames(
-          classes.connectedContainer,
-          classes.connectionWarning
-        )}
-      >
-        <RawAddress>{validateAddress(pilotAddress)}</RawAddress>
-        <Tag head={<RiAlertLine />} color="warning">
-          Chain mismatch
-        </Tag>
-        {chainId && (
-          <Button
-            className={classes.disconnectButton}
-            onClick={() => {
-              injectedWallet.switchChain(chainId)
-            }}
-          >
-            Switch wallet to {CHAIN_NAME[chainId] || `#${chainId}`}
-          </Button>
-        )}
-      </div>
+  // Injected wallet
+  if (isInjectedWallet && injectedWallet.provider) {
+    const accountInWallet = injectedWallet.accounts.some(
+      (acc) => acc.toLowerCase() === pilotAddress
     )
-  }
 
-  if (
-    route.providerType === ProviderType.InjectedWallet &&
-    injectedWallet.provider &&
-    pilotAddress
-  ) {
     // Wallet disconnected
     if (injectedWallet.accounts.length === 0) {
       return (
@@ -145,16 +167,41 @@ const ConnectButton: React.FC<Props> = ({ route, onConnect, onDisconnect }) => {
 
           <div className="flex justify-end gap-2">
             <Button onClick={() => injectedWallet.connect()}>Connect</Button>
-            <Button onClick={disconnect}>Disconnect</Button>
+            <Button onClick={onDisconnect}>Disconnect</Button>
           </div>
         </div>
       )
     }
 
+    // Injected wallet: right account, wrong chain
+    if (accountInWallet && injectedWallet.chainId !== chainId) {
+      return (
+        <div
+          className={classNames(
+            classes.connectedContainer,
+            classes.connectionWarning
+          )}
+        >
+          <RawAddress>{validateAddress(pilotAddress)}</RawAddress>
+          <Tag head={<RiAlertLine />} color="warning">
+            Chain mismatch
+          </Tag>
+          {chainId && (
+            <Button
+              className={classes.disconnectButton}
+              onClick={() => {
+                injectedWallet.switchChain(chainId)
+              }}
+            >
+              Switch wallet to {CHAIN_NAME[chainId] || `#${chainId}`}
+            </Button>
+          )}
+        </div>
+      )
+    }
+
     // Wrong account
-    if (
-      !injectedWallet.accounts.some((acc) => acc.toLowerCase() === pilotAddress)
-    ) {
+    if (!accountInWallet) {
       return (
         <div className="flex flex-col gap-4">
           <Alert title="Account is not connected">
@@ -163,52 +210,11 @@ const ConnectButton: React.FC<Props> = ({ route, onConnect, onDisconnect }) => {
 
           <Account providerType={route.providerType}>{pilotAddress}</Account>
 
-          <Button onClick={disconnect}>Disconnect</Button>
+          <Button onClick={onDisconnect}>Disconnect</Button>
         </div>
       )
     }
   }
-
-  // not connected
-  return (
-    <div className="flex flex-col gap-2">
-      <Button
-        disabled={walletConnect == null}
-        onClick={async () => {
-          invariant(
-            walletConnect != null,
-            'walletConnect provider is not available'
-          )
-
-          const { chainId, accounts } = await walletConnect.connect()
-          onConnect({
-            providerType: ProviderType.WalletConnect,
-            chainId: chainId as ChainId,
-            account: accounts[0],
-          })
-        }}
-      >
-        <ProviderLogo providerType={ProviderType.WalletConnect} />
-        Connect with WalletConnect
-      </Button>
-      {injectedWallet.provider && (
-        <Button
-          disabled={!injectedWallet.connected}
-          onClick={async () => {
-            const { chainId, accounts } = await injectedWallet.connect()
-            onConnect({
-              providerType: ProviderType.InjectedWallet,
-              chainId: chainId as ChainId,
-              account: accounts[0],
-            })
-          }}
-        >
-          <ProviderLogo providerType={ProviderType.InjectedWallet} />
-          Connect with MetaMask
-        </Button>
-      )}
-    </div>
-  )
 }
 
 export default ConnectButton
