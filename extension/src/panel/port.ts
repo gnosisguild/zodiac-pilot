799b7536286f028ce882cb9c755bdc76d1f8486b
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-01 15:03:22 +0100

    init port only once

--- extension/src/panel/port.ts
+++ extension/src/panel/port.ts
@@ -1,10 +1,21 @@
 import { Message, PilotMessageType } from '@/messages'
 import { getActiveTab } from '@/utils'
+import { invariant } from '@epic-web/invariant'
 import { PILOT_PANEL_PORT } from '../const'
 import { setWindowId } from '../inject/bridge'
 
 // all communication with the background script goes through this central port
-export const port = chrome.runtime.connect({ name: PILOT_PANEL_PORT })
+let port: chrome.runtime.Port | null = null
+
+export const getPort = () => {
+  if (port == null) {
+    port = chrome.runtime.connect({ name: PILOT_PANEL_PORT })
+  }
+
+  invariant(port != null, 'Port not created')
+
+  return port
+}
 
 // notify the background script that the panel has been opened
 export const initPort = async () => {
@@ -13,7 +24,7 @@ export const initPort = async () => {
   const windowId = activeTab.windowId
   setWindowId(activeTab.windowId)
 
-  port.postMessage({
+  getPort().postMessage({
     type: PilotMessageType.PILOT_PANEL_OPENED,
     windowId,
     tabId: activeTab.id,
