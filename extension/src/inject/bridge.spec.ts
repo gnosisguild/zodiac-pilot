5d2cae65a1c953473b99921037430a500b5049be
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-14 10:39:41 +0100

    turn bridge into a hook and add first set of specs

--- extension/src/inject/bridge.spec.ts
+++ extension/src/inject/bridge.spec.ts
@@ -1,21 +1,36 @@
 import { InjectedProviderMessage, InjectedProviderMessageTyp } from '@/messages'
 import { callListeners, chromeMock, mockActiveTab } from '@/test-utils'
 import { Eip1193Provider } from '@/types'
-import { renderHook } from '@testing-library/react'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { cleanup, renderHook } from '@testing-library/react'
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  MockedFunction,
+  vi,
+} from 'vitest'
 import { useProviderBridge } from './bridge'
 
 describe('Bridge', () => {
   describe('Provider handling', () => {
     class MockProvider implements Eip1193Provider {
-      request = vi.fn().mockResolvedValue(null)
+      request: MockedFunction<Eip1193Provider['request']>
+
       on = vi.fn()
       removeListener = vi.fn()
+
+      constructor() {
+        this.request = vi.fn().mockResolvedValue(null)
+      }
     }
 
     beforeEach(() => {
       mockActiveTab()
     })
+
+    afterEach(cleanup)
 
     it('relays requests to the provider', async () => {
       const provider = new MockProvider()
@@ -36,6 +51,97 @@ describe('Bridge', () => {
       )
 
       expect(provider.request).toHaveBeenCalledWith(request)
+    })
+
+    it('forwards the response from the provider', async () => {
+      const provider = new MockProvider()
+
+      const response = { data: 'test' }
+
+      provider.request.mockResolvedValue(response)
+
+      renderHook(() => useProviderBridge({ provider }))
+
+      const request = { method: 'eth_chainId' }
+
+      const sendMessage = vi.fn()
+
+      await callListeners(
+        chromeMock.runtime.onMessage,
+        {
+          type: InjectedProviderMessageTyp.INJECTED_PROVIDER_REQUEST,
+          request,
+          requestId: 'test-id',
+        } satisfies InjectedProviderMessage,
+        { id: chromeMock.runtime.id },
+        sendMessage
+      )
+
+      expect(sendMessage).toHaveBeenCalledWith({
+        type: InjectedProviderMessageTyp.INJECTED_PROVIDER_RESPONSE,
+        response,
+        requestId: 'test-id',
+      })
+    })
+
+    it('catches and forwards errors from the provider', async () => {
+      const provider = new MockProvider()
+
+      const error = { message: 'Something went wrong', code: 666 }
+
+      provider.request.mockRejectedValue(error)
+
+      renderHook(() => useProviderBridge({ provider }))
+
+      const request = { method: 'eth_chainId' }
+
+      const sendMessage = vi.fn()
+
+      await callListeners(
+        chromeMock.runtime.onMessage,
+        {
+          type: InjectedProviderMessageTyp.INJECTED_PROVIDER_REQUEST,
+          request,
+          requestId: 'test-id',
+        } satisfies InjectedProviderMessage,
+        { id: chromeMock.runtime.id },
+        sendMessage
+      )
+
+      expect(sendMessage).toHaveBeenCalledWith({
+        type: InjectedProviderMessageTyp.INJECTED_PROVIDER_ERROR,
+        requestId: 'test-id',
+        error,
+      })
+    })
+
+    it('only calls the current provider', async () => {
+      const providerA = new MockProvider()
+      const providerB = new MockProvider()
+
+      const request = { method: 'eth_chainId' }
+
+      const { rerender } = renderHook(
+        ({ provider }: { provider: Eip1193Provider }) =>
+          useProviderBridge({ provider }),
+        { initialProps: { provider: providerA } }
+      )
+
+      rerender({ provider: providerB })
+
+      await callListeners(
+        chromeMock.runtime.onMessage,
+        {
+          type: InjectedProviderMessageTyp.INJECTED_PROVIDER_REQUEST,
+          request,
+          requestId: 'test-id',
+        } satisfies InjectedProviderMessage,
+        { id: chromeMock.runtime.id },
+        vi.fn()
+      )
+
+      expect(providerA.request).not.toHaveBeenCalled()
+      expect(providerB.request).toHaveBeenCalledWith(request)
     })
   })
 
