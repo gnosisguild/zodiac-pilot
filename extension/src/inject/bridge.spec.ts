00706d9aece8de721838653becd9022dd738616c
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-14 10:19:14 +0100

    wip putting bridge into the react lifecycle

--- extension/src/inject/bridge.spec.ts
+++ extension/src/inject/bridge.spec.ts
@@ -1,7 +1,45 @@
-import { describe, it } from 'vitest'
+import { InjectedProviderMessage, InjectedProviderMessageTyp } from '@/messages'
+import { callListeners, chromeMock, mockActiveTab } from '@/test-utils'
+import { Eip1193Provider } from '@/types'
+import { renderHook } from '@testing-library/react'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { useProviderBridge } from './bridge'
 
 describe('Bridge', () => {
-  it('waits for the provider to be initialized', () => {
-    throw new Error('Not implemented')
+  describe('Provider handling', () => {
+    class MockProvider implements Eip1193Provider {
+      request = vi.fn().mockResolvedValue(null)
+      on = vi.fn()
+      removeListener = vi.fn()
+    }
+
+    beforeEach(() => {
+      mockActiveTab()
+    })
+
+    it('relays requests to the provider', async () => {
+      const provider = new MockProvider()
+
+      renderHook(() => useProviderBridge({ provider }))
+
+      const request = { method: 'eth_chainId' }
+
+      await callListeners(
+        chromeMock.runtime.onMessage,
+        {
+          type: InjectedProviderMessageTyp.INJECTED_PROVIDER_REQUEST,
+          request,
+          requestId: 'test-id',
+        } satisfies InjectedProviderMessage,
+        { id: chromeMock.runtime.id },
+        vi.fn()
+      )
+
+      expect(provider.request).toHaveBeenCalledWith(request)
+    })
   })
+
+  describe('Account handling', () => {})
+
+  describe('Chain handling', () => {})
 })
--- extension/src/inject/bridge.ts
+++ extension/src/inject/bridge.ts
@@ -3,6 +3,7 @@ import { InjectedProviderMessage, InjectedProviderMessageTyp } from '@/messages'
 import { getActiveTab, sendMessageToTab } from '@/utils'
 import { invariant } from '@epic-web/invariant'
 import { toQuantity } from 'ethers'
+import { useCallback, useEffect, useRef } from 'react'
 import { ChainId } from 'ser-kit'
 import { Eip1193Provider } from '../types'
 
@@ -47,7 +48,9 @@ export const update = (
 const emitEvent = async (eventName: string, eventData: any) => {
   const tab = await getActiveTab()
 
-  sendMessageToTab(tab.id!, {
+  invariant(tab.id != null, 'Can only send events to tabs that have an ID')
+
+  sendMessageToTab(tab.id, {
     type: InjectedProviderMessageTyp.INJECTED_PROVIDER_EVENT,
     eventName,
     eventData,
@@ -55,30 +58,123 @@ const emitEvent = async (eventName: string, eventData: any) => {
 }
 
 // Relay RPC requests
-chrome.runtime.onMessage.addListener(
-  (message: InjectedProviderMessage, sender, sendResponse) => {
-    // only handle messages from our extension
-    if (sender.id !== chrome.runtime.id) return
+// chrome.runtime.onMessage.addListener(
+//   (message: InjectedProviderMessage, sender, sendResponse) => {
+//     // only handle messages from our extension
+//     if (sender.id !== chrome.runtime.id) return
 
-    // only handle messages from the current window
-    if (sender.tab?.windowId !== windowId) return
+//     // only handle messages from the current window
+//     if (sender.tab?.windowId !== windowId) return
 
-    if (message.type === InjectedProviderMessageTyp.INJECTED_PROVIDER_REQUEST) {
-      promise
-        .then(() => {
-          invariant(
-            provider != null,
-            'The provider for the injected bridge has not been set, yet'
-          )
+//     if (message.type === InjectedProviderMessageTyp.INJECTED_PROVIDER_REQUEST) {
+//       promise
+//         .then(() => {
+//           invariant(
+//             provider != null,
+//             'The provider for the injected bridge has not been set, yet'
+//           )
 
-          return provider.request(message.request)
-        })
+//           return provider.request(message.request)
+//         })
+//         .then((response) => {
+//           sendResponse({
+//             type: InjectedProviderMessageTyp.INJECTED_PROVIDER_RESPONSE,
+//             requestId: message.requestId,
+//             response,
+//           } satisfies InjectedProviderMessage)
+//         })
+//         .catch((error) => {
+//           sendResponse({
+//             type: InjectedProviderMessageTyp.INJECTED_PROVIDER_ERROR,
+//             requestId: message.requestId,
+//             error: {
+//               message: error.message,
+//               code: error.code,
+//             },
+//           } satisfies InjectedProviderMessage)
+//         })
+
+//       // without this the response won't be sent
+//       return true
+//     }
+//   }
+// )
+
+type ResponseFn = (response: InjectedProviderMessage) => void
+
+type UseProviderBridgeOptions = {
+  provider: Eip1193Provider
+  chainId?: ChainId
+  account?: `0x${string}`
+}
+
+export const useProviderBridge = ({
+  provider,
+  chainId,
+  account,
+}: UseProviderBridgeOptions) => {
+  useHandleProviderRequests(provider)
+
+  const chainIdRef = useRef(chainId)
+
+  useEffect(() => {
+    if (chainId == null) {
+      return
+    }
+
+    if (chainIdRef.current == null) {
+      emitEvent('connect', { chainId: toQuantity(chainId) })
+    } else {
+      emitEvent('chainChanged', [toQuantity(chainId)])
+    }
+
+    chainIdRef.current = chainId
+  }, [chainId])
+
+  const accountRef = useRef(account)
+
+  useEffect(() => {
+    if (accountRef.current == null && account == null) {
+      return
+    }
+
+    accountRef.current = account
+
+    emitEvent('accountsChanged', account == null ? [] : [account])
+  }, [account])
+}
+
+const useHandleProviderRequests = (provider: Eip1193Provider) => {
+  const handleMessage = useCallback(
+    (
+      message: InjectedProviderMessage,
+      sender: chrome.runtime.MessageSender,
+      sendResponse: ResponseFn
+    ) => {
+      // only handle messages from our extension
+      if (sender.id !== chrome.runtime.id) {
+        return
+      }
+
+      // only handle messages from the current window
+      if (sender.tab?.windowId !== windowId) {
+        return
+      }
+
+      if (
+        message.type !== InjectedProviderMessageTyp.INJECTED_PROVIDER_REQUEST
+      ) {
+        return
+      }
+
+      provider
+        .request(message.request)
         .then((response) => {
           sendResponse({
             type: InjectedProviderMessageTyp.INJECTED_PROVIDER_RESPONSE,
             requestId: message.requestId,
             response,
-          } satisfies InjectedProviderMessage)
+          })
         })
         .catch((error) => {
           sendResponse({
@@ -88,11 +184,20 @@ chrome.runtime.onMessage.addListener(
               message: error.message,
               code: error.code,
             },
-          } satisfies InjectedProviderMessage)
+          })
         })
 
       // without this the response won't be sent
       return true
+    },
+    [provider]
+  )
+
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener(handleMessage)
+
+    return () => {
+      chrome.runtime.onMessage.removeListener(handleMessage)
     }
-  }
-)
+  }, [handleMessage])
+}
