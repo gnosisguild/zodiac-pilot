2427ca3c8b83892c5c89a34060c438fa375a8c74
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-15 11:47:30 +0100

    add spec to create port when tab is ready

--- extension/src/connect/port/createPortWhenTabIsReady.ts
+++ extension/src/connect/port/createPortWhenTabIsReady.ts
@@ -2,55 +2,54 @@ import { createPort } from './createPort'
 
 export const createPortWhenTabIsReady = async (tab: chrome.tabs.Tab) => {
   if (tab.id == null) {
-    return Promise.resolve(null)
+    return null
   }
+
+  const { promise, resolve } =
+    Promise.withResolvers<chrome.runtime.Port | null>()
 
   const tabId = tab.id
 
-  return new Promise<chrome.runtime.Port | null>((resolve) => {
+  if (tab.status !== 'complete') {
+    console.debug(`Tab (id: "${tab.id}", url: "${tab.url}") NOT ready.`)
+
     const handleTabWhenReady = async (
       tabId: number,
       info: chrome.tabs.TabChangeInfo
     ) => {
-      const url = info.url
-
       if (tab.id === tabId && info.status === 'complete') {
         chrome.tabs.onUpdated.removeListener(handleTabWhenReady)
 
         console.debug(
-          `Tab (id: "${tab.id}", url: "${url}") became ready. Trying to open port.`
+          `Tab (id: "${tab.id}", url: "${tab.url}") became ready. Trying to open port.`
         )
 
-        const port = await createPort(tabId, url)
+        const port = await createPort(tabId, tab.url)
 
-        console.debug(`Port to Tab (id: "${tab.id}", url: "${url}") created.`)
-
-        resolve(port)
-      }
-    }
-
-    if (tab.status !== 'complete') {
-      console.debug(`Tab (id: "${tab.id}", url: "${tab.url}") NOT ready.`)
-
-      chrome.tabs.onUpdated.addListener(handleTabWhenReady)
-
-      return
-    }
-
-    console.debug(
-      `Tab (id: "${tab.id}", url: "${tab.url}") was already loaded. Trying to create port.`
-    )
-
-    createPort(tabId, tab.url).then((port) => {
-      if (port != null) {
         console.debug(
           `Port to Tab (id: "${tab.id}", url: "${tab.url}") created.`
         )
 
         resolve(port)
-      } else {
-        resolve(null)
       }
-    })
-  })
+    }
+
+    chrome.tabs.onUpdated.addListener(handleTabWhenReady)
+
+    return promise
+  }
+
+  console.debug(
+    `Tab (id: "${tab.id}", url: "${tab.url}") was already loaded. Trying to create port.`
+  )
+
+  const port = await createPort(tabId, tab.url)
+
+  if (port == null) {
+    return null
+  }
+
+  console.debug(`Port to Tab (id: "${tab.id}", url: "${tab.url}") created.`)
+
+  return port
 }
