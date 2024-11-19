b3966666c2562285204f16df121bf1469dd1dfa5
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-19 11:17:58 +0100

    update general transaction list layout

--- extension/src/panel/app-routes/transactions/RouteBubble/index.tsx
+++ extension/src/panel/app-routes/transactions/RouteBubble/index.tsx
@@ -4,7 +4,6 @@ import { useZodiacRoute } from '@/zodiac-routes'
 import { Link } from 'react-router-dom'
 import { asLegacyConnection } from '../../legacyConnectionMigrations'
 import { ConnectionsIcon } from './ConnectionsIcon'
-import classes from './style.module.css'
 
 export const RouteBubble = () => {
   const route = useZodiacRoute()
@@ -12,54 +11,63 @@ export const RouteBubble = () => {
   const connection = asLegacyConnection(route)
 
   return (
-    <Box rounded className={classes.routeBubble}>
+    <Box
+      rounded
+      className="group hover:border-zodiac-light-mustard hover:border-opacity-80"
+    >
       <div className="flex gap-1">
-        <Box bg roundedLeft className="flex-1 py-0 pl-0 pr-8">
+        <Box bg roundedLeft className="flex-1 py-0 pl-0 pr-2">
           <Link to={'/routes/' + route.id}>
             <div className="flex items-center gap-2">
-              <div className={classes.blockieStack}>
+              <div className="flex h-10 flex-shrink-0 p-1">
                 {connection.pilotAddress && (
-                  <Box rounded className={classes.blockieBox}>
+                  <div className="rounded-full border-2 border-slate-900">
                     <Blockie
                       address={connection.pilotAddress}
-                      className={classes.blockie}
+                      className="h-full"
                     />
-                  </Box>
+                  </div>
                 )}
                 {connection.moduleAddress && (
-                  <Box rounded className={classes.blockieBox}>
+                  <div className="-ml-4 rounded-full border-2 border-slate-900">
                     <Blockie
                       address={connection.moduleAddress}
-                      className={classes.blockie}
+                      className="h-full"
                     />
-                  </Box>
+                  </div>
                 )}
-                <Box rounded className={classes.blockieBox}>
+                <div className="-ml-4 rounded-full border-2 border-slate-900">
                   <Blockie
                     address={connection.avatarAddress}
-                    className={classes.blockie}
+                    className="h-full"
                   />
-                </Box>
+                </div>
               </div>
-              <p className={classes.label}>{connection.label}</p>
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap">
+                {connection.label}
+              </p>
             </div>
           </Link>
         </Box>
         <Link to="/routes">
-          <Box bg roundedRight className={classes.connectionsContainer}>
+          <Box
+            bg
+            roundedRight
+            className="h-11 w-14 overflow-hidden group-hover:border-zodiac-light-mustard group-hover:border-opacity-80"
+          >
             <span className="sr-only">Configure routes</span>
             <ConnectionsIcon height="100%" width="100%" />
           </Box>
         </Link>
       </div>
 
-      <div className={classes.infoContainer}>
-        <Box bg p={3} className={classes.info}>
+      <div className="pointer-events-none absolute -bottom-24 -right-px isolate z-10 opacity-0 transition-opacity group-hover:pointer-events-auto group-hover:opacity-100">
+        <Box className="rounded-md bg-slate-900 bg-opacity-20 p-4 backdrop-blur-sm">
           <Link to={'/routes/' + connection.id}>
             <ConnectionStack
               chainId={chainId}
               connection={connection}
-              className={classes.stack}
+              className="hover:border-zodiac-light-mustard hover:border-opacity-80"
             />
           </Link>
         </Box>
