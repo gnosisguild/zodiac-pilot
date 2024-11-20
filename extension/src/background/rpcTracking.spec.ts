847f4f6f0f37530dca5c46b9bf706c86ce945723
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2024-11-20 14:24:05 +0100

    fix: update RPC redirect rules when new networks are detected (#213)
    * make it possible to get notified when new rpc endpoints are tracked
    
    * make it possible to remove listeners
    
    * update rpc redirect rules during a simulation
    
    * session handles updated rpc redirect rules
    
    * do not mutate session data all the time
    
    * make sure to access the correct session info
    
    * fix a couple other wrong accesses

---
+++ extension/src/background/rpcTracking.spec.ts
@@ -0,0 +1,44 @@
+import { mockRPCRequest, startPilotSession } from '@/test-utils'
+import { describe, expect, it, vi } from 'vitest'
+import { trackRequests } from './rpcTracking'
+import { trackSessions } from './sessionTracking'
+
+describe('RPC Tracking', () => {
+  it('notifies when a new network has been tracked', async () => {
+    const result = trackRequests()
+    trackSessions(result)
+
+    await startPilotSession({ windowId: 1, tabId: 1 })
+    const handler = vi.fn()
+
+    result.onNewRPCEndpointDetected.addListener(handler)
+
+    await mockRPCRequest({
+      chainId: 1,
+      tabId: 1,
+      url: 'http://test-json-rpc.com',
+    })
+
+    expect(handler).toHaveBeenCalled()
+  })
+
+  it('is possible to stop getting notified', async () => {
+    const result = trackRequests()
+    trackSessions(result)
+
+    await startPilotSession({ windowId: 1, tabId: 1 })
+
+    const handler = vi.fn()
+
+    result.onNewRPCEndpointDetected.addListener(handler)
+    result.onNewRPCEndpointDetected.removeListener(handler)
+
+    await mockRPCRequest({
+      chainId: 1,
+      tabId: 1,
+      url: 'http://test-json-rpc.com',
+    })
+
+    expect(handler).not.toHaveBeenCalled()
+  })
+})
