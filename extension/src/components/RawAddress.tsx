baf3c6c43d17ada4329510d3bad7ac6142b4b763
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-21 16:57:20 +0100

    update raw address style

--- extension/src/components/RawAddress.tsx
+++ extension/src/components/RawAddress.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react'
 
 export const RawAddress = ({ children }: PropsWithChildren) => (
-  <code className="max-w-full overflow-hidden text-ellipsis text-nowrap rounded-md border border-zodiac-light-mustard border-opacity-30 bg-zodiac-dark-mustard px-2 py-1 font-mono">
+  <code className="max-w-full overflow-hidden text-ellipsis text-nowrap rounded-md border border-zinc-600 bg-zinc-800 px-2 py-1 font-mono">
     {children}
   </code>
 )
