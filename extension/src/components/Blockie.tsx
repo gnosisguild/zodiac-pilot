98dd9dfe4e3e2c1a1557af113299075e3df1c2cc
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-26 11:08:59 +0100

    remove style module from Blockie

--- extension/src/components/Blockie.tsx
+++ extension/src/components/Blockie.tsx
@@ -2,8 +2,6 @@ import classNames from 'classnames'
 import makeBlockie from 'ethereum-blockies-base64'
 import { useMemo } from 'react'
 
-import classes from './style.module.css'
-
 interface Props {
   address: string
   className?: string
@@ -12,8 +10,8 @@ interface Props {
 export const Blockie = ({ address, className }: Props) => {
   const blockie = useMemo(() => address && makeBlockie(address), [address])
   return (
-    <div className={classNames(classes.container, className)}>
-      <img src={blockie} alt={address} />
+    <div className={classNames('overflow-hidden rounded-full', className)}>
+      <img src={blockie} alt={address} className="block h-full" />
     </div>
   )
 }
