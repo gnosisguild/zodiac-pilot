a2c5e51a4d75004d223ddc17faaed9e13d0e7b21
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-26 13:40:39 +0100

    fit some more specs

--- extension/src/panel/providers/wallet-connect/useDisconnectWalletConnectIfNeeded.spec.ts
+++ extension/src/panel/providers/wallet-connect/useDisconnectWalletConnectIfNeeded.spec.ts
@@ -32,4 +32,66 @@ describe('Disconnect Wallet Connect if needed', () => {
 
     expect(onDisconnect).toHaveBeenCalled()
   })
+
+  it('does not call "onDisconnect" after the hook unmounts', async () => {
+    const route = createMockRoute({
+      providerType: ProviderType.WalletConnect,
+      avatar: ETH_ZERO_ADDRESS,
+    })
+
+    const onDisconnect = vi.fn()
+
+    const { unmount } = await renderHook(
+      () => useDisconnectWalletConnectIfNeeded(route, { onDisconnect }),
+      {
+        routes: [route],
+      }
+    )
+
+    const provider = await getWalletConnectProvider(route.id)
+
+    // Finish the event loop once so that all listeners could be
+    // set up
+    await sleep(1)
+
+    unmount()
+
+    provider.events.emit('disconnect')
+
+    expect(onDisconnect).not.toHaveBeenCalled()
+  })
+
+  it('always calls the latest callback', async () => {
+    const route = createMockRoute({
+      providerType: ProviderType.WalletConnect,
+      avatar: ETH_ZERO_ADDRESS,
+    })
+
+    const firstOnDisconnect = vi.fn()
+    const secondOnDisconnect = vi.fn()
+
+    const { rerender } = await renderHook(
+      ({ onDisconnect }) =>
+        useDisconnectWalletConnectIfNeeded(route, { onDisconnect }),
+      {
+        routes: [route],
+        initialProps: {
+          onDisconnect: firstOnDisconnect,
+        },
+      }
+    )
+
+    const provider = await getWalletConnectProvider(route.id)
+
+    // Finish the event loop once so that all listeners could be
+    // set up
+    await sleep(1)
+
+    rerender({ onDisconnect: secondOnDisconnect })
+
+    provider.events.emit('disconnect')
+
+    expect(firstOnDisconnect).not.toHaveBeenCalled()
+    expect(secondOnDisconnect).toHaveBeenCalled()
+  })
 })
