82f476df7c2dd147898611b65ac6324d742ed925
Philipp Giese <giese.philipp+git@gmail.com>
2024-11-29 13:03:42 +0100

    make remove button request confirmation

--- extension/src/panel/app-routes/edit-route/RemoveButton.tsx
+++ extension/src/panel/app-routes/edit-route/RemoveButton.tsx
@@ -1,6 +1,7 @@
-import { GhostButton } from '@/components'
+import { GhostButton, Modal, PrimaryButton } from '@/components'
 import { useRemoveZodiacRoute } from '@/zodiac-routes'
 import { Trash2 } from 'lucide-react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useRouteId } from './useRouteId'
 
@@ -8,18 +9,42 @@ export const RemoveButton = () => {
   const navigate = useNavigate()
   const removeRouteById = useRemoveZodiacRoute()
   const routeId = useRouteId()
+  const [confirmRemove, setConfirmRemove] = useState(false)
 
   return (
-    <GhostButton
-      iconOnly
-      icon={Trash2}
-      style="critical"
-      onClick={() => {
-        removeRouteById(routeId)
-        navigate('/routes')
-      }}
-    >
-      Remove this connection
-    </GhostButton>
+    <>
+      <GhostButton
+        iconOnly
+        icon={Trash2}
+        style="critical"
+        onClick={() => setConfirmRemove(true)}
+      >
+        Remove this connection
+      </GhostButton>
+
+      <Modal
+        closeLabel="Cancel"
+        onClose={() => setConfirmRemove(false)}
+        open={confirmRemove}
+        title="Remove route"
+      >
+        Are you sure want to remove this route?
+        <Modal.Actions>
+          <GhostButton style="contrast" onClick={() => setConfirmRemove(false)}>
+            Cancel
+          </GhostButton>
+
+          <PrimaryButton
+            style="contrast"
+            onClick={() => {
+              removeRouteById(routeId)
+              navigate('/routes')
+            }}
+          >
+            Remove
+          </PrimaryButton>
+        </Modal.Actions>
+      </Modal>
+    </>
   )
 }
