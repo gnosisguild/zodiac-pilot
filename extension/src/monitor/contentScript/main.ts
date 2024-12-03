3920e8847b6ffcbcf0a2ec718fed162504f3792a
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-12-03 10:20:38 +0100

    fix file reference

--- extension/src/monitor/contentScript/main.ts
+++ extension/src/monitor/contentScript/main.ts
@@ -7,7 +7,7 @@ window.document.documentElement.dataset.__zodiacPilotBasePath =
   chrome.runtime.getURL('/')
 window.document.documentElement.dataset.__zodiacExtensionId = chrome.runtime.id
 
-injectScript('build/pilot-connect-monitor/injectedScript/main.js')
+injectScript('build/monitor/injectedScript/main.js')
 
 chrome.runtime.onMessage.addListener((message: Message) => {
   if (
