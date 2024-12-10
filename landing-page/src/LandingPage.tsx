faa37f3c803afed531c5b4b5d0e7f8d63574dc01
Philipp Giese <giese.philipp+git@gmail.com>
2024-12-10 11:39:33 +0100

    responsiveness

--- landing-page/src/LandingPage.tsx
+++ landing-page/src/LandingPage.tsx
@@ -6,11 +6,11 @@ import {
 import { PilotType, ZodiacOsPlain } from './logos'
 
 export const LandingPage = () => (
-  <div className="mx-auto flex flex-col justify-between lg:w-2/3">
-    <header className="my-24 flex items-center justify-between">
+  <div className="mx-8 my-8 flex flex-col justify-between lg:mx-auto lg:w-2/3">
+    <header className="mb-24 flex items-center justify-between lg:my-24">
       <div className="flex items-center gap-4">
-        <ZodiacOsPlain className="h-8" />
-        <PilotType className="h-10 dark:invert" />
+        <ZodiacOsPlain className="h-6 lg:h-8" />
+        <PilotType className="h-8 lg:h-10 dark:invert" />
       </div>
 
       <aside className="flex gap-4">
@@ -20,7 +20,7 @@ export const LandingPage = () => (
           rel="noreferrer noopener"
         >
           <span className="sr-only">Open Discord</span>
-          <SiDiscord className="size-8" />
+          <SiDiscord className="size-6 lg:size-8" />
         </a>
 
         <a
@@ -29,16 +29,16 @@ export const LandingPage = () => (
           rel="noreferrer noopener"
         >
           <span className="sr-only">View on GitHub</span>
-          <SiGithub className="size-8" />
+          <SiGithub className="size-6 lg:size-8" />
         </a>
       </aside>
     </header>
     <main className="flex flex-col gap-24">
-      <h1 className="text-balance text-center text-5xl font-thin">
+      <h1 className="text-balance text-center text-3xl font-thin lg:text-5xl">
         Secure, flexible, smart accounts with the ease of a browser extension.
       </h1>
 
-      <section className="flex items-center justify-around gap-4">
+      <section className="flex flex-col-reverse items-center justify-around gap-32 lg:flex-row">
         <div className="rounded-xl border border-gray-300/80 p-4 shadow-2xl dark:border-gray-700/80 dark:shadow-zinc-800">
           <img
             src="/extension-dark.png"
@@ -54,7 +54,7 @@ export const LandingPage = () => (
         </div>
 
         <div className="flex flex-col gap-14">
-          <div className="flex flex-col gap-4 text-2xl font-extralight text-zinc-500 dark:text-zinc-300">
+          <div className="flex flex-col gap-4 text-balance text-center text-zinc-500 md:text-xl md:font-extralight lg:text-2xl xl:text-left dark:text-zinc-300">
             <p>Trusted by institutions.</p>
             <p>Flexible enough for individuals.</p>
             <p>Let Pilot guide your transactions.</p>
