facf0db4f55276b6a67d00239c9202dc2bd07acb
Philipp Giese <giese.philipp+git@gmail.com>
2024-12-10 12:24:22 +0100

    use vite plugin for og tags

--- landing-page/vite.config.ts
+++ landing-page/vite.config.ts
@@ -1,9 +1,24 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import openGraph from 'vite-plugin-open-graph'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    openGraph({
+      basic: {
+        type: 'website',
+        title: 'Gnosis Guild',
+        description: 'Zodiac Pilot — Batch and simulate transactions',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: 'Gnosis Guild',
+        description: 'Zodiac Pilot — Batch and simulate transactions',
+      },
+    }),
+  ],
   build: {
     manifest: true,
   },
