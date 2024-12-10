e657a0e47827e2a4e2e25815b310018cc6574f8a
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2024-12-10 12:52:14 +0100

    update announce image (#286)

--- extension/src/inject/injectedScript/announceProvider.ts
+++ extension/src/inject/injectedScript/announceProvider.ts
@@ -8,7 +8,7 @@ export const announceEip6963Provider = (provider: InjectedProvider) => {
   const info = {
     uuid: '2a0c727b-4359-49a0-920c-b411d32b1d1e', // random uuid
     name: 'Zodiac Pilot',
-    icon: '//pilot.gnosisguild.org/pilot48.png',
+    icon: '//pilot.gnosisguild.org/zodiac48.png',
     rdns: 'org.gnosisguild.pilot',
   }
 
