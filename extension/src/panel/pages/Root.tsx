c83096b8d460b4c8694d1e52c8c7041d7cc1f15d
Philipp Giese <giese.philipp+git@gmail.com>
2024-12-12 11:55:15 +0100

    start handling the case of no routes

--- extension/src/panel/pages/Root.tsx
+++ extension/src/panel/pages/Root.tsx
@@ -1,6 +1,6 @@
 import { getChainId } from '@/chains'
 import {
-  useExecutionRoute,
+  getRoutes,
   useMarkRouteAsUsed,
   useSaveExecutionRoute,
 } from '@/execution-routes'
@@ -10,20 +10,38 @@ import {
   useDisconnectWalletConnectIfNeeded,
 } from '@/providers'
 import { useProvider } from '@/providers-ui'
-import { Outlet } from 'react-router-dom'
+import { invariant } from '@epic-web/invariant'
+import {
+  Outlet,
+  useLoaderData,
+  type LoaderFunctionArgs,
+} from 'react-router-dom'
 import { parsePrefixedAddress } from 'ser-kit'
 import { useStorage } from '../utils'
+import { getActiveRouteId } from './getActiveRouteId'
 import {
   asLegacyConnection,
   fromLegacyConnection,
 } from './legacyConnectionMigrations'
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const activeRouteId = getActiveRouteId(params)
+
+  const routes = await getRoutes()
+
+  const route = routes.find((route) => route.id === activeRouteId)
+
+  invariant(route != null, `Could not find route with id "${activeRouteId}"`)
+
+  return { route }
+}
 
 export const Root = () => {
   // update the last used timestamp for the current route
   useMarkRouteAsUsed()
 
   // make sure the injected provider stays updated on every relevant route change
-  const route = useExecutionRoute()
+  const { route } = useLoaderData<typeof loader>()
 
   const saveRoute = useSaveExecutionRoute()
 
--- extension/src/panel/pages/index.tsx
+++ extension/src/panel/pages/index.tsx
@@ -1,19 +1,31 @@
-import type { RouteObject } from 'react-router-dom'
-import { Root } from './Root'
+import { getRoutes } from '@/execution-routes'
+import { redirect, type RouteObject } from 'react-router-dom'
+import { loader, Root } from './Root'
 import { routes } from './routes'
 import { Transactions } from './transactions'
 
 export const pages: RouteObject[] = [
   {
     path: '/',
+    loader: async () => {
+      const [route] = await getRoutes()
+
+      if (route != null) {
+        return redirect(`/${route.id}`)
+      }
+    },
+  },
+  {
+    path: '/:activeRouteId',
     element: <Root />,
+    loader,
     children: [
       {
-        path: '/',
+        path: '',
         element: <Transactions />,
       },
       {
-        path: '/routes',
+        path: 'routes',
         children: routes,
       },
     ],
