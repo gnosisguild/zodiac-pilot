54f961f8e76ee259953ff0822f289f6c44646df8
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2024-12-13 15:15:07 +0100

    chore :add specs for edit route (#348)
    * focus specs more on important inputs
    
    * add spec for chain change
    
    * add first couple of tests for edit route
    
    * add a test for role select
    
    * remove .only

--- extension/test-utils/creators/randomHex.ts
+++ extension/test-utils/creators/randomHex.ts
@@ -1,4 +1,5 @@
 import type { HexAddress } from '@/types'
+import { formatPrefixedAddress, type ChainId } from 'ser-kit'
 
 export const randomHex = (size: number): HexAddress => {
   const hex = [...Array(size)]
@@ -7,3 +8,15 @@ export const randomHex = (size: number): HexAddress => {
 
   return `0x${hex}`
 }
+
+export const randomAddress = (size: number = 40) => randomHex(size)
+
+type RandomPrefixedAddressOptions = {
+  chainId?: ChainId
+  address?: HexAddress
+}
+
+export const randomPrefixedAddress = ({
+  chainId = 1,
+  address = randomAddress(),
+}: RandomPrefixedAddressOptions = {}) => formatPrefixedAddress(chainId, address)
