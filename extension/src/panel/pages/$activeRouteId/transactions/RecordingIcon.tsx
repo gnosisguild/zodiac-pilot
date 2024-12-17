c7138b222264ebf5f55685d42ac921d8e48a54db
Philipp Giese <giese.philipp+git@gmail.com>
2024-12-17 15:30:24 +0100

    minor layout update

--- extension/src/panel/pages/$activeRouteId/transactions/RecordingIcon.tsx
+++ extension/src/panel/pages/$activeRouteId/transactions/RecordingIcon.tsx
@@ -8,13 +8,13 @@ export const RecordingIcon = ({ active }: RecordingIconProps) => (
   <div className="relative flex size-4 items-center justify-center">
     <div
       className={classNames(
-        'absolute size-4 rounded-full',
+        'absolute size-4 rounded-full transition-all',
         active && 'animate-ping bg-red-400/50 motion-reduce:animate-none',
       )}
     />
     <div
       className={classNames(
-        'size-3 rounded-full',
+        'size-3 rounded-full transition-all',
         active && 'bg-red-500',
         !active && 'bg-gray-500',
       )}
