a56cf33b82e10d2893d94dab077ddeec67ee0d8e
Philipp Giese <giese.philipp+git@gmail.com>
2024-12-18 12:15:31 +0100

    migrate error toast

--- extension/src/components/toasts/errorToast.tsx
+++ extension/src/components/toasts/errorToast.tsx
@@ -1,24 +1,26 @@
-import { Toast, toast } from './Toast'
+import { type DerivedToastProps, Toast, toast } from './Toast'
 import type { ToastProps } from './ToastProps'
 
+const ErrorToast = ({
+  data: { title, message },
+  closeToast,
+}: DerivedToastProps) => (
+  <Toast className="border-red-500/60 bg-red-500 dark:border-red-700">
+    <div className="flex items-center justify-between gap-4">
+      {title && <Toast.Title className="text-white">{title}</Toast.Title>}
+
+      <Toast.Dismiss
+        className="text-white hover:bg-white/20"
+        onDismiss={closeToast}
+      />
+    </div>
+
+    <div className="text-red-50">{message}</div>
+  </Toast>
+)
+
 export const errorToast = ({ title, message }: ToastProps) =>
-  toast(
-    ({ dismiss }) => (
-      <>
-        <div className="flex items-center justify-between gap-4">
-          {title && <Toast.Title className="text-white">{title}</Toast.Title>}
-
-          <Toast.Dismiss
-            className="text-white hover:bg-white/20"
-            onDismiss={dismiss}
-          />
-        </div>
-
-        <div className="text-red-50">{message}</div>
-      </>
-    ),
-    {
-      className: 'border-red-500/60 bg-red-500 dark:border-red-700',
-      autoClose: false,
-    },
-  )
+  toast(ErrorToast, {
+    data: { title, message },
+    autoClose: false,
+  })
