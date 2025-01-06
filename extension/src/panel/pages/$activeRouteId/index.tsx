57352e63c3fb1edcb560d8ec20c338b4c71d735b
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2025-01-06 10:43:57 +0100

    fix: register action to make wallet connect disconnect work (#462)

--- extension/src/panel/pages/$activeRouteId/index.tsx
+++ extension/src/panel/pages/$activeRouteId/index.tsx
@@ -1,5 +1,5 @@
 import { redirect, type RouteObject } from 'react-router'
-import { ActiveRoute as Component, loader } from './ActiveRoute'
+import { action, ActiveRoute as Component, loader } from './ActiveRoute'
 import { ClearTransactions } from './clear-transactions.$newActiveRouteId'
 import { Transactions } from './transactions'
 
@@ -7,6 +7,7 @@ export const ActiveRoute: RouteObject = {
   path: '/:activeRouteId',
   element: <Component />,
   loader,
+  action,
   children: [
     { index: true, loader: () => redirect('transactions') },
     Transactions,
