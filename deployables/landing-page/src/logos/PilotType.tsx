39f42c156cee47c92443c2d2c365178b03db7241
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-09 13:49:38 +0100

    use shared typescript config in landing-page, fix issues

--- deployables/landing-page/src/logos/PilotType.tsx
+++ deployables/landing-page/src/logos/PilotType.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react'
+import type { ComponentPropsWithoutRef } from 'react'
 
 export const PilotType = (props: ComponentPropsWithoutRef<'svg'>) => (
   <svg
