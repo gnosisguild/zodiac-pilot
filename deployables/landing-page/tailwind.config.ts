be7290da0b9d19e86d33304ee58193b0297188ad
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-09 12:39:38 +0100

    remove important modifier

--- deployables/landing-page/tailwind.config.ts
+++ deployables/landing-page/tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from 'tailwindcss'
 
 const tailwindConfig = {
   ...zodiacTailwindConfig,
-  important: true,
   content: [...zodiacTailwindConfig.content, './index.html'],
 } satisfies Config
 
