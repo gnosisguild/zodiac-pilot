73e4071de12d9ba5056be206d9a795b27f5303dd
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-09 10:52:55 +0100

    also use styles from ui pcakage

--- deployables/app/tailwind.config.ts
+++ deployables/app/tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}',
+    '../../packages/ui/**/*.tsx',
+  ],
   theme: {
     extend: {
       fontFamily: {
