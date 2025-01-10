b61d572c43c9a3ac8f566052a3cde6377a078e9d
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-10 14:38:44 +0100

    define connect iframe url during build

--- deployables/extension/esbuild.mjs
+++ deployables/extension/esbuild.mjs
@@ -45,6 +45,7 @@ esbuild
       '.png': 'file',
       '.html': 'text',
     },
+
     target: ['chrome96'],
     outdir: './public/build',
     publicPath: '/build',
@@ -59,6 +60,7 @@ esbuild
           ? `"http://127.0.0.1:${SERVE_PORT}/esbuild"`
           : 'false',
       global: 'window',
+      'process.env.CONNECT_IFRAME_URL': process.env.CONNECT_IFRAME_URL,
     },
     plugins: [
       plugin(stdLibBrowser),
