94272e345aec109f9f1b5ccb571662f6838fd034
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-14 12:50:44 +0100

    extract fetchZodiacModules

--- packages/modules/src/ZodiacModule.ts
+++ packages/modules/src/ZodiacModule.ts
@@ -1,9 +1,12 @@
 import { KnownContracts } from '@gnosis.pm/zodiac'
 
-export type SupportedModuleType =
-  | KnownContracts.DELAY
-  | KnownContracts.ROLES_V1
-  | KnownContracts.ROLES_V2
+export const SUPPORTED_ZODIAC_MODULES = [
+  KnownContracts.DELAY,
+  KnownContracts.ROLES_V1,
+  KnownContracts.ROLES_V2,
+] as const
+
+export type SupportedModuleType = (typeof SUPPORTED_ZODIAC_MODULES)[number]
 
 export type ZodiacModule = {
   moduleAddress: string
@@ -12,7 +15,7 @@ export type ZodiacModule = {
   modules?: ZodiacModule[]
 }
 
-export const ZODIAC_MODULE_NAMES = {
+export const ZODIAC_MODULE_NAMES: Record<SupportedModuleType, string> = {
   [KnownContracts.DELAY]: 'Delay',
   [KnownContracts.ROLES_V1]: 'Roles v1',
   [KnownContracts.ROLES_V2]: 'Roles v2',
--- packages/modules/src/index.ts
+++ packages/modules/src/index.ts
@@ -1,2 +1,3 @@
-export { ZODIAC_MODULE_NAMES } from './ZodiacModule'
+export { fetchZodiacModules } from './fetchZodiacModules'
+export { SUPPORTED_ZODIAC_MODULES, ZODIAC_MODULE_NAMES } from './ZodiacModule'
 export type { SupportedModuleType, ZodiacModule } from './ZodiacModule'
