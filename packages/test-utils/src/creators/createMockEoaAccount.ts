192005517ba431968d1176a83d849c5fcde00153
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-16 10:14:13 +0100

    default starting point to zero address

--- packages/test-utils/src/creators/createMockEoaAccount.ts
+++ packages/test-utils/src/creators/createMockEoaAccount.ts
@@ -1,12 +1,12 @@
-import { AccountType, type Account } from 'ser-kit'
-import { randomAddress, randomPrefixedAddress } from './randomHex'
+import { Chain, ZERO_ADDRESS } from '@zodiac/chains'
+import { AccountType, formatPrefixedAddress, type Account } from 'ser-kit'
 
 type Eoa = Extract<Account, { type: AccountType.EOA }>
 
 export const createMockEoaAccount = (eoa: Partial<Eoa> = {}): Eoa => ({
   type: AccountType.EOA,
-  address: randomAddress(),
-  prefixedAddress: randomPrefixedAddress(),
+  address: ZERO_ADDRESS,
+  prefixedAddress: formatPrefixedAddress(Chain.ETH, ZERO_ADDRESS),
 
   ...eoa,
 })
