ab796f0bf2793bdf5f7c8d7eb2754048c9e8fff8
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-20 12:07:41 +0100

    do not refetch modules all the time

--- deployables/app/app/routes/$avatar.$chainId/modules.ts
+++ deployables/app/app/routes/$avatar.$chainId/modules.ts
@@ -1,7 +1,9 @@
 import { jsonRpcProvider, validateAddress } from '@/utils'
 import { invariantResponse } from '@epic-web/invariant'
 import { verifyChainId } from '@zodiac/chains'
+import { getOptionalString } from '@zodiac/form-data'
 import { fetchZodiacModules } from '@zodiac/modules'
+import type { ShouldRevalidateFunction } from 'react-router'
 import type { Route } from './+types/modules'
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
@@ -21,4 +23,18 @@ export const loader = async ({ params }: Route.LoaderArgs) => {
     console.error(e)
     return []
   }
+}
+
+export const shouldRevalidate: ShouldRevalidateFunction = ({ formData }) => {
+  if (formData == null) {
+    return false
+  }
+
+  const intent = getOptionalString(formData, 'intent')
+
+  if (intent == null) {
+    return false
+  }
+
+  return intent === 'UpdateAvatar' || intent === 'RemoveAvatar'
 }
