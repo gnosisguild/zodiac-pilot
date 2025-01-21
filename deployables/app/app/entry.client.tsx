99bf987551d2e973d18eb52a7b68b5b7a922adeb
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2025-01-21 10:49:30 +0100

    chore: init sentry only on production (#601)

--- deployables/app/app/entry.client.tsx
+++ deployables/app/app/entry.client.tsx
@@ -1,21 +1,22 @@
+import * as Sentry from '@sentry/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HydratedRouter } from 'react-router/dom'
 import './app.css'
 
-import * as Sentry from '@sentry/react'
-
-Sentry.init({
-  dsn: 'https://23cb3bbf49ea43ed9bbaf41199c40d1c@o4508675621912576.ingest.us.sentry.io/4508676926078976',
-  integrations: [Sentry.browserTracingIntegration()],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: [
-    'localhost',
-    /^https:\/\/app\.pilot\.gnosisguild\.org/,
-  ],
-})
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://23cb3bbf49ea43ed9bbaf41199c40d1c@o4508675621912576.ingest.us.sentry.io/4508676926078976',
+    integrations: [Sentry.browserTracingIntegration()],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [
+      'localhost',
+      /^https:\/\/app\.pilot\.gnosisguild\.org/,
+    ],
+  })
+}
 
 ReactDOM.hydrateRoot(
   document,
