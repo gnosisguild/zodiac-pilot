f5b74b75ea018c888f0e04a5cf90934b57e0d2b1
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-21 17:01:10 +0100

    modify custom jsonRpcError format

--- deployables/app/app/utils/dryRun/JsonRpcError.ts
+++ deployables/app/app/utils/dryRun/JsonRpcError.ts
@@ -1,20 +1,24 @@
 import { z } from 'zod'
 
 export interface JsonRpcError extends Error {
-  data: {
-    code: number
-    message?: string
-    data?: string
-    originalError?: JsonRpcError['data']
-  }
+  data:
+    | {
+        code: number
+        message?: string
+        data?: string
+        originalError?: Extract<JsonRpcError['data'], { code: number }>
+      }
+    | string
 }
 
 const jsonRpcErrorSchema = z.object({
-  data: z.object({
-    code: z.number(),
-    message: z.string().optional(),
-    data: z.string().optional(),
-  }),
+  data: z
+    .object({
+      code: z.number(),
+      message: z.string().optional(),
+      data: z.string().optional(),
+    })
+    .or(z.string()),
 })
 
 export const isJsonRpcError = (error: unknown): error is JsonRpcError => {
