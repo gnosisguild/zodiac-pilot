2c387e6c5d08989a90340392214eefa07e054228
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-22 10:02:01 +0100

    remove edit route

--- deployables/extension/src/panel/integrations/zodiac/rolesMultisend.ts
+++
@@ -1,6 +0,0 @@
-import { getReadOnlyProvider } from '@/providers'
-import { queryRolesV1MultiSend as baseQueryRolesV1MultiSend } from '@zodiac/modules'
-import type { ChainId } from 'ser-kit'
-
-export const queryRolesV1MultiSend = (chainId: ChainId, modAddress: string) =>
-  baseQueryRolesV1MultiSend(getReadOnlyProvider(chainId), modAddress)
--- deployables/extension/src/panel/pages/routes/edit.$routeId/wallet/injectedWallet/index.ts
+++
@@ -1,2 +0,0 @@
-export { InjectedWallet } from './InjectedWallet'
-export { InjectedWalletConnect } from './InjectedWalletConnect'
