1d4d5b5971bb869e8bc79224bf2efdd16291c9b5
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-22 13:29:15 +0100

    nicer global error boundary

--- deployables/extension/src/sentry/SentryErrorBoundary.tsx
+++ deployables/extension/src/sentry/SentryErrorBoundary.tsx
@@ -1,3 +1,4 @@
+import { Error as ErrorAlert } from '@zodiac/ui'
 import { useEffect } from 'react'
 import { useRouteError } from 'react-router'
 import { client } from './client'
@@ -9,5 +10,23 @@ export const SentryErrorBoundary = () => {
     client.captureException(error)
   }, [error])
 
-  return null
+  if (error instanceof Error) {
+    return (
+      <div className="m-4">
+        <ErrorAlert title={error.message}>
+          <pre className="overflow-auto text-xs">{error.stack}</pre>
+        </ErrorAlert>
+      </div>
+    )
+  }
+
+  return (
+    <div className="m-4">
+      <ErrorAlert title="Something went wrong">
+        <pre className="overflow-auto text-xs">
+          {JSON.stringify(error, undefined, 2)}
+        </pre>
+      </ErrorAlert>
+    </div>
+  )
 }
