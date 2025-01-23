0e35efe9532a2610e0691ce6e0b2afeb7bed63d1
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-23 13:15:02 +0100

    make app e2e work with vercel

--- deployables/app/playwright.config.ts
+++ deployables/app/playwright.config.ts
@@ -23,6 +23,10 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3040',
 
+    extraHTTPHeaders: {
+      'x-vercel-protection-bypass': process.env.VERCEL_PROTECTION_BYPASS || '',
+    },
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
