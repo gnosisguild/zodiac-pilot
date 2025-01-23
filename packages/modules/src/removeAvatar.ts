8d53ca421c88648e0dc6d88af6a468b53a9f5185
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-23 09:46:28 +0100

    remove waypoints when avatar is removed

--- packages/modules/src/removeAvatar.ts
+++ packages/modules/src/removeAvatar.ts
@@ -1,6 +1,6 @@
 import { getChainId, ZERO_ADDRESS } from '@zodiac/chains'
 import type { ExecutionRoute } from '@zodiac/schema'
-import { AccountType, formatPrefixedAddress } from 'ser-kit'
+import { formatPrefixedAddress } from 'ser-kit'
 
 export const removeAvatar = (route: ExecutionRoute): ExecutionRoute => {
   const chainId = getChainId(route.avatar)
@@ -9,16 +9,11 @@ export const removeAvatar = (route: ExecutionRoute): ExecutionRoute => {
     return { ...route, avatar: formatPrefixedAddress(chainId, ZERO_ADDRESS) }
   }
 
-  const [startingPoint, ...waypoints] = route.waypoints
+  const [startingPoint] = route.waypoints
 
   return {
     ...route,
     avatar: formatPrefixedAddress(chainId, ZERO_ADDRESS),
-    waypoints: [
-      startingPoint,
-      ...waypoints.filter(
-        (waypoint) => waypoint.account.type !== AccountType.SAFE,
-      ),
-    ],
+    waypoints: [startingPoint],
   }
 }
