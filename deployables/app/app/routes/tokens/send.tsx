06e984823ab7993404a7d0e9e90e416480b102ed
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-24 14:05:35 +0100

    show error

--- deployables/app/app/routes/tokens/send.tsx
+++ deployables/app/app/routes/tokens/send.tsx
@@ -1,6 +1,7 @@
 import { getHexString, getString } from '@zodiac/form-data'
 import {
   AddressInput,
+  Error,
   Form,
   PilotType,
   PrimaryButton,
@@ -11,10 +12,10 @@ import { useSendTransaction } from 'wagmi'
 import { TokenValueInput } from './TokenValueInput'
 
 const Send = () => {
-  const { sendTransaction, isPending } = useSendTransaction()
+  const { sendTransaction, isPending, error } = useSendTransaction()
 
   return (
-    <div className="flex h-full flex-col">
+    <div className="flex h-full flex-col overflow-y-auto pb-16">
       <header className="mx-auto my-16 flex w-3/4 items-center justify-between gap-4 md:w-1/2 2xl:w-1/4">
         <div className="flex items-center gap-4">
           <ZodiacOsPlain className="h-6 lg:h-8" />
@@ -49,6 +50,8 @@ const Send = () => {
               Send
             </PrimaryButton>
           </Form.Actions>
+
+          {error && <Error title={error.message}>{error.stack}</Error>}
         </Form>
       </main>
     </div>
