98a951d092f2e07ca645d1b7558dfb0583a5905f
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-27 13:12:14 +0100

    fix: do not try to open port to edit route page

--- deployables/extension/src/utils/isValidTab.ts
+++ deployables/extension/src/utils/isValidTab.ts
@@ -1,5 +1,10 @@
+import { getCompanionAppUrl } from '@zodiac/env'
+
 export const isValidTab = (url: string | undefined) =>
-  url != null && url !== '' && isValidProtocol(url)
+  url != null && url !== '' && isValidProtocol(url) && isValidPage(url)
 
 const isValidProtocol = (url: string) =>
   ['chrome:', 'about:'].every((protocol) => !url.startsWith(protocol))
+
+const isValidPage = (url: string) =>
+  !url.startsWith(`${getCompanionAppUrl()}/edit-route`)
