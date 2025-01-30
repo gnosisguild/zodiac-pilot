11f7324ef0663ce6167a38b261b0378157cb8164
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-30 10:17:33 +0100

    improve spelling

--- packages/modules/src/createAccount.spec.ts
+++ packages/modules/src/createAccount.spec.ts
@@ -1,6 +1,6 @@
 import { Chain } from '@zodiac/chains'
 import { getDefaultProvider } from 'ethers'
-import { AccountType } from 'ser-kit'
+import { AccountType, formatPrefixedAddress } from 'ser-kit'
 import { describe, expect, it } from 'vitest'
 import { createAccount } from './createAccount'
 
@@ -8,27 +8,25 @@ describe('createAccount', () => {
   const ethProvider = getDefaultProvider(1)
 
   it('creates an EAO account if the passed address does not have code', async () => {
-    const account = await createAccount(
-      ethProvider,
-      '0xd8da6bf26964af9d7eed9e03e53415d37aa96045', // vitalik's EOA address
-    )
+    const vitalikEoaAddress = '0xd8da6bf26964af9d7eed9e03e53415d37aa96045'
+
+    const account = await createAccount(ethProvider, vitalikEoaAddress)
 
     expect(account).toEqual({
       type: AccountType.EOA,
-      address: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
-      prefixedAddress: `eoa:0xd8da6bf26964af9d7eed9e03e53415d37aa96045`,
+      address: vitalikEoaAddress,
+      prefixedAddress: formatPrefixedAddress(undefined, vitalikEoaAddress),
     })
   })
   it('creates a Safe account if the passed address has code', async () => {
-    const account = await createAccount(
-      ethProvider,
-      '0x849d52316331967b6ff1198e5e32a0eb168d039d', // Gnosis DAO active treasury mgmt Safe address
-    )
+    const gnosisDaoTreasury = '0x849d52316331967b6ff1198e5e32a0eb168d039d'
+
+    const account = await createAccount(ethProvider, gnosisDaoTreasury)
 
     expect(account).toEqual({
       type: AccountType.SAFE,
-      address: '0x849d52316331967b6ff1198e5e32a0eb168d039d',
-      prefixedAddress: 'eth:0x849d52316331967b6ff1198e5e32a0eb168d039d',
+      address: gnosisDaoTreasury,
+      prefixedAddress: formatPrefixedAddress(Chain.ETH, gnosisDaoTreasury),
       threshold: NaN,
       chain: Chain.ETH,
     })
