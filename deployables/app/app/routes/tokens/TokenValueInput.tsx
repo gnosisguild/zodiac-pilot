63adfa2415f3f97c67062f7e394062608bdfa74c
Philipp Giese <giese.philipp+git@gmail.com>
2025-01-30 14:42:24 +0100

    TokenValueInput writes value as string representation of bigint

--- deployables/app/app/routes/tokens/TokenValueInput.tsx
+++ deployables/app/app/routes/tokens/TokenValueInput.tsx
@@ -1,4 +1,4 @@
-import { useTokenBalances } from '@/balances-client'
+import { useTokenBalances, type TokenBalance } from '@/balances-client'
 import { invariant } from '@epic-web/invariant'
 import {
   GhostButton,
@@ -6,69 +6,90 @@ import {
   Select,
   type NumberInputProps,
 } from '@zodiac/ui'
-import { useState } from 'react'
-import { formatUnits } from 'viem'
+import { useEffect, useState } from 'react'
+import { formatUnits, parseUnits } from 'viem'
 
 type TokenValueInputProps = Omit<
   NumberInputProps,
   'value' | 'onChange' | 'defaultValue' | 'after'
 >
 
-export const TokenValueInput = (props: TokenValueInputProps) => {
+export const TokenValueInput = ({ name, ...props }: TokenValueInputProps) => {
   const [maxBalance, setMaxBalance] = useState<string | null>(null)
   const [tokenBalances] = useTokenBalances()
+  const [selectedToken, setSelectedToken] = useState<TokenBalance | null>(null)
 
   const [amount, setAmount] = useState('')
 
+  useEffect(() => {
+    if (selectedToken == null) {
+      return
+    }
+
+    setMaxBalance(
+      formatUnits(BigInt(selectedToken.balance), selectedToken.decimals),
+    )
+  }, [selectedToken])
+
   return (
-    <NumberInput
-      {...props}
-      value={amount}
-      onChange={(ev) => setAmount(ev.target.value)}
-      step="any"
-      min={0}
-      max={maxBalance == null ? undefined : maxBalance}
-      after={
-        <div className="mr-1">
-          <GhostButton
-            size="tiny"
-            disabled={maxBalance == null}
-            onClick={() => {
-              invariant(maxBalance != null, 'Balance is not loaded')
-              setAmount(maxBalance)
-            }}
-          >
-            Max
-          </GhostButton>
+    <>
+      <input
+        type="hidden"
+        name={name}
+        value={
+          selectedToken == null
+            ? ''
+            : parseUnits(amount, selectedToken.decimals).toString()
+        }
+      />
 
-          <Select
-            isMulti={false}
-            label="Available tokens"
-            onChange={(value) => {
-              if (value == null) {
-                return
-              }
+      <NumberInput
+        {...props}
+        value={amount}
+        onChange={(ev) => setAmount(ev.target.value)}
+        step="any"
+        min={0}
+        max={maxBalance == null ? undefined : maxBalance}
+        after={
+          <div className="mr-1">
+            <GhostButton
+              size="tiny"
+              disabled={maxBalance == null}
+              onClick={() => {
+                invariant(maxBalance != null, 'Balance is not loaded')
+                setAmount(maxBalance)
+              }}
+            >
+              Max
+            </GhostButton>
 
-              const balance = tokenBalances.find(
-                ({ name }) => name === value.value,
-              )
+            <Select
+              isMulti={false}
+              label="Available tokens"
+              onChange={(value) => {
+                if (value == null) {
+                  return
+                }
 
-              invariant(
-                balance != null,
-                `Could not find a token balance for "${value.value}"`,
-              )
+                const token = tokenBalances.find(
+                  ({ name }) => name === value.value,
+                )
 
-              setMaxBalance(
-                formatUnits(BigInt(balance.balance), balance.decimals),
-              )
-            }}
-            options={tokenBalances.map(({ name }) => ({
-              value: name,
-              label: name,
-            }))}
-          />
-        </div>
-      }
-    />
+                invariant(
+                  token != null,
+                  `Could not find a token for "${value.value}"`,
+                )
+
+                setSelectedToken(token)
+              }}
+              options={tokenBalances.map(({ name }) => ({
+                value: name,
+                label: name,
+              }))}
+            />
+          </div>
+        }
+      />
+    </>
   )
 }
