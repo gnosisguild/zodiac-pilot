88fe0ec703bb1558df48c86d89838ad24da01a11
Philipp Giese <giese.philipp+git@gmail.com>
2025-02-05 10:25:04 +0100

    adjust E2E test

--- deployables/app/e2e/smoketest.spec.ts
+++ deployables/app/e2e/smoketest.spec.ts
@@ -4,6 +4,6 @@ test('connection to example app', async ({ page }) => {
   await page.goto('/new-route')
 
   await expect(
-    page.getByRole('heading', { name: 'Route configuration' }),
+    page.getByRole('heading', { name: 'Edit route' }),
   ).toBeInViewport()
 })
