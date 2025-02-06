89068baa20eec72f4f0a8c1dda9eb245c53bdfc8
Philipp Giese <giese.philipp+git@gmail.com>
2025-02-06 10:23:40 +0100

    wip new e2e test

---
+++ deployables/app/e2e/edit-route/saveRoute.spec.ts
@@ -0,0 +1,17 @@
+import test, { expect } from '@playwright/test'
+import { loadExtension } from '../utils'
+
+test.describe('Edit route', () => {
+  test('it is possible to save a route', async ({ page }) => {
+    await page.goto('/new-route')
+
+    const extension = await loadExtension(page)
+
+    await page.getByRole('textbox', { name: 'Label' }).fill('New route')
+    await page.getByRole('button', { name: 'Save & Close' }).click()
+
+    await expect(
+      extension.getByRole('heading', { name: 'New route' }),
+    ).toBeInViewport()
+  })
+})
