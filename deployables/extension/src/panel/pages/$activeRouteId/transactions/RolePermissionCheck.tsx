4f98f9001ec8fc9acc79e0a2fd27a77cce2b3a9a
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-12 14:24:32 +0100

    add link to roles app for viewing recorded calls

--- deployables/extension/src/panel/pages/$activeRouteId/transactions/RolePermissionCheck.tsx
+++ deployables/extension/src/panel/pages/$activeRouteId/transactions/RolePermissionCheck.tsx
@@ -4,15 +4,54 @@ import type { TransactionState } from '@/state'
 import { useApplicableTranslation } from '@/transaction-translation'
 import { invariant } from '@epic-web/invariant'
 import { EOA_ZERO_ADDRESS } from '@zodiac/chains'
-import { CopyToClipboard, Tag } from '@zodiac/ui'
-import { Check, TriangleAlert, UsersRound } from 'lucide-react'
+import type { ExecutionRoute } from '@zodiac/schema'
+import {
+  CopyToClipboard,
+  errorToast,
+  GhostButton,
+  GhostLinkButton,
+  Tag,
+} from '@zodiac/ui'
+import {
+  CassetteTape,
+  Check,
+  SquareArrowOutUpRight,
+  TriangleAlert,
+  UsersRound,
+} from 'lucide-react'
 import { useEffect, useState } from 'react'
 import {
+  AccountType,
   checkPermissions,
+  ConnectionType,
   PermissionViolation,
   type Route as SerRoute,
 } from 'ser-kit'
+import {
+  recordCalls,
+  useRoleRecordLink,
+} from '../../../integrations/zodiac/roles'
 import { Translate } from './Translate'
+
+const extractRoles = (route: ExecutionRoute) => {
+  return (
+    route.waypoints?.flatMap((wp) => {
+      if (
+        wp.account.type === AccountType.ROLES &&
+        'connection' in wp &&
+        wp.connection.type === ConnectionType.IS_MEMBER
+      ) {
+        return {
+          rolesMod: wp.account.prefixedAddress,
+          version: wp.account.version,
+          roles: wp.connection.roles,
+          defaultRole: wp.connection.defaultRole,
+        }
+      }
+      return []
+    }) || []
+  )
+}
 
 type Props = {
   transactionState: TransactionState
@@ -58,7 +97,38 @@ export const RolePermissionCheck = ({
     }
   }, [transactionState, route, provider])
 
-  
+  // if the role is unambiguous and from a v2 Roles module, we can record a permissions request to the Roles app
+  const roleToRecordToCandidates = extractRoles(route).filter(
+    (r) => r.version === 2 && (r.defaultRole || r.roles.length === 1),
+  )
+  const roleToRecordTo =
+    roleToRecordToCandidates.length === 1
+      ? {
+          rolesMod: roleToRecordToCandidates[0].rolesMod,
+          roleKey:
+            roleToRecordToCandidates[0].defaultRole ||
+            roleToRecordToCandidates[0].roles[0],
+        }
+      : undefined
+
+  const roleRecordLink = useRoleRecordLink(roleToRecordTo)
+
+  const [recordCallPending, setRecordCallPending] = useState(false)
+  const recordCall = async () => {
+    invariant(roleToRecordTo, 'No role to record to')
+    setRecordCallPending(true)
+    try {
+      await recordCalls([transactionState.transaction], roleToRecordTo)
+    } catch (e) {
+      errorToast({
+        id: 'roles-record-call-error',
+        title: 'Error recording call',
+        message: (e as Error).message,
+      })
+    } finally {
+      setRecordCallPending(false)
+    }
+  }
 
   if (error === undefined) return null
 
@@ -89,27 +159,46 @@ export const RolePermissionCheck = ({
             </Tag>
           )}
 
-          {error && (
-            <>
-              {translationAvailable ? (
-                <Translate transactionId={transactionState.id} />
-              ) : (
-                <CopyToClipboard
-                  iconOnly
-                  size="small"
-                  data={transactionState.transaction}
-                >
-                  Copy transaction data to the clipboard
-                </CopyToClipboard>
-              )}
-            </>
+          {error && translationAvailable && (
+            <Translate transactionId={transactionState.id} />
           )}
 
           {error && (
-
+            <CopyToClipboard
+              iconOnly
+              size="small"
+              data={transactionState.transaction}
+            >
+              Copy transaction data to the clipboard
+            </CopyToClipboard>
           )}
         </div>
       </div>
+
+      {error && roleToRecordTo && (
+        <div className="flex gap-2">
+          <GhostButton
+            fluid
+            icon={CassetteTape}
+            onClick={recordCall}
+            disabled={recordCallPending}
+          >
+            Record call to request permissions
+          </GhostButton>
+
+          {roleRecordLink && (
+            <GhostLinkButton
+              openInNewWindow
+              iconOnly
+              size="small"
+              icon={SquareArrowOutUpRight}
+              to={roleRecordLink}
+            >
+              View recorded calls
+            </GhostLinkButton>
+          )}
+        </div>
+      )}
     </div>
   )
 }
