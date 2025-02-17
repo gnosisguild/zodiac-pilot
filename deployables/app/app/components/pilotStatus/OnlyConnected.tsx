9ea4999856f7e7bfd24014fe3df4c973cbf890fe
Philipp Giese <giese.philipp+git@gmail.com>
2025-02-17 09:41:01 +0100

    only show connected to pilot warning after a delay

--- deployables/app/app/components/pilotStatus/OnlyConnected.tsx
+++ deployables/app/app/components/pilotStatus/OnlyConnected.tsx
@@ -1,12 +1,26 @@
 import { Info } from '@zodiac/ui'
 import type { PropsWithChildren } from 'react'
+import { useEffect, useState } from 'react'
 import { useConnected } from './PilotStatusContext'
 
 export const OnlyConnected = ({ children }: PropsWithChildren) => {
   const connected = useConnected()
+  const [delayed, setDelayed] = useState(true)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDelayed(false)
+    }, 1000)
+
+    return () => clearTimeout(timeout)
+  }, [])
 
   if (connected) {
     return children
+  }
+
+  if (delayed) {
+    return null
   }
 
   return (
