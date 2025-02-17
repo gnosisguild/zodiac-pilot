e2fd599c13589fa92e1811852567eee3cbd1a9e8
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2025-02-17 09:33:30 +0100

    feat(pilot-app,extension): new entry to create account (#845)
    Fixes #841

--- packages/form-data/src/getHexString.ts
+++ packages/form-data/src/getHexString.ts
@@ -1,5 +1,16 @@
 import { verifyHexAddress } from '@zodiac/schema'
+import { getOptionalString } from './getOptionalString'
 import { getString } from './getString'
 
 export const getHexString = (data: FormData, key: string) =>
   verifyHexAddress(getString(data, key))
+
+export const getOptionalHexString = (data: FormData, key: string) => {
+  const value = getOptionalString(data, key)
+
+  if (value == null) {
+    return null
+  }
+
+  return verifyHexAddress(value)
+}
