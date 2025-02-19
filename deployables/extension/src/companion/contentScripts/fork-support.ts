583cf8045e2af526ce5aac77b4ebe693cd4c157e
Philipp Giese <giese.philipp+git@gmail.com>
2025-02-19 12:52:15 +0100

    split companion content script

---
+++ deployables/extension/src/companion/contentScripts/fork-support.ts
@@ -0,0 +1,8 @@
+import {
+  CompanionAppMessageType,
+  type CompanionAppMessage,
+} from '@zodiac/messages'
+
+chrome.runtime.sendMessage<CompanionAppMessage>({
+  type: CompanionAppMessageType.REQUEST_FORK_INFO,
+})
