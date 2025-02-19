676a17dcbf916f1f3544be008f36990c2db343a4
Philipp Giese <giese.philipp+git@gmail.com>
2025-02-19 13:57:12 +0100

    adjust e2e

--- deployables/app/e2e/edit-route/saveRoute.spec.ts
+++ deployables/app/e2e/edit-route/saveRoute.spec.ts
@@ -1,13 +1,15 @@
+import { encode } from '@zodiac/schema'
 import { expect, loadExtension, test } from '../utils'
+import { route } from './fixture'
 
 test.describe('Edit route', () => {
   test('it is possible to save a route', async ({ page }) => {
-    await page.goto('/new-route')
+    await page.goto(`/edit/${encode(route)}`)
 
     const extension = await loadExtension(page)
 
     await page.getByRole('textbox', { name: 'Label' }).fill('New route')
-    await page.getByRole('button', { name: 'Save & Close' }).click()
+    await page.getByRole('button', { name: 'Save' }).click()
 
     await expect(
       extension.getByRole('heading', { name: 'New route' }),
