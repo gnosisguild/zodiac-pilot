b36599b1f8e3ea5eab9ca670f7bfa8171caea66e
Philipp Giese <giese.philipp+git@gmail.com>
2025-02-24 11:05:41 +0100

    show error details in dev

---
+++ deployables/app/app/routes/errorBoundary.tsx
@@ -0,0 +1,51 @@
+import { Page, useIsDev } from '@/components'
+import { Error as ErrorAlert, Info } from '@zodiac/ui'
+import { isRouteErrorResponse, Outlet, useRouteError } from 'react-router'
+
+const Main = () => <Outlet />
+
+export default Main
+
+export const ErrorBoundary = () => {
+  const isDev = useIsDev()
+
+  return (
+    <Page fullWidth>
+      <Page.Main>
+        <div className="mx-auto flex h-full w-2/3 flex-1 flex-col items-center justify-center gap-8">
+          <ErrorAlert title="The Pilot app encountered an error">
+            This part of the website is not working as intended. Please reload
+            the page and if the error persists please reach out to us on Discord
+            for further assistance.
+          </ErrorAlert>
+
+          {isDev && <ErrorMessage />}
+        </div>
+      </Page.Main>
+    </Page>
+  )
+}
+
+const ErrorMessage = () => {
+  const error = useRouteError()
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <Info title={error.status === 404 ? '404' : 'Error'}>
+        {error.statusText}
+      </Info>
+    )
+  }
+
+  if (error instanceof Error) {
+    return (
+      <Info title={error.message}>
+        <div className="max-w-full overflow-scroll">
+          <pre>{error.stack}</pre>
+        </div>
+      </Info>
+    )
+  }
+
+  return null
+}
