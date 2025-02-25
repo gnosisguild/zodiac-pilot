8eecc4ecbd2193ff6b8292c8feeb9f47846a6e41
Philipp Giese <giese.philipp+git@gmail.com>
2025-02-25 09:53:17 +0100

    launch route from incoming message

--- deployables/extension/src/panel/pages/Root.tsx
+++ deployables/extension/src/panel/pages/Root.tsx
@@ -4,6 +4,7 @@ import { getCompanionAppUrl } from '@zodiac/env'
 import { Outlet, useLoaderData, useNavigate } from 'react-router'
 import { FutureClearTransactionsModal } from './ClearTransactionsModal'
 import { useDeleteRoute } from './useDeleteRoute'
+import { useLaunchRoute } from './useLaunchRoute'
 import { useSaveRoute } from './useSaveRoute'
 
 export const loader = async () => {
@@ -17,12 +18,19 @@ export const Root = () => {
   const { isUpdatePending, cancelUpdate, saveUpdate } =
     useSaveRoute(lastUsedRouteId)
   useDeleteRoute()
+  const { isLaunchPending, cancelLaunch, proceedWithLaunch } = useLaunchRoute()
 
   const navigate = useNavigate()
 
   return (
     <ProvideCompanionAppContext url={companionAppUrl}>
       <Outlet />
+
+      <FutureClearTransactionsModal
+        open={isLaunchPending}
+        onCancel={cancelLaunch}
+        onAccept={proceedWithLaunch}
+      />
 
       <FutureClearTransactionsModal
         open={isUpdatePending}
