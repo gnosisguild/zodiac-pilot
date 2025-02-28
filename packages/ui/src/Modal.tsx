7c0b29d379ca7445d2bc87d603d9c65680048409
Philipp Giese <giese.philipp+git@gmail.com>
2025-02-28 12:06:42 +0100

    chore: make modal look better on large screens

--- packages/ui/src/Modal.tsx
+++ packages/ui/src/Modal.tsx
@@ -30,12 +30,12 @@ export const Modal = ({
       transition
       open={open}
       onClose={onClose}
-      className="backdrop-blur-xs data-closed:backdrop-blur-0 absolute inset-0 z-50 bg-zinc-200/20 transition-all duration-300 ease-out dark:bg-slate-900/20"
+      className="backdrop-blur-xs data-closed:backdrop-blur-0 absolute inset-0 z-50 bg-zinc-200/20 transition-all duration-300 ease-out md:duration-700 dark:bg-slate-900/20"
     >
-      <div className="fixed inset-0 flex h-full w-screen flex-col items-center justify-end px-4">
+      <div className="fixed inset-0 flex h-full w-screen flex-col items-center justify-end px-4 md:justify-center">
         <DialogPanel
           transition
-          className="data-closed:translate-y-full data-closed:opacity-0 w-full space-y-4 rounded-t-xl border-x border-t border-zinc-200/80 bg-zinc-900 px-2 pb-4 pt-2 text-sm shadow-lg transition-all dark:border-zinc-900/80 dark:bg-zinc-100"
+          className="data-closed:translate-y-full md:data-closed:translate-y-0 md:data-closed:scale-125 data-closed:opacity-0 w-full space-y-4 rounded-t-xl border-x border-t border-zinc-200/80 bg-zinc-900 px-2 pb-4 pt-2 text-sm shadow-lg transition-all md:w-1/3 md:rounded-b-xl dark:border-zinc-900/80 dark:bg-zinc-100"
         >
           <div className="flex items-center justify-between gap-4 text-zinc-50 dark:text-zinc-800">
             <DialogTitle className="pl-2 font-bold">{title}</DialogTitle>
