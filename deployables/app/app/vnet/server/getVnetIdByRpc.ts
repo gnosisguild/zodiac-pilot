43c9aaaf6e496010782bf2029a6dbfcd33c4d3d7
juliopavila <javila1997@gmail.com>
2025-03-05 09:34:41 -0300

    chore: solved check types

--- deployables/app/app/vnet/server/getVnetIdByRpc.ts
+++ deployables/app/app/vnet/server/getVnetIdByRpc.ts
@@ -3,8 +3,8 @@ import { getVnetList } from './getVnetList'
 export const getVnetIdByRpc = async (
   rpcUrl: string,
 ): Promise<string | null> => {
-  const vnets = await getVnetList()
-  for (const vnet of vnets) {
+  const vnetList = await getVnetList()
+  for (const vnet of vnetList) {
     for (const rpc of vnet.rpcs) {
       if (rpc.url === rpcUrl) {
         return vnet.id
