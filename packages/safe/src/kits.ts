41277feafb8556771584f0f6cd649e2a881f23fd
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2025-03-06 10:23:04 +0100

    fix(extension): add back esm interop for safe packages (#990)
    Fixes #988

--- packages/safe/src/kits.ts
+++ packages/safe/src/kits.ts
@@ -23,15 +23,32 @@ export const initSafeApiKit = (chainId: ChainId): SafeApiKit => {
     throw new Error(`service not available for chain #${chainId}`)
   }
 
+  // @ts-expect-error SafeApiKit is only available as a CJS module. That doesn't play super nice with us being ESM.
+  if (SafeApiKit.default) {
+    // @ts-expect-error See above
+    return new SafeApiKit.default({ txServiceUrl, chainId: BigInt(chainId) })
+  }
+
   return new SafeApiKit({ txServiceUrl, chainId: BigInt(chainId) })
 }
 
 export const initSafeProtocolKit = (
   chainId: ChainId,
   safeAddress: string,
-): Promise<Safe> =>
-  Safe.init({
+): Promise<Safe> => {
+  // @ts-expect-error protocol-kit is only available as a CJS module. That doesn't play super nice with us being ESM.
+  if (Safe.default) {
+    // @ts-expect-error protocol-kit is only available as a CJS module. That doesn't play super nice with us being ESM.
+    return Safe.default.init({
+      // we must pass the RPC endpoint as a string. If we pass an EIP1193 provider, Safe will send eth_requestAccounts calls (which will fail)
+      provider: RPC[chainId],
+      safeAddress,
+    })
+  }
+
+  return Safe.init({
     // we must pass the RPC endpoint as a string. If we pass an EIP1193 provider, Safe will send eth_requestAccounts calls (which will fail)
     provider: RPC[chainId],
     safeAddress,
   })
+}
