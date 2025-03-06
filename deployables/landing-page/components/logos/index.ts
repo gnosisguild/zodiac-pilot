93da78b7f16e4611abe5ac4673f4e27bd72a8367
Philipp Giese <giese.philipp+git@gmail.com>
2025-03-06 16:33:57 +0100

    add logos

---
+++ deployables/landing-page/components/logos/index.ts
@@ -0,0 +1,7 @@
+export { BalancerLogo } from './BalancerLogo'
+export { EnsLogo } from './EnsLogo'
+export { GnosisLogo } from './GnosisLogo'
+export { HatsLogo } from './HatsLogo'
+export { KarpatkeyLogo } from './karpatkeyLogo'
+export { RethinkLogo } from './RethinkLogo'
+export { SafeLogo } from './SafeLogo'
