9ce583d87c02d6e7761762d6d09760a7820fa83b
Philipp Giese <giese.philipp+git@gmail.com>
2025-03-10 10:16:10 +0100

    adjust specs

--- deployables/app/e2e/edit-route/saveRoute.spec.ts
+++ deployables/app/e2e/edit-route/saveRoute.spec.ts
@@ -4,15 +4,13 @@ import { route } from './fixture'
 
 test.describe('Edit route', () => {
   test('it is possible to save a route', async ({ page }) => {
-    await page.goto(`/edit/${encode(route)}`)
+    await page.goto(`/edit/${route.id}/${encode(route)}`)
 
     const extension = await loadExtension(page)
 
     await page.getByRole('textbox', { name: 'Label' }).fill('New route')
     await page.getByRole('button', { name: 'Save' }).click()
 
-    await expect(
-      extension.getByRole('heading', { name: 'New route' }),
-    ).toBeInViewport()
+    await expect(extension.getByText('New route')).toBeInViewport()
   })
 })
--- deployables/app/e2e/smoketest.spec.ts
+++ deployables/app/e2e/smoketest.spec.ts
@@ -1,11 +1,11 @@
-import { loadExtension, test } from './utils'
+import { expect, loadExtension, test } from './utils'
 
 test('connection to companion app', async ({ page }) => {
+  await page.goto('/create')
+
   await loadExtension(page)
 
-  // await page.goto('/create')
-
-  // await expect(
-  //   page.getByRole('heading', { name: 'New Account' }),
-  // ).toBeInViewport()
+  await expect(
+    page.getByRole('heading', { name: 'New Account' }),
+  ).toBeInViewport()
 })
