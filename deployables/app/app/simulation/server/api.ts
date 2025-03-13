706aa3ca5075d132a3b922821b5b25f1a1200ee3
juliopavila <javila1997@gmail.com>
2025-03-13 20:00:21 -0300

    feat (pilot-app):  introduce tenderly simulation

---
+++ deployables/app/app/simulation/server/api.ts
@@ -0,0 +1,44 @@
+import { getTenderlyCredentials } from '@zodiac/env'
+import type { z, ZodTypeAny } from 'zod'
+
+type ApiOptions<Schema extends ZodTypeAny> = {
+  schema: Schema
+  data?: Record<string, string | number | boolean | string[] | number[]>
+  body?: Record<string, unknown>
+  method?: 'GET' | 'POST'
+}
+
+export const api = async <Schema extends ZodTypeAny>(
+  endpoint: `/${string}`,
+  { schema, data = {}, method = 'GET', body }: ApiOptions<Schema>,
+) => {
+  const { TENDERLY_ACCESS_KEY, TENDERLY_PROJECT, TENDERLY_USER } =
+    getTenderlyCredentials()
+
+  const url = new URL(
+    `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}${endpoint}`,
+  )
+  console.log('TENDERLY_ACCESS_KEY', TENDERLY_ACCESS_KEY)
+  const fetchOptions: RequestInit = {
+    method,
+    headers: {
+      'X-Access-Key': TENDERLY_ACCESS_KEY,
+      'Content-Type': 'application/json',
+    },
+  }
+
+  Object.entries(data).forEach(([key, value]) =>
+    Array.isArray(value)
+      ? value.forEach((entry) => url.searchParams.append(key, entry.toString()))
+      : url.searchParams.set(key, value.toString()),
+  )
+
+  if (method !== 'GET') {
+    fetchOptions.body = JSON.stringify(body)
+  }
+
+  const response = await fetch(url, fetchOptions)
+
+  const json = await response.json()
+  return schema.parse(json) as z.infer<Schema>
+}
---
+++ deployables/app/app/simulation/server/helper.ts
@@ -0,0 +1,51 @@
+import type { TokenTransfer } from '@/balances-client'
+import { getChain, getTokenBalance } from '@/balances-server'
+import { verifyChainId } from '@zodiac/chains'
+import type { HexAddress } from '@zodiac/schema'
+import { formatUnits } from 'viem'
+import type { SimulationResult } from '../types'
+
+export const extractTokenFlowsFromSimulation = async (
+  simulation: SimulationResult,
+): Promise<TokenTransfer[]> => {
+  const results = simulation.simulation_results ?? []
+
+  const flowsPerResult = await Promise.all(
+    results.map(async ({ transaction }) => {
+      const assetChanges = transaction?.transaction_info?.asset_changes
+      if (!assetChanges?.length) {
+        return []
+      }
+
+      const chainId = parseInt(transaction.network_id)
+      const chain = await getChain(verifyChainId(chainId))
+
+      return Promise.all(
+        assetChanges.map(async (change) => {
+          const isErc20 = change.token_info.standard === 'ERC20'
+          const tokenAddress = isErc20
+            ? change.token_info.contract_address
+            : change.token_info.symbol
+
+          const tokenDetails = await getTokenBalance(
+            chain,
+            tokenAddress as HexAddress,
+          )
+          const formattedAmount = formatUnits(
+            BigInt(change.raw_amount),
+            tokenDetails.decimals,
+          )
+
+          return {
+            ...tokenDetails,
+            from: change.from as HexAddress,
+            to: change.to as HexAddress,
+            token: tokenAddress,
+            amount: formattedAmount,
+          } as TokenTransfer
+        }),
+      )
+    }),
+  )
+  return flowsPerResult.flat()
+}
