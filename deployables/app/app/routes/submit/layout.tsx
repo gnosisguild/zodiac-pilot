5266cba8b95d0c32178687016222d53f917d109e
juliopavila <javila1997@gmail.com>
2025-03-17 08:53:12 -0300

    chore: remove old code

--- deployables/app/app/routes/submit/layout.tsx
+++ deployables/app/app/routes/submit/layout.tsx
@@ -1,28 +1,25 @@
-import { ProvideForkContext } from '@/balances-client'
 import { ConnectWalletButton, Page, WalletProvider } from '@/components'
 import { Outlet } from 'react-router'
 
 const SubmitLayout = () => (
-  <ProvideForkContext>
-    <WalletProvider>
-      <Page fullWidth>
-        <Page.Header
-          action={
-            <ConnectWalletButton
-              connectLabel="Connect signer wallet"
-              connectedLabel="Signer wallet"
-            />
-          }
-        >
-          Sign Transaction Bundle
-        </Page.Header>
+  <WalletProvider>
+    <Page fullWidth>
+      <Page.Header
+        action={
+          <ConnectWalletButton
+            connectLabel="Connect signer wallet"
+            connectedLabel="Signer wallet"
+          />
+        }
+      >
+        Sign Transaction Bundle
+      </Page.Header>
 
-        <Page.Main>
-          <Outlet />
-        </Page.Main>
-      </Page>
-    </WalletProvider>
-  </ProvideForkContext>
+      <Page.Main>
+        <Outlet />
+      </Page.Main>
+    </Page>
+  </WalletProvider>
 )
 
 export default SubmitLayout
