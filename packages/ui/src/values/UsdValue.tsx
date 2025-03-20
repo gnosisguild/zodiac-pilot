e72a6cce8548292689c2a807cafe24688ad25f8d
juliopavila <javila1997@gmail.com>
2025-03-20 17:35:46 -0300

    feat(pilot-app): Show balance diff

--- packages/ui/src/values/UsdValue.tsx
+++ packages/ui/src/values/UsdValue.tsx
@@ -1,10 +1,38 @@
+import { Popover } from '../overlays'
+
 const usdFormatter = new Intl.NumberFormat('en-US', {
   currency: 'USD',
   style: 'currency',
 })
 
-export const UsdValue = ({ children }: { children: number }) => (
-  <span className="text-sm slashed-zero tabular-nums">
-    {usdFormatter.format(children)}
-  </span>
-)
+export const UsdValue = ({
+  children,
+  balanceDiff,
+}: {
+  children: number
+  balanceDiff?: number
+}) => {
+  const textColor =
+    balanceDiff == null
+      ? 'text-gray-600'
+      : balanceDiff > 0
+        ? 'text-green-600 dark:text-green-400'
+        : 'text-red-600 dark:text-red-400'
+
+  return (
+    <Popover
+      inline
+      popover={
+        balanceDiff && (
+          <span className="tabular-numbs text-sm slashed-zero">
+            {usdFormatter.format(balanceDiff)}
+          </span>
+        )
+      }
+    >
+      <span className={`text-sm slashed-zero tabular-nums ${textColor}`}>
+        {usdFormatter.format(children)}
+      </span>
+    </Popover>
+  )
+}
