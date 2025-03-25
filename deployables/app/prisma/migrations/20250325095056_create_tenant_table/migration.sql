14d25fbbf976acf7a4b2b51bd6e16b8912e714e2
Philipp Giese <giese.philipp+git@gmail.com>
2025-03-25 10:52:29 +0100

    add prisma and first migration

---
+++ deployables/app/prisma/migrations/20250325095056_create_tenant_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Tenant" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Tenant_pkey" PRIMARY KEY ("id")
+);
