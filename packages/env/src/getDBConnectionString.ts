889813536ef3bc9f945933c737d5f4cbb0190e68
Philipp Giese <giese.philipp+git@gmail.com>
2025-03-27 09:12:14 +0100

    fix env package

--- packages/env/src/getDBConnectionString.ts
+++ packages/env/src/getDBConnectionString.ts
@@ -1,7 +1,7 @@
 import { invariant } from '@epic-web/invariant'
 
 export const getDBConnectionString = () => {
-  const { POSTGRES_PRISMA_URL } = process.env
+  const POSTGRES_PRISMA_URL = process.env.POSTGRES_PRISMA_URL
 
   invariant(
     POSTGRES_PRISMA_URL != null,
--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,4 +1,4 @@
 export { getCompanionAppUrl } from './getCompanionAppUrl.ts'
-export { getDBConnectionString } from './getDBConnectionString'
+export { getDBConnectionString } from './getDBConnectionString.ts'
 export { getDeBankApiKey } from './getDeBankApiKey.ts'
 export { getTenderlyCredentials } from './getTenderlyCredentials.ts'
