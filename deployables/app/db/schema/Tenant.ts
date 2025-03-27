a4e4e9f22e8d7a0526f2cc93a293a823e2afd864
Philipp Giese <giese.philipp+git@gmail.com>
2025-03-27 13:23:43 +0100

    add default to tenant id property

--- deployables/app/db/schema/Tenant.ts
+++ deployables/app/db/schema/Tenant.ts
@@ -1,7 +1,7 @@
 import { date, pgTable, text, uuid } from 'drizzle-orm/pg-core'
 
 export const Tenant = pgTable('Tenant', {
-  id: uuid().primaryKey(),
+  id: uuid().defaultRandom().primaryKey(),
   name: text().notNull(),
   createdAt: date().notNull().defaultNow(),
 })
