675c5eb52976518eabe5eff8deb7113296fcf4d6
Philipp Giese <giese.philipp+git@gmail.com>
2025-03-28 17:19:53 +0100

    better loaders and wip user profile

--- deployables/app/app/routes/auth/logout.ts
+++ deployables/app/app/routes/auth/logout.ts
@@ -1,15 +1,15 @@
 import { invariantResponse } from '@epic-web/invariant'
-import { authkitLoader, getWorkOS } from '@workos-inc/authkit-remix'
-import { redirect, type LoaderFunction } from 'react-router'
+import { authkitLoader, getWorkOS } from '@workos-inc/authkit-react-router'
+import { redirect } from 'react-router'
 import type { Route } from './+types/logout'
 
-export const loader: LoaderFunction = (args: Route.LoaderArgs) =>
+export const loader = (args: Route.LoaderArgs) =>
   authkitLoader(args, async ({ auth }) => {
     const workos = getWorkOS()
 
     invariantResponse(auth.sessionId != null, 'User is not logged in')
 
-    return redirect(
+    throw redirect(
       workos.userManagement.getLogoutUrl({
         sessionId: auth.sessionId,
         returnTo: 'http://localhost:3040',
--- deployables/app/app/routes/auth/sign-in.ts
+++ deployables/app/app/routes/auth/sign-in.ts
@@ -1,6 +1,6 @@
-import { getSignInUrl } from '@workos-inc/authkit-remix'
+import { authkitLoader, getSignInUrl } from '@workos-inc/authkit-react-router'
 import { redirect } from 'react-router'
+import type { Route } from './+types/sign-in'
 
-export const loader = async () => {
-  return redirect(await getSignInUrl())
-}
+export const loader = async (args: Route.LoaderArgs) =>
+  authkitLoader(args, async () => redirect(await getSignInUrl()))
