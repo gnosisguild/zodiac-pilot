696fc2d149af86b888d68ae3cc853fc7d151cac2
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-04-01 14:56:47 +0200

    chore: track non-truncated url in custom context

--- deployables/app/app/entry.client.tsx
+++ deployables/app/app/entry.client.tsx
@@ -7,7 +7,15 @@ if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: 'https://c39d76bbc73d7a511713c23ef37f1e94@o4508675621912576.ingest.us.sentry.io/4508676926078976',
     integrations: [],
-    maxValueLength: 5000, // enable capturing entire submit & route urls
+
+    // enable capturing entire submit & route urls (Sentry cuts off the URL is tracks by default)
+    maxValueLength: 5000,
+    beforeSend(event) {
+      event.contexts = event.contexts || {}
+      event.contexts.custom = event.contexts.custom || {}
+      event.contexts.custom.url = event.request?.url
+      return event
+    },
   })
 }
 
--- deployables/app/instrument.server.mjs
+++ deployables/app/instrument.server.mjs
@@ -2,5 +2,13 @@ import * as Sentry from '@sentry/react-router'
 
 Sentry.init({
   dsn: 'https://c39d76bbc73d7a511713c23ef37f1e94@o4508675621912576.ingest.us.sentry.io/4508676926078976',
-  maxValueLength: 5000, // enable capturing entire submit & route urls
+
+  // enable capturing entire submit & route urls (Sentry cuts off the URL is tracks by default)
+  maxValueLength: 5000,
+  beforeSend(event) {
+    event.contexts = event.contexts || {}
+    event.contexts.custom = event.contexts.custom || {}
+    event.contexts.custom.url = event.request?.url
+    return event
+  },
 })
