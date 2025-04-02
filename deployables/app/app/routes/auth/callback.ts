97e6e8021aeb063ba645f874e4e106028bf7ac87
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2025-04-02 10:06:29 +0200

    fix(pilot-app): throw sign out when something goes wrong in success callback (#1245)

--- deployables/app/app/routes/auth/callback.ts
+++ deployables/app/app/routes/auth/callback.ts
@@ -1,23 +1,32 @@
 import { createUser, dbClient, getTenant } from '@/db'
 import { getOrganizationForUser, updateExternalUserId } from '@/workOS'
-import { authLoader } from '@workos-inc/authkit-react-router'
+import { authLoader, signOut } from '@workos-inc/authkit-react-router'
+import type { Route } from './+types/callback'
 
-export const loader = authLoader({
-  async onSuccess({ user: workOSUser }) {
-    if (workOSUser.externalId != null) {
-      return
-    }
+export const loader = ({ request, ...options }: Route.LoaderArgs) => {
+  const response = authLoader({
+    async onSuccess({ user: workOSUser }) {
+      if (workOSUser.externalId != null) {
+        return
+      }
 
-    await dbClient().transaction(async (db) => {
-      const organization = await getOrganizationForUser(workOSUser.id)
+      await dbClient().transaction(async (db) => {
+        const organization = await getOrganizationForUser(workOSUser.id)
 
-      const tenant = await getTenant(db, organization.externalId)
-      const user = await createUser(db, tenant)
+        try {
+          const tenant = await getTenant(db, organization.externalId)
+          const user = await createUser(db, tenant)
 
-      await updateExternalUserId({
-        userId: workOSUser.id,
-        externalId: user.id,
+          await updateExternalUserId({
+            userId: workOSUser.id,
+            externalId: user.id,
+          })
+        } catch {
+          throw await signOut(request)
+        }
       })
-    })
-  },
-})
+    },
+  })
+
+  return response({ request, ...options })
+}
