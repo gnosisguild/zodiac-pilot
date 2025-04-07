1aeb6027ea00ee9d1fd34ff3196470a1ac5aa79f
Philipp Giese <giese.philipp+git@gmail.com>
2025-04-07 13:09:47 +0200

    moah logos

--- deployables/landing-page/components/Hero.tsx
+++ deployables/landing-page/components/Hero.tsx
@@ -1,3 +1,4 @@
+import klerosLogo from '@/images/kleros-logo.png'
 import { SiGithub, SiGooglechrome } from '@icons-pack/react-simple-icons'
 import { PrimaryLinkButton, SecondaryLinkButton } from '@zodiac/ui'
 import { GraduationCap } from 'lucide-react'
@@ -5,13 +6,17 @@ import { useEffect, useRef, type PropsWithChildren } from 'react'
 import { annotate } from 'rough-notation'
 import { Container } from './Container'
 import {
+  ArbitrumOne,
   BalancerLogo,
   EnsLogo,
   GnosisLogo,
+  GnosisPay,
   HatsLogo,
   KarpatkeyLogo,
+  OneInch,
   RethinkLogo,
   SafeLogo,
+  Tally,
 } from './logos'
 
 export function Hero({ children }: PropsWithChildren) {
@@ -48,7 +53,7 @@ export function Hero({ children }: PropsWithChildren) {
         <span className="relative z-10">Smart Accounts</span>
       </h1>
       <p className="mx-auto mb-6 mt-12 max-w-2xl text-balance text-2xl font-semibold tracking-tight text-zinc-700 dark:text-zinc-200">
-        Zodiac Pilot makes Safe execution smarter, faster, and safer.
+        Zodiac Pilot is a powerful execution interface for Safe accounts
       </p>
       <p className="mx-auto mb-12 max-w-2xl text-balance tracking-tight text-zinc-700 dark:text-zinc-400">
         Build multi-dapp transactions with modular batching, programmable
@@ -81,10 +86,14 @@ export function Hero({ children }: PropsWithChildren) {
         </SecondaryLinkButton>
       </div>
 
-      <div className="mt-24 2xl:mt-44">
+      <div className="mt-24 2xl:mt-28">
+        <h3 className="mb-16 font-semibold">
+          Zodiac is battle-tested and trusted by leading organizations
+        </h3>
+
         {children}
 
-        <ul className="mt-24 flex items-center justify-center gap-8 sm:flex-col sm:gap-x-0 sm:gap-y-10 xl:flex-row xl:gap-x-12 xl:gap-y-0 2xl:mt-44">
+        <ul className="mt-24 flex flex-col items-center justify-center gap-8 2xl:mt-44">
           <li>
             <ul className="grid grid-cols-3 flex-col items-center gap-8 sm:flex-row sm:gap-x-12 sm:gap-y-0 xl:flex">
               <li className="flex justify-center">
@@ -120,6 +129,39 @@ export function Hero({ children }: PropsWithChildren) {
               <li className="col-span-3 flex justify-center">
                 <a href="https://www.gnosis.io/dao">
                   <GnosisLogo />
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <ul className="grid grid-cols-3 flex-col items-center gap-8 sm:flex-row sm:gap-x-12 sm:gap-y-0 xl:flex">
+              <li className="flex justify-center">
+                <a href="https://arbitrum.io/">
+                  <ArbitrumOne />
+                </a>
+              </li>
+              <li className="flex justify-center">
+                <a href="https://1inch.io/">
+                  <OneInch />
+                </a>
+              </li>
+              <li className="flex justify-center">
+                <a href="https://gnosispay.com/">
+                  <GnosisPay />
+                </a>
+              </li>
+              <li className="flex justify-center">
+                <a href="https://kleros.io">
+                  <img
+                    src={klerosLogo}
+                    alt=""
+                    className="h-10 invert dark:invert-0"
+                  />
+                </a>
+              </li>
+              <li className="flex justify-center">
+                <a href="https://www.tally.xyz/">
+                  <Tally />
                 </a>
               </li>
             </ul>
