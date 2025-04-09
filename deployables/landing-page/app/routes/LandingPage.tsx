76aedf786b06db488dfe8ecb83162db1211b4219
Philipp Giese <giese.philipp+git@gmail.com>
2025-04-09 11:47:03 +0200

    render stats only when they could be retrieved

--- deployables/landing-page/app/routes/LandingPage.tsx
+++ deployables/landing-page/app/routes/LandingPage.tsx
@@ -14,34 +14,42 @@ import { invariantResponse } from '@epic-web/invariant'
 import type { Route } from './+types/LandingPage'
 
 export const loader = async ({ context }: Route.LoaderArgs) => {
-  const dune = new DuneClient(context.cloudflare.env.DUNE_ANALYTICS_API_KEY)
-  const [{ result: totalTransactedValue }, { result: totalValueSecured }] =
-    await Promise.all([
-      dune.getLatestResult({ queryId: 4175289 }),
-      dune.getLatestResult({ queryId: 4151598 }),
-    ])
+  try {
+    const dune = new DuneClient(context.cloudflare.env.DUNE_ANALYTICS_API_KEY)
+    const [{ result: totalTransactedValue }, { result: totalValueSecured }] =
+      await Promise.all([
+        dune.getLatestResult({ queryId: 4175289 }),
+        dune.getLatestResult({ queryId: 4151598 }),
+      ])
 
-  invariantResponse(totalTransactedValue != null, 'Could not load TTV')
-  invariantResponse(totalValueSecured != null, 'Could not load TVS')
+    invariantResponse(totalTransactedValue != null, 'Could not load TTV')
+    invariantResponse(totalValueSecured != null, 'Could not load TVS')
 
-  const [ttvRow] = totalTransactedValue.rows
-  const [tvsRow] = totalValueSecured.rows
+    const [ttvRow] = totalTransactedValue.rows
+    const [tvsRow] = totalValueSecured.rows
 
-  const usdFormatter = new Intl.NumberFormat('en-us', {
-    currency: 'USD',
-    notation: 'compact',
-    style: 'currency',
-  })
+    const usdFormatter = new Intl.NumberFormat('en-us', {
+      currency: 'USD',
+      notation: 'compact',
+      style: 'currency',
+    })
 
-  const numberFormatter = new Intl.NumberFormat('en-us', {
-    notation: 'compact',
-    style: 'decimal',
-  })
+    const numberFormatter = new Intl.NumberFormat('en-us', {
+      notation: 'compact',
+      style: 'decimal',
+    })
 
-  return {
-    ttv: usdFormatter.format(ttvRow['total_usd_value'] as number),
-    tvs: usdFormatter.format(tvsRow['total'] as number),
-    ttp: numberFormatter.format(ttvRow['total_tx_count'] as number),
+    return {
+      ttv: usdFormatter.format(ttvRow['total_usd_value'] as number),
+      tvs: usdFormatter.format(tvsRow['total'] as number),
+      ttp: numberFormatter.format(ttvRow['total_tx_count'] as number),
+    }
+  } catch {
+    return {
+      ttv: null,
+      tvs: null,
+      ttp: null,
+    }
   }
 }
 
@@ -54,11 +62,13 @@ export const LandingPage = ({
 
       <main className="flex flex-col">
         <Hero>
-          <Stats>
-            <Stats.Stat name="Total transacted value">{ttv}</Stats.Stat>
-            <Stats.Stat name="Total value secured">{tvs}</Stats.Stat>
-            <Stats.Stat name="Total transactions processed">{ttp}</Stats.Stat>
-          </Stats>
+          {ttv != null && tvs != null && ttp != null && (
+            <Stats>
+              <Stats.Stat name="Total transacted value">{ttv}</Stats.Stat>
+              <Stats.Stat name="Total value secured">{tvs}</Stats.Stat>
+              <Stats.Stat name="Total transactions processed">{ttp}</Stats.Stat>
+            </Stats>
+          )}
         </Hero>
 
         <PrimaryFeatures />
