fdb05d26cf55d872a08742e10728d61842a42423
Philipp Giese <giese.philipp+git@gmail.com>
2025-04-09 10:25:35 +0200

    try to use static assets better

--- deployables/landing-page/workers/app.ts
+++ deployables/landing-page/workers/app.ts
@@ -3,6 +3,7 @@ import { createRequestHandler } from 'react-router'
 declare global {
   interface CloudflareEnvironment extends Env {
     DUNE_ANALYTICS_API_KEY: string
+    ASSETS: { fetch: typeof window.fetch }
   }
 }
 
@@ -22,6 +23,12 @@ const requestHandler = createRequestHandler(
 
 export default {
   fetch(request, env, ctx) {
+    const url = new URL(request.url)
+
+    if (url.pathname.startsWith('/assets')) {
+      return env.ASSETS.fetch(request)
+    }
+
     return requestHandler(request, {
       cloudflare: { env, ctx },
     })
