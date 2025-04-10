06667a0d819cd770a66f6b9aa31f70184bf6c8ac
Philipp Giese <giese.philipp+git@gmail.com>
2025-04-10 12:49:04 +0200

    remove alias config from tests

--- packages/db/vitest.config.ts
+++ packages/db/vitest.config.ts
@@ -1,20 +1,9 @@
 /// <reference types="vitest" />
 
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
-import tsConfig from './tsconfig.json'
-
-const alias = Object.entries(tsConfig.compilerOptions.paths).reduce(
-  (result, [key, value]) => ({
-    ...result,
-    [key]: fileURLToPath(new URL(value[0], import.meta.url)),
-  }),
-  {},
-)
 
 export default defineConfig({
   test: {
-    alias,
     environment: 'happy-dom',
     setupFiles: ['./test-utils/setupTests.ts'],
     include: ['./src/**/*.{spec,test}.{ts,tsx}'],
