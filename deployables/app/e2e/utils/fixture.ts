a55b3ad834a03713823cb1c32a50d6dac73092fd
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2025-04-15 10:08:22 +0200

    chore: run e2e tests against latest public release (#1360)
    Fixes #1346

--- deployables/app/e2e/utils/fixture.ts
+++ deployables/app/e2e/utils/fixture.ts
@@ -2,13 +2,16 @@
 import { test as base, chromium, type BrowserContext } from '@playwright/test'
 import { fileURLToPath } from 'url'
 
+const extensionDirectory =
+  process.env.PILOT_EXTENSION_DIRECTORY || '../../../extension/public'
+
 export const test = base.extend<{
   context: BrowserContext
   extensionId: string
 }>({
   context: async ({}, use) => {
     const pathToExtension = fileURLToPath(
-      new URL('../../../extension/public', import.meta.url),
+      new URL(extensionDirectory, import.meta.url),
     )
 
     const context = await chromium.launchPersistentContext('', {
