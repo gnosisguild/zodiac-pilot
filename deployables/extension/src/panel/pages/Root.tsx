4af7b08140a897ee5b568efa90979f2b6bc033db
Philipp Giese <giese.philipp+git@gmail.com>
2025-04-16 13:02:19 +0200

    communicate in accounts from root

--- deployables/extension/src/panel/pages/Root.tsx
+++ deployables/extension/src/panel/pages/Root.tsx
@@ -1,5 +1,5 @@
+import { getAccount, getActiveAccount, saveActiveAccount } from '@/accounts'
 import { getFeatures, ProvideCompanionAppContext } from '@/companion'
-import { getLastUsedRouteId, saveLastUsedAccountId } from '@/execution-routes'
 import {
   formData,
   getActiveTab,
@@ -28,10 +28,10 @@ import { useLaunchRouteOnMessage } from './useLaunchRoute'
 import { useSaveRoute } from './useSaveRoute'
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const lastUsedRouteId = await getLastUsedRouteId()
+  const activeAccount = await getActiveAccount()
 
   return {
-    lastUsedRouteId,
+    activeAccountId: activeAccount == null ? null : activeAccount.id,
     companionAppUrl: getCompanionAppUrl(),
     features: await getFeatures({ signal: request.signal }),
   }
@@ -40,33 +40,34 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 export const action = async ({ request }: ActionFunctionArgs) => {
   const data = await request.formData()
 
-  const routeId = getString(data, 'routeId')
-  const lastUsedRouteId = await getLastUsedRouteId()
+  const accountId = getString(data, 'accountId')
+  const activeAccount = await getActiveAccount({ signal: request.signal })
+  const account = await getAccount(accountId, { signal: request.signal })
 
-  await saveLastUsedAccountId(routeId)
+  await saveActiveAccount(account)
 
   const activeTab = await getActiveTab()
 
   if (activeTab.id != null) {
     sendMessageToCompanionApp(activeTab.id, {
       type: CompanionResponseMessageType.PROVIDE_ACTIVE_ROUTE,
-      activeRouteId: routeId,
+      activeRouteId: accountId,
     })
   }
 
-  if (lastUsedRouteId != null) {
-    return redirect(`/${lastUsedRouteId}/clear-transactions/${routeId}`)
+  if (activeAccount != null) {
+    return redirect(`/${activeAccount.id}/clear-transactions/${accountId}`)
   }
 
-  return redirect(`/${routeId}`)
+  return redirect(`/${accountId}`)
 }
 
 const Root = () => {
   const submit = useSubmit()
 
-  const { lastUsedRouteId, companionAppUrl, features } =
+  const { activeAccountId, companionAppUrl, features } =
     useLoaderData<typeof loader>()
-  const [saveOptions, saveAndLaunchOptions] = useSaveRoute(lastUsedRouteId, {
+  const [saveOptions, saveAndLaunchOptions] = useSaveRoute(activeAccountId, {
     onSave: (route, tabId) => {
       sendMessageToCompanionApp(tabId, {
         type: CompanionResponseMessageType.PROVIDE_ROUTE,
@@ -79,8 +80,8 @@ const Root = () => {
 
   const { isLaunchPending, cancelLaunch, proceedWithLaunch } =
     useLaunchRouteOnMessage({
-      onLaunch(routeId) {
-        submit(formData({ routeId }), { method: 'POST' })
+      onLaunch(accountId) {
+        submit(formData({ accountId }), { method: 'POST' })
       },
     })
 
@@ -89,7 +90,7 @@ const Root = () => {
     async (_, { tabId }) => {
       await sendMessageToCompanionApp(tabId, {
         type: CompanionResponseMessageType.PROVIDE_ACTIVE_ROUTE,
-        activeRouteId: lastUsedRouteId,
+        activeRouteId: activeAccountId,
       })
     },
   )
