8cb365a53667ff51aa07ed2b093550596658736c
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-04-17 11:20:14 +0200

    chore: streamline dev db setup

---
+++ packages/db/init-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE "zodiac-os";
+CREATE DATABASE "zodiac-os-test";
+
+GRANT ALL PRIVILEGES ON DATABASE "zodiac-os" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "zodiac-os-test" TO postgres;
