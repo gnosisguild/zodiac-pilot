e5357c4867aa087b1637db2e2e3ab1ab446fddc0
Philipp Giese <187786+frontendphil@users.noreply.github.com>
2025-04-23 11:01:47 +0200

    chore: add missing spec (#1439)

--- deployables/extension/src/panel/pages/$activeAccountId/ActiveAccount.spec.ts
+++ deployables/extension/src/panel/pages/$activeAccountId/ActiveAccount.spec.ts
@@ -122,6 +122,35 @@ describe('Active Account', () => {
 
       expect(await screen.findByText('Remote account')).toBeInTheDocument()
     })
+
+    it('communicates the new active route as an event', async () => {
+      await mockRoute({ id: 'first-route', label: 'First route' })
+
+      const { mockedTab } = await render('/first-route', {
+        activeTab: { url: getCompanionAppUrl() },
+      })
+
+      expect(chromeMock.tabs.sendMessage).toHaveBeenCalledWith(mockedTab.id, {
+        type: CompanionResponseMessageType.PROVIDE_ACTIVE_ROUTE,
+        activeRouteId: 'first-route',
+      } satisfies CompanionResponseMessage)
+    })
+
+    it('tries to store the new active account on the remote', async () => {
+      const tenant = tenantFactory.createWithoutDb()
+      const user = userFactory.createWithoutDb(tenant)
+
+      const account = accountFactory.createWithoutDb(tenant, user)
+
+      mockGetRemoteAccount.mockResolvedValue(account)
+
+      await render(`/${account.id}`)
+
+      expect(saveRemoteActiveAccount).toHaveBeenCalledWith(
+        account,
+        expect.anything(),
+      )
+    })
   })
 
   describe('Edit', () => {
@@ -290,32 +319,13 @@ describe('Active Account', () => {
     })
   })
 
-  it('communicates the new active route as an event', async () => {
-    await mockRoute({ id: 'first-route', label: 'First route' })
+  describe('Removed remote account', () => {
+    it('handles deleted remote accounts gracefully', async () => {
+      mockGetRemoteAccount.mockRejectedValue('Account not found')
 
-    const { mockedTab } = await render('/first-route', {
-      activeTab: { url: getCompanionAppUrl() },
+      await render('/deleted-account-id')
+
+      await expectRouteToBe('/')
     })
-
-    expect(chromeMock.tabs.sendMessage).toHaveBeenCalledWith(mockedTab.id, {
-      type: CompanionResponseMessageType.PROVIDE_ACTIVE_ROUTE,
-      activeRouteId: 'first-route',
-    } satisfies CompanionResponseMessage)
-  })
-
-  it('tries to store the new active account on the remote', async () => {
-    const tenant = tenantFactory.createWithoutDb()
-    const user = userFactory.createWithoutDb(tenant)
-
-    const account = accountFactory.createWithoutDb(tenant, user)
-
-    mockGetRemoteAccount.mockResolvedValue(account)
-
-    await render(`/${account.id}`)
-
-    expect(saveRemoteActiveAccount).toHaveBeenCalledWith(
-      account,
-      expect.anything(),
-    )
   })
 })
