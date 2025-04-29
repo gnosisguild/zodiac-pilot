ff497a582bb79b2441ca1a025b4a99b8b01cbc58
Philipp Giese <giese.philipp+git@gmail.com>
2025-04-29 12:16:19 +0200

    remove obsolete extension e2e package

--- deployables/extension/e2e/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { expect, test } from './fixture'
-export { loadExtension } from './loadExtension'
-export { defaultMockAccount, mockWeb3 } from './mockWeb3'
-export { waitFor } from './waitFor'
