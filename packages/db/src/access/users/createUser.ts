a18b3148a2fce4aeb747983a7c65397463d789f1
Philipp Giese <giese.philipp+git@gmail.com>
2025-05-06 11:00:49 +0200

    create users module

--- packages/db/src/access/users/createUser.ts
+++ packages/db/src/access/users/createUser.ts
@@ -1,5 +1,5 @@
 import { UserTable } from '@zodiac/db/schema'
-import type { DBClient } from '../dbClient'
+import type { DBClient } from '../../dbClient'
 
 export const createUser = async (db: DBClient) => {
   const [user] = await db.insert(UserTable).values({}).returning()
--- packages/db/src/access/users/getUser.ts
+++ packages/db/src/access/users/getUser.ts
@@ -1,6 +1,6 @@
 import { invariant } from '@epic-web/invariant'
 import type { UUID } from 'crypto'
-import type { DBClient } from '../dbClient'
+import type { DBClient } from '../../dbClient'
 
 export const getUser = async (db: DBClient, userId: UUID) => {
   const user = await db.query.user.findFirst({
