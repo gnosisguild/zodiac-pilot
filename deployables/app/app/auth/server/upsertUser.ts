d70b3a5253f1de4cb776880a99adc515581507b4
Philipp Giese <giese.philipp+git@gmail.com>
2025-05-22 10:28:19 +0200

    make sure to keep an updated user name

--- deployables/app/app/auth/server/upsertUser.ts
+++ deployables/app/app/auth/server/upsertUser.ts
@@ -1,11 +1,23 @@
 import type { User } from '@workos-inc/node'
-import { createUser, findUserByExternalId, type DBClient } from '@zodiac/db'
+import {
+  createUser,
+  findUserByExternalId,
+  updateUser,
+  type DBClient,
+} from '@zodiac/db'
 
 export const upsertUser = async (db: DBClient, workOSUser: User) => {
   const existingUser = await findUserByExternalId(db, workOSUser.id)
 
   if (existingUser != null) {
-    return existingUser
+    const fullName =
+      `${workOSUser.firstName ?? ''} ${workOSUser.lastName ?? ''}`.trim()
+
+    if (fullName === existingUser.fullName) {
+      return existingUser
+    }
+
+    return updateUser(db, existingUser.id, { fullName })
   }
 
   return createUser(db, {
