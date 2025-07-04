d45333317adce30dc329049183f16018504b2338
Philipp Giese <giese.philipp+git@gmail.com>
2025-07-04 14:37:16 +0200

    select offline version of default workspace on load

--- deployables/app/app/routes/index.tsx
+++ deployables/app/app/routes/index.tsx
@@ -1,3 +1,24 @@
+import { authorizedLoader } from '@/auth-server'
+import { dbClient, getDefaultWorkspace } from '@zodiac/db'
 import { href, redirect } from 'react-router'
+import type { Route } from './+types'
 
-export const loader = () => redirect(href('/connect'))
+export const loader = (args: Route.LoaderArgs) =>
+  authorizedLoader(
+    args,
+    async ({
+      context: {
+        auth: { tenant },
+      },
+    }) => {
+      if (tenant == null) {
+        return redirect(href('/offline'))
+      }
+
+      const defaultWorkspace = await getDefaultWorkspace(dbClient(), tenant.id)
+
+      return redirect(
+        href('/workspace/:workspaceId', { workspaceId: defaultWorkspace.id }),
+      )
+    },
+  )
