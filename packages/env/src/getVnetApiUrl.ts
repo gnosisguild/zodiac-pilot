1320ca5e98f843de56a6acdeb7ceb66044900ae3
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-07-07 23:25:41 +0200

    use final vnet url

--- packages/env/src/getVnetApiUrl.ts
+++ packages/env/src/getVnetApiUrl.ts
@@ -1,4 +1,4 @@
 // for now we just use hard-coding
 export const getVnetApiUrl = () => {
-  return 'https://b641ddb2-vnet-api.gnosisguild.workers.dev' // 'https://vnet-api.pilot.gnosisguild.org' // TODO
+  return 'https://vnet.pilot.gnosisguild.org'
 }
