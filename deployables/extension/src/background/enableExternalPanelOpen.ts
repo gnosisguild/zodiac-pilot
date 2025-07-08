998fb68b5498a2c1f5bdde375549731376ab59e7
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-07-08 23:11:37 +0200

    ensure side panel is global across tabs

--- deployables/extension/src/background/enableExternalPanelOpen.ts
+++ deployables/extension/src/background/enableExternalPanelOpen.ts
@@ -8,14 +8,12 @@ export const enableExternalPanelOpen = () => {
   chrome.runtime.onMessage.addListener(
     createTabMessageHandler(
       CompanionAppMessageType.OPEN_PILOT,
-      async ({ search = '' }, { tabId, windowId }) => {
-        // This will open a tab-specific side panel only on the current tab.
+      async ({ search = '' }, { windowId }) => {
+        // Open the side panel for the current window
         await chrome.sidePanel.open({
-          tabId,
           windowId,
         })
         await chrome.sidePanel.setOptions({
-          tabId,
           path: resolve('../../sidepanel.html') + search,
           enabled: true,
         })
