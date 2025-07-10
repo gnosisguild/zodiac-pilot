0fc82e55be09500e112d78732a392bf621a32b42
Philipp Giese <giese.philipp+git@gmail.com>
2025-07-10 16:02:15 +0200

    reduce side-loading of wallets

--- deployables/app/app/routes/extension/routes.ts
+++ deployables/app/app/routes/extension/routes.ts
@@ -1,6 +1,12 @@
 import { authorizedLoader } from '@/auth-server'
-import { invariantResponse } from '@epic-web/invariant'
-import { dbClient, getAccount, getRoutes, toExecutionRoute } from '@zodiac/db'
+import { invariant, invariantResponse } from '@epic-web/invariant'
+import {
+  dbClient,
+  getAccount,
+  getRoutes,
+  getWallets,
+  toExecutionRoute,
+} from '@zodiac/db'
 import { isUUID } from '@zodiac/schema'
 import type { Route } from './+types/routes'
 
@@ -19,14 +25,22 @@ export const loader = (args: Route.LoaderArgs) =>
 
       invariantResponse(isUUID(accountId), '"accountId" is not a UUID')
 
-      const [routes, account] = await Promise.all([
+      const [routes, account, wallets] = await Promise.all([
         getRoutes(dbClient(), tenant.id, { userId: user.id, accountId }),
         getAccount(dbClient(), accountId),
+        getWallets(dbClient(), user.id),
       ])
 
-      return routes.map((route) =>
-        toExecutionRoute({ wallet: route.wallet, account, route }),
-      )
+      return routes.map((route) => {
+        const wallet = wallets.find((wallet) => wallet.id === route.fromId)
+
+        invariant(
+          wallet != null,
+          `Could not find wallet with id "${route.fromId}"`,
+        )
+
+        return toExecutionRoute({ wallet, account, route })
+      })
     },
     {
       async hasAccess({ tenant, params: { accountId } }) {
