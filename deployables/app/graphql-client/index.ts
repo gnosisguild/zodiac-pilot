edf6ef95c0bd6754648cdaa9d2093d69643d726b
Philipp Giese <giese.philipp+git@gmail.com>
2025-07-11 16:43:03 +0200

    etup graphql

---
+++ deployables/app/graphql-client/index.ts
@@ -0,0 +1,1 @@
+export { graphqlClient } from './graphql-client'
---
+++ graphql.config.ts
@@ -0,0 +1,4 @@
+export default {
+  schema: 'https://gnosisguild.squids.live/roles:production/api/graphql',
+  documents: ['**/*.graphql'],
+}
