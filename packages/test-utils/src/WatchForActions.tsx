c90f1ba398b0a68ffa484ce75c108f16f71d8c07
Philipp Giese <giese.philipp+git@gmail.com>
2025-07-25 10:27:14 +0200

    make it possible to wait for actions with a certain intent

--- packages/test-utils/src/WatchForActions.tsx
+++ packages/test-utils/src/WatchForActions.tsx
@@ -1,21 +1,34 @@
 import { screen, waitFor } from '@testing-library/react'
+import { getOptionalString } from '@zodiac/form-data'
+import { useEffect, useRef } from 'react'
 import { useNavigation } from 'react-router'
 import { sleepTillIdle } from './sleepTillIdle'
 
 const watchActionStateElementId = 'test-action-state'
 
 export const WatchForActions = () => {
-  const navigation = useNavigation()
+  const { state, formData } = useNavigation()
+
+  const intentRef = useRef<string | undefined>('')
+
+  useEffect(() => {
+    if (formData == null) {
+      return
+    }
+
+    intentRef.current = getOptionalString(formData, 'intent')
+  }, [formData])
 
   return (
     <div
       data-testid={watchActionStateElementId}
-      data-state={navigation.state}
+      data-state={state}
+      data-intent={intentRef.current}
     />
   )
 }
 
-export const waitForPendingActions = async () => {
+export const waitForPendingActions = async (intent?: string) => {
   await sleepTillIdle()
 
   return waitFor(async () => {
@@ -28,6 +41,10 @@ export const waitForPendingActions = async () => {
       const state = testElement.getAttribute('data-state')
 
       expect(state).toEqual('idle')
+
+      if (intent != null) {
+        expect(intent).toEqual(testElement.getAttribute('data-intent'))
+      }
     })
   })
 }
