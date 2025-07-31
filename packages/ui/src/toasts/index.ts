cc749a53a8768d843c6e0e40e778e9e4925ec123
Philipp Giese <giese.philipp+git@gmail.com>
2025-07-31 14:02:51 +0200

    prevent hydration error

--- packages/ui/src/toasts/index.ts
+++ packages/ui/src/toasts/index.ts
@@ -1,4 +1,4 @@
-export { ToastContainer } from 'react-toastify'
+export { ToastContainer } from 'react-toastify/unstyled'
 export { errorToast } from './errorToast'
 export { infoToast } from './infoToast'
 export { successToast } from './successToast'
