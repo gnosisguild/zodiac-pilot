964d5fcfe447993e37a11cdc9c2b7c3fc5efdb4e
Philipp Giese <giese.philipp+git@gmail.com>
2025-08-06 17:18:30 +0200

    start work on building out swapper role permissions

--- packages/schema/src/hex.ts
+++ packages/schema/src/hex.ts
@@ -2,8 +2,24 @@ import { z } from 'zod'
 
 export type Hex = `0x${string}`
 
-export const isHex = (value: unknown): value is Hex =>
-  typeof value === 'string' && value.startsWith('0x') && value.length >= 2
+type IsHexOptions = {
+  length?: number
+}
+
+export const isHex = (
+  value: unknown,
+  { length }: IsHexOptions = {},
+): value is Hex => {
+  if (typeof value !== 'string' || !value.startsWith('0x')) {
+    return false
+  }
+
+  if (length != null) {
+    return value.length === length
+  }
+
+  return value.length >= 2
+}
 
 export const hexSchema = z.custom<Hex>(
   (value) => typeof value === 'string' && isHex(value),
