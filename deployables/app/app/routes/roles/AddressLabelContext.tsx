c9a1e4ce532985df7b9f77fea48871309351ed23
Philipp Giese <giese.philipp+git@gmail.com>
2025-08-11 11:47:23 +0200

    add labels for created roles

---
+++ deployables/app/app/routes/roles/AddressLabelContext.tsx
@@ -0,0 +1,34 @@
+import { HexAddress } from '@zodiac/schema'
+import { Address } from '@zodiac/web3'
+import {
+  ComponentProps,
+  createContext,
+  PropsWithChildren,
+  useContext,
+} from 'react'
+import { unprefixAddress } from 'ser-kit'
+
+export type Labels = Record<HexAddress, string>
+const AddressLabelContext = createContext<Labels>({})
+
+export const ProvideAddressLabels = ({
+  children,
+  labels,
+}: PropsWithChildren<{ labels: Labels }>) => (
+  <AddressLabelContext value={labels}>{children}</AddressLabelContext>
+)
+
+export const LabeledAddress = ({
+  children,
+  ...props
+}: ComponentProps<typeof Address>) => {
+  const labels = useContext(AddressLabelContext)
+
+  const address = unprefixAddress(children)
+
+  return (
+    <Address {...props} label={labels[address]}>
+      {children}
+    </Address>
+  )
+}
