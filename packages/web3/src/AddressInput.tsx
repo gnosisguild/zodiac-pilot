3c3c740f2bd00acfb122108a3cd183d94907d1ea
Philipp Giese <giese.philipp+git@gmail.com>
2025-08-14 12:31:41 +0200

    show blockie for current input

--- packages/web3/src/AddressInput.tsx
+++ packages/web3/src/AddressInput.tsx
@@ -2,7 +2,7 @@ import { ZERO_ADDRESS } from '@zodiac/chains'
 import { addressSchema, type HexAddress } from '@zodiac/schema'
 import { Input, InputLayout, type ComposableInputProps } from '@zodiac/ui'
 import classNames from 'classnames'
-import type { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { useState, type ComponentPropsWithoutRef, type ReactNode } from 'react'
 import { getAddress } from 'viem'
 import { Blockie } from './Blockie'
 
@@ -30,62 +30,73 @@ export const AddressInput = ({
   onChange,
   required = false,
   ...props
-}: AddressInputProps) => (
-  <Input
-    label={label}
-    description={description}
-    error={error}
-    hideLabel={hideLabel}
-    required={required}
-  >
-    {({ inputId, descriptionId }) => (
-      <InputLayout
-        disabled={disabled}
-        before={
-          <Blockie
-            address={value || defaultValue || ZERO_ADDRESS}
-            className="ml-4 size-5 shrink-0"
-          />
-        }
-        after={action && <div className="mr-2">{action}</div>}
-      >
-        <input
-          {...props}
-          type="text"
-          pattern="0x\w{40}"
-          id={inputId}
-          required={required}
-          disabled={disabled}
-          value={value == null ? undefined : getAddress(value)}
-          defaultValue={defaultValue}
-          aria-describedby={descriptionId}
-          aria-errormessage={error ?? undefined}
-          placeholder={placeholder}
-          onInvalid={(e) => {
-            if (e.currentTarget.validity.patternMismatch) {
-              e.currentTarget.setCustomValidity(
-                'Please use a 40 character hex address',
-              )
-            }
-          }}
-          onInput={(e) => e.currentTarget.setCustomValidity('')}
-          onChange={(event) => {
-            const value = event.target.value
+}: AddressInputProps) => {
+  const [blockieValue, setBlockieValue] = useState(
+    defaultValue || value || ZERO_ADDRESS,
+  )
 
-            if (onChange != null) {
-              try {
-                onChange(addressSchema.parse(value))
-              } catch {
-                onChange(null)
+  return (
+    <Input
+      label={label}
+      description={description}
+      error={error}
+      hideLabel={hideLabel}
+      required={required}
+    >
+      {({ inputId, descriptionId }) => (
+        <InputLayout
+          disabled={disabled}
+          before={
+            <Blockie address={blockieValue} className="ml-4 size-5 shrink-0" />
+          }
+          after={action && <div className="mr-2">{action}</div>}
+        >
+          <input
+            {...props}
+            type="text"
+            pattern="0x\w{40}"
+            id={inputId}
+            required={required}
+            disabled={disabled}
+            value={value == null ? undefined : getAddress(value)}
+            defaultValue={defaultValue}
+            aria-describedby={descriptionId}
+            aria-errormessage={error ?? undefined}
+            placeholder={placeholder}
+            onInvalid={(e) => {
+              if (e.currentTarget.validity.patternMismatch) {
+                e.currentTarget.setCustomValidity(
+                  'Please use a 40 character hex address',
+                )
               }
-            }
-          }}
-          className={classNames(
-            'outline-hidden w-full border-none bg-transparent px-4 py-2 font-mono text-sm',
-            disabled && 'cursor-not-allowed',
-          )}
-        />
-      </InputLayout>
-    )}
-  </Input>
-)
+            }}
+            onInput={(e) => e.currentTarget.setCustomValidity('')}
+            onChange={(event) => {
+              const value = event.target.value
+
+              try {
+                const address = addressSchema.parse(value)
+
+                setBlockieValue(address)
+
+                if (onChange != null) {
+                  onChange(address)
+                }
+              } catch {
+                setBlockieValue(ZERO_ADDRESS)
+
+                if (onChange != null) {
+                  onChange(null)
+                }
+              }
+            }}
+            className={classNames(
+              'outline-hidden w-full border-none bg-transparent px-4 py-2 font-mono text-sm',
+              disabled && 'cursor-not-allowed',
+            )}
+          />
+        </InputLayout>
+      )}
+    </Input>
+  )
+}
