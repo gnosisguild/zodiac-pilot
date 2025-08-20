5056639b0cffb9f052e69877e04890d15b6774e3
Philipp Giese <giese.philipp+git@gmail.com>
2025-08-20 12:01:31 +0200

    make sure to redirect user to a logged in url when loading a local account

--- deployables/app/app/routes/local-accounts/load-account.tsx
+++ deployables/app/app/routes/local-accounts/load-account.tsx
@@ -1,11 +1,29 @@
+import { authorizedLoader } from '@/auth-server'
 import { Page } from '@/components'
 import { CompanionAppMessageType, companionRequest } from '@zodiac/messages'
 import { encode } from '@zodiac/schema'
 import { href, redirect } from 'react-router'
 import type { Route } from './+types/load-account'
 
+export const loader = (args: Route.LoaderArgs) =>
+  authorizedLoader(
+    args,
+    async ({
+      context: {
+        auth: { tenant },
+      },
+    }) => {
+      if (tenant == null) {
+        return { defaultWorkspaceId: null }
+      }
+
+      return { defaultWorkspaceId: tenant.defaultWorkspaceId }
+    },
+  )
+
 export const clientLoader = async ({
   params: { accountId, workspaceId },
+  serverLoader,
 }: Route.ClientLoaderArgs) => {
   const { promise, resolve, reject } = Promise.withResolvers<string>()
 
@@ -14,21 +32,23 @@ export const clientLoader = async ({
       type: CompanionAppMessageType.REQUEST_ROUTE,
       routeId: accountId,
     },
-    ({ route }) => {
+    async ({ route }) => {
       if (route == null) {
         return reject(
           `Account with id "${accountId}" does not exist in the local extension scope`,
         )
       }
 
+      const { defaultWorkspaceId } = await serverLoader()
+
       resolve(
-        workspaceId == null
+        defaultWorkspaceId == null
           ? href('/offline/accounts/:accountId/:data', {
               accountId: route.id,
               data: encode(route),
             })
           : href('/workspace/:workspaceId/local-accounts/:accountId/:data', {
-              workspaceId,
+              workspaceId: workspaceId || defaultWorkspaceId,
               accountId: route.id,
               data: encode(route),
             }),
