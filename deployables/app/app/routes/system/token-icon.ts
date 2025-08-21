f3fd62ea2ae8b69505eda410e5e3298d5578a308
Philipp Giese <giese.philipp+git@gmail.com>
2025-08-21 16:35:23 +0200

    add fallback and cache to token-icon endpoint

--- deployables/app/app/routes/system/token-icon.ts
+++ deployables/app/app/routes/system/token-icon.ts
@@ -1,13 +1,42 @@
+import { getChain, getTokenDetails } from '@/balances-server'
 import { getVerifiedTokens } from '@/token-list'
+import { getChainId } from '@zodiac/chains'
 import { verifyPrefixedAddress } from '@zodiac/schema'
+import { unprefixAddress } from 'ser-kit'
 import { Route } from './+types/token-icon'
 
 export const loader = async ({
   params: { prefixedAddress },
 }: Route.LoaderArgs) => {
-  const [token] = await getVerifiedTokens([
-    verifyPrefixedAddress(prefixedAddress),
-  ])
+  const verifiedAddress = verifyPrefixedAddress(prefixedAddress)
 
-  return await fetch(token.logoURI)
+  const [token] = await getVerifiedTokens([verifiedAddress])
+
+  if (token == null) {
+    const chain = await getChain(getChainId(verifiedAddress))
+    const details = await getTokenDetails(chain, {
+      address: unprefixAddress(verifiedAddress),
+    })
+
+    if (details == null || details.logoUrl == null) {
+      return null
+    }
+
+    const response = await fetch(details.logoUrl)
+
+    return new Response(response.body, {
+      headers: {
+        ['Cache-Control']:
+          'public, max-age=604800, stale-while-revalidate=86400',
+      },
+    })
+  }
+
+  const response = await fetch(token.logoURI)
+
+  return new Response(response.body, {
+    headers: {
+      ['Cache-Control']: 'public, max-age=604800, stale-while-revalidate=86400',
+    },
+  })
 }
